b0cf7e1cf4dcbe85ca8be8ab44e1d3bdc5a9a58b
Jozef Mikovic <jmikovic@redhat.com>
2019-01-15 08:51:34 +0100

    remove debug from cmdline when adding boot entry

--- repos/system_upgrade/el7toel8/actors/createinitrdbootentry/actor.py
+++ repos/system_upgrade/el7toel8/actors/createinitrdbootentry/actor.py
@@ -1,6 +1,9 @@
+import os
+
 from leapp.actors import Actor
 from leapp.tags import IPUWorkflowTag, InterimPreparationPhaseTag
 from subprocess import check_call
+
 
 class CreateInitRdBootEntry(Actor):
     name = 'create_init_rd_boot_entry'
@@ -12,11 +15,12 @@ class CreateInitRdBootEntry(Actor):
     def process(self):
         vmlinuz_fpath = self.get_file_path('vmlinuz-upgrade.x86_64')
         initram_fpath = self.get_file_path('initramfs-upgrade.x86_64.img')
+        debug = 'debug' if os.getenv('LEAPP_DEBUG', '0') == '1' else ''
 
         if vmlinuz_fpath is None or initram_fpath is None:
             self.report_error('Could not find vmlinuz-upgrade.x86_64 and/or initramfs-upgrade.x86_64.img '
                               'in the following paths: {}'.format(' '.join(self.files_paths)),
-                               details='You may want to try to reinstall "leapp-repository" package')
+                              details={'solution': 'You may want to try to reinstall "leapp-repository" package'})
             return
 
         check_call(['/bin/cp', vmlinuz_fpath, initram_fpath, '/boot'])
@@ -27,5 +31,5 @@ class CreateInitRdBootEntry(Actor):
             '--title=RHEL Upgrade RAMDISK',
             '--copy-default',
             '--make-default',
-            '--args="debug enforcing=0 rd.plymouth=0 plymouth.enable=0"'
+            '--args="{DEBUG} enforcing=0 rd.plymouth=0 plymouth.enable=0"'.format(DEBUG=debug)
         ])
