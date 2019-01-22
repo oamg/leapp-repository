f595dbfd7d7bafb33fdbeb12b03030e9fa5dca23
Michal Reznik <mreznik@redhat.com>
2019-01-22 12:45:55 +0100

    Fix "transaction_workarounds" actor tests
    We changed "to_install" field to "local_rpms" field and did not
    reflect that in the tests.

--- repos/system_upgrade/el7toel8/actors/transactionworkarounds/tests/test_transaction_workarounds.py
+++ repos/system_upgrade/el7toel8/actors/transactionworkarounds/tests/test_transaction_workarounds.py
@@ -5,6 +5,7 @@ from leapp.models import RpmTransactionTasks
 def test_execution(current_actor_context):
     current_actor_context.run()
     assert current_actor_context.consume(RpmTransactionTasks)
-    assert current_actor_context.consume(RpmTransactionTasks)[0].to_install
+    assert current_actor_context.consume(RpmTransactionTasks)[0].local_rpms
+    assert not current_actor_context.consume(RpmTransactionTasks)[0].to_install
     assert not current_actor_context.consume(RpmTransactionTasks)[0].to_remove
     assert not current_actor_context.consume(RpmTransactionTasks)[0].to_keep
