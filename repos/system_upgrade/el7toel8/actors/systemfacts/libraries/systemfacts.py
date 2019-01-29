e59d81013d53c17030d4c5b7e18c7f1d34da75d0
Arthur Mello <amello@redhat.com>
2019-01-29 09:55:42 +0000

    Improve code following peer review:
    - Remove unnecessary trailing commas
    - Improve functions identation

--- repos/system_upgrade/el7toel8/actors/systemfacts/libraries/systemfacts.py
+++ repos/system_upgrade/el7toel8/actors/systemfacts/libraries/systemfacts.py
@@ -45,96 +45,121 @@ def anyhasprefix(value, prefixes):
     return False
 
 
+@aslist
+def _get_system_users():
+    for p in pwd.getpwall():
+        yield User(
+            name=p.pw_name,
+            uid=p.pw_uid,
+            gid=p.pw_gid,
+            home=p.pw_dir
+        )
+
+
 def get_system_users_status():
     ''' Get a list of users from `/etc/passwd` '''
-
-    @aslist
-    def _get_system_users():
-        for p in pwd.getpwall():
-            yield User(
-                name=p.pw_name,
-                uid=p.pw_uid,
-                gid=p.pw_gid,
-                home=p.pw_dir
-            )
-
     return UsersFacts(users=_get_system_users())
+
+
+@aslist
+def _get_system_groups():
+    for g in grp.getgrall():
+        yield Group(
+            name=g.gr_name,
+            gid=g.gr_gid,
+            members=g.gr_mem
+        )
 
 
 def get_system_groups_status():
     ''' Get a list of groups from `/etc/groups` '''
-
-    @aslist
-    def _get_system_groups():
-        for g in grp.getgrall():
-            yield Group(
-                name=g.gr_name,
-                gid=g.gr_gid,
-                members=g.gr_mem
-            )
-
     return GroupsFacts(groups=_get_system_groups())
+
+
+@aslist
+def _get_active_kernel_modules(logger):
+    lines = call(['lsmod'])
+    for l in lines[1:]:
+        name = l.split(' ')[0]
+
+        # Read parameters of the given module as exposed by the
+        # `/sys` VFS, if there are no parameters exposed we just
+        # take the name of the module
+        base_path = '/sys/module/{module}'.format(module=name)
+        parameters_path = os.path.join(base_path, 'parameters')
+        if not os.path.exists(parameters_path):
+            yield ActiveKernelModule(filename=name, parameters=[])
+            continue
+
+        # Use `modinfo` to probe for signature information
+        parameter_dict = {}
+        try:
+            signature = call(['modinfo', '-F', 'signature', name], split=False)
+        except CalledProcessError:
+            signature = None
+
+        signature_string = None
+        if signature:
+            # Remove whitspace from the signature string
+            signature_string = re.sub(r"\s+", "", signature, flags=re.UNICODE)
+
+        # Since we're using the `/sys` VFS we need to use `os.listdir()` to get
+        # all the property names and then just read from all the listed paths
+        parameters = sorted(os.listdir(parameters_path))
+        for param in parameters:
+            try:
+                with open(os.path.join(parameters_path, param), mode='r') as fp:
+                    parameter_dict[param] = fp.read().strip()
+            except IOError as exc:
+                # Some parameters are write-only, in that case we just log the name of parameter
+                # and the module and continue 
+                if exc.errno in (errno.EACCES, errno.EPERM):
+                    msg = 'Unable to read parameter "{param}" of kernel module "{name}"'
+                    logger.warning(msg.format(param=param, name=name))
+                else:
+                    raise exc
+
+        # Project the dictionary as a list of key values
+        items = [
+            KernelModuleParameter(name=k, value=v)
+            for (k, v) in six.iteritems(parameter_dict)
+        ]
+
+        yield ActiveKernelModule(
+            filename=name,
+            parameters=items,
+            signature=signature_string
+        )
 
 
 def get_active_kernel_modules_status(logger):
     ''' Get a list of active kernel modules '''
-
-    @aslist
-    def _get_active_kernel_modules(logger):
-        lines = call(['lsmod'])
-        for l in lines[1:]:
-            name = l.split(' ')[0]
-
-            # Read parameters of the given module as exposed by the
-            # `/sys` VFS, if there are no parameters exposed we just
-            # take the name of the module
-            base_path = '/sys/module/{module}'.format(module=name)
-            parameters_path = os.path.join(base_path, 'parameters')
-            if not os.path.exists(parameters_path):
-                yield ActiveKernelModule(filename=name, parameters=[])
-                continue
-
-            # Use `modinfo` to probe for signature information
-            parameter_dict = {}
-            try:
-                signature = call(['modinfo', '-F', 'signature', name], split=False)
-            except CalledProcessError:
-                signature = None
-
-            signature_string = None
-            if signature:
-                # Remove whitspace from the signature string
-                signature_string = re.sub(r"\s+", "", signature, flags=re.UNICODE)
-
-            # Since we're using the `/sys` VFS we need to use `os.listdir()` to get
-            # all the property names and then just read from all the listed paths
-            parameters = sorted(os.listdir(parameters_path))
-            for param in parameters:
-                try:
-                    with open(os.path.join(parameters_path, param), mode='r') as fp:
-                        parameter_dict[param] = fp.read().strip()
-                except IOError as exc:
-                    # Some parameters are write-only, in that case we just log the name of parameter
-                    # and the module and continue 
-                    if exc.errno in (errno.EACCES, errno.EPERM):
-                        msg = 'Unable to read parameter "{param}" of kernel module "{name}"'
-                        logger.warning(msg.format(param=param, name=name))
-                    else:
-                        raise exc
-
-            # Project the dictionary as a list of key values
-            items = [
-                KernelModuleParameter(name=k, value=v)
-                for (k, v) in six.iteritems(parameter_dict)
-            ]
-
-            yield ActiveKernelModule(
-                filename=name,
-                parameters=items,
-                signature=signature_string
-            )
-
     return ActiveKernelModulesFacts(kernel_modules=_get_active_kernel_modules(logger))
+
+
+@aslist
+def _get_sysctls():
+    unstable = ('fs.dentry-state', 'fs.file-nr', 'fs.inode-nr',
+                'fs.inode-state', 'kernel.random.uuid', 'kernel.random.entropy_avail',
+                'kernel.ns_last_pid', 'net.netfilter.nf_conntrack_count',
+                'net.netfilter.nf_conntrack_events', 'kernel.sched_domain.',
+                'dev.cdrom.info', 'kernel.pty.nr')
+
+    variables = []
+    for sc in call(['sysctl', '-a']):
+        name = sc.split(' ', 1)[0]
+        # if the sysctl name has an unstable prefix, we skip
+        if anyhasprefix(name, unstable):
+            continue
+        variables.append(sc)
+
+    # sort our variables so they can be diffed directly when needed
+    for var in sorted(variables):
+        name, value = tuple(map(type(var).strip, var.split('=')))
+        yield SysctlVariable(
+            name=name,
+            value=value
+        )
 
 
 def get_sysctls_status():
@@ -148,65 +173,41 @@ def get_sysctls_status():
                 | cut -d' ' -f1\
                 | cut -d+ -f2
     '''
+    return SysctlVariablesFacts(sysctl_variables=_get_sysctls())
+
+
+@aslist
+def _get_repositories():
+    def asbool(x):
+        return x == 0
 
     @aslist
-    def _get_sysctls():
-        unstable = ('fs.dentry-state', 'fs.file-nr', 'fs.inode-nr',
-                    'fs.inode-state', 'kernel.random.uuid', 'kernel.random.entropy_avail',
-                    'kernel.ns_last_pid', 'net.netfilter.nf_conntrack_count',
-                    'net.netfilter.nf_conntrack_events', 'kernel.sched_domain.',
-                    'dev.cdrom.info', 'kernel.pty.nr')
+    def _parse(r):
+        with open(r, mode='r') as fp:
+            cp = configparser.ConfigParser()
+            cp.readfp(fp)
+            for section in cp.sections():
+                prepared = {'additional_fields': {}}
+                data = dict(cp.items(section))
+                for key in data.keys():
+                    if key in RepositoryData.fields:
+                        if isinstance(RepositoryData.fields[key], fields.Boolean):
+                            data[key] = asbool(data[key])
+                        prepared[key] = data[key]
+                    else:
+                        prepared['additional_fields'][key] = data[key]
+                prepared['additional_fields'] = json.dumps(prepared['additional_fields'])
+                yield RepositoryData(**prepared)
 
-        variables = []
-        for sc in call(['sysctl', '-a']):
-            name = sc.split(' ', 1)[0]
-            # if the sysctl name has an unstable prefix, we skip
-            if anyhasprefix(name, unstable):
-                continue
-            variables.append(sc)
-
-        # sort our variables so they can be diffed directly when needed
-        for var in sorted(variables):
-            name, value = tuple(map(type(var).strip, var.split('=')))
-            yield SysctlVariable(
-                name=name,
-                value=value
-            )
-
-    return SysctlVariablesFacts(sysctl_variables=_get_sysctls())
+    repos = call(
+        ['find', '/etc/yum.repos.d/', '-type', 'f', '-name', '*.repo']
+    )
+    for repo in repos:
+        yield RepositoryFile(file=repo, data=_parse(repo))
 
 
 def get_repositories_status():
     ''' Get a basic information about YUM repositories installed in the system '''
-    @aslist
-    def _get_repositories():
-        def asbool(x):
-            return x == 0
-
-        @aslist
-        def _parse(r):
-            with open(r, mode='r') as fp:
-                cp = configparser.ConfigParser()
-                cp.readfp(fp)
-                for section in cp.sections():
-                    prepared = {'additional_fields': {}}
-                    data = dict(cp.items(section))
-                    for key in data.keys():
-                        if key in RepositoryData.fields:
-                            if isinstance(RepositoryData.fields[key], fields.Boolean):
-                                data[key] = asbool(data[key])
-                            prepared[key] = data[key]
-                        else:
-                            prepared['additional_fields'][key] = data[key]
-                    prepared['additional_fields'] = json.dumps(prepared['additional_fields'])
-                    yield RepositoryData(**prepared)
-
-        repos = call(
-            ['find', '/etc/yum.repos.d/', '-type', 'f', '-name', '*.repo']
-        )
-        for repo in repos:
-            yield RepositoryFile(file=repo, data=_parse(repo))
-
     return RepositoriesFacts(repositories=_get_repositories())
 
 
@@ -259,5 +260,5 @@ def get_firewalls_status():
 
     return FirewallsFacts(
             firewalld=_get_firewall_status('firewalld'),
-            iptables=_get_firewall_status('iptables'),
+            iptables=_get_firewall_status('iptables')
             )
