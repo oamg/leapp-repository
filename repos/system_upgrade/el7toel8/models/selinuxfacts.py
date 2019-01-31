81171044ebe7246f0b040942a35ef3183d39e7d0
Radomil Pelisek <radomil@redhat.com>
2019-01-31 15:49:38 +0100

    Fix SELinux model issue #20

--- repos/system_upgrade/el7toel8/models/selinuxfacts.py
+++ repos/system_upgrade/el7toel8/models/selinuxfacts.py
@@ -5,10 +5,8 @@ from leapp.topics import SystemFactsTopic
 class SELinuxFacts(Model):
     topic = SystemFactsTopic
 
-    # FIXME: fixme properly regarding the issue:
-    # # https://github.com/oamg/leapp-repository/issues/20
     runtime_mode = fields.Nullable(fields.StringEnum(['enforcing', 'permissive']))
     static_mode = fields.StringEnum(['enforcing', 'permissive', 'disabled'])
     enabled = fields.Boolean()
-    policy = fields.String()
+    policy = fields.StringEnum(['targeted', 'minimum', 'mls'])
     mls_enabled = fields.Boolean()
