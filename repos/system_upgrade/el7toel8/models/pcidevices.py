f0370b7b03d9ef417f085b75b2e3c41c4b876551
Arthur Mello <amello@redhat.com>
2019-01-31 07:51:37 -0200

    Update test files for new actors following peer review

--- repos/system_upgrade/el7toel8/models/pcidevices.py
+++ repos/system_upgrade/el7toel8/models/pcidevices.py
@@ -6,13 +6,13 @@ class PCIDevice(Model):
     topic = SystemInfoTopic
 
     slot = fields.String()
-    cls = fields.String()
+    dev_cls = fields.String()
     vendor = fields.String()
     name = fields.String()
     subsystem_vendor = fields.String()
     subsystem_name = fields.String()
-    rev = fields.String()
-    progif = fields.String()
+    rev = fields.Nullable(fields.String())
+    progif = fields.Nullable(fields.String())
 
 
 class PCIDevices(Model):
