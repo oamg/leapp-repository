7ede6ea55b31921f26e691bc7d1ee725e9c72bc6
Arthur Mello <amello@redhat.com>
2019-03-01 18:06:55 +0000

    Enable repositories based on PES data
    - Refactor actor to use library
    - Add rhel8-CRB abd rhel8-Buildroot to blacklist
    - Remove packages from to_install list based on blacklisted repositories and show a message
    - Display skipped packages by blacklisted/unmapped repositories
    - Handle PES Events using V2 structure
    - Report error if using outdated PES data
    - Map repositories from PES to RHSM repo id
    - Add unit tests

---
+++ repos/system_upgrade/el7toel8/models/repositoriessetuptasks.py
@@ -0,0 +1,15 @@
+from leapp.models import Model, fields
+from leapp.topics import SystemFactsTopic
+
+
+class RepositoriesSetupTasks(Model):
+    """
+    Information about repositories that must be managed in order to complete upgrade process.
+
+    'to_enable' field consists of a list of repositories that should be enabled in order to complete
+    upgrade process. This information should be processed by an actor dedicated to manage
+    repositories.
+    """
+    topic = SystemFactsTopic
+
+    to_enable = fields.List(fields.String(), default=[])
