1e6bbaa9164d9bee197e5b03739bf8141dcff4f5
Petr Stodůlka <xstodu05@gmail.com>
2019-04-02 12:11:35 +0200

    Handle the yum configuration files (Fixes #19) (#115)
    RPM cannot handle replace of directories by symlinks by default
    without the %pretrans scriptlet. As yum package is packaged wrong,
    we have to workround that by actors so we will be able to keep
    installed yum package.
    
    Current solution handle the yum configuration files to replace
    relevant directories by symlinks as expected on target system. This
    change is done on two places:
    - inside the container when RPM transactions is calculated and rpms
      are downloaded (the preupgradeupgradetransaction actor)
      - system is unaffected by this change
    - before the upgrade itself (PreparationPhase) in the prepareyumconfig
      actor.
    
    The important code handling that migration is stored in the tools
    directory of the el7toel8 repository. See the handleyumconfig tool.

---
+++ repos/system_upgrade/el7toel8/actors/prepareyumconfig/actor.py
@@ -0,0 +1,27 @@
+from leapp.actors import Actor
+from leapp.tags import IPUWorkflowTag, PreparationPhaseTag
+from leapp.libraries.stdlib import run, CalledProcessError
+
+
+class Prepareyumconfig(Actor):
+    """
+    Handle migration of the yum configuration files
+    
+    RPM cannot handle replacement of directories by symlinks by default
+    without the %pretrans scriptlet. As yum package is packaged wrong,
+    we have to workround that by migration of the yum configuration files
+    before the rpm transaction is processed.
+    """
+
+    name = 'prepareyumconfig'
+    consumes = ()
+    produces = ()
+    tags = (IPUWorkflowTag, PreparationPhaseTag)
+
+    def process(self):
+        try:
+            run(['handleyumconfig'])
+        except CalledProcessError as e:
+            raise StopActorExecutionError(
+                    'Migration of yum configuration failed.',
+                    details={'details': str(e)})
