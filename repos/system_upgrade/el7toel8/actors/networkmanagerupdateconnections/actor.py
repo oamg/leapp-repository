6fbc79869baf2e336daf7c02a78c3a258a3371ed
Beniamino Galvani <b.galvani@gmail.com>
2019-04-10 14:59:26 +0200

    Rework the NetworkManagerUpdateClientId actor (v2) (#117)
    * Rename the networkmanagerupdateclientid actor
    
    It seems better to use a more generic name, so that in the future the
    actor could also handle other changes to connections.
    
    * Fix the networkmanagerupdateconnections actor
    
    The helper script to update client-ids needs a python D-Bus library
    which is only available for Python 3 in the new system. Therefore,
    explicitly run the script through the python3 interpreter.
    
    Also, the script needs to be called when NetworkManager is running and
    so move the actor to the FirstBoot phase.

--- repos/system_upgrade/el7toel8/actors/networkmanagerupdateconnections/actor.py
+++ repos/system_upgrade/el7toel8/actors/networkmanagerupdateconnections/actor.py
@@ -1,12 +1,12 @@
 from leapp.actors import Actor
 from leapp.libraries.stdlib import CalledProcessError, run
 from leapp.models import NetworkManagerConfig
-from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
+from leapp.tags import FirstBootPhaseTag, IPUWorkflowTag
 
 
-class NetworkManagerUpdateClientId(Actor):
+class NetworkManagerUpdateConnections(Actor):
     """
-    Updates DHCP client-ids during Upgrade process.
+    Update NetworkManager connections.
     
     When using dhcp=dhclient on Red Hat Enterprise Linux 7, a non-hexadecimal client-id (a string)
     is sent on the wire as is (i.e. the first character is the 'type' as per RFC 2132 section
@@ -14,23 +14,21 @@ class NetworkManagerUpdateClientId(Actor):
     preserve behavior on upgrade, we convert client-ids to the hexadecimal form.
     """
 
-    name = 'network_manager_update_client_id'
+    name = 'network_manager_update_connections'
     consumes = (NetworkManagerConfig,)
     produces = ()
-    tags = (ApplicationsPhaseTag, IPUWorkflowTag)
+    tags = (FirstBootPhaseTag, IPUWorkflowTag)
 
     def process(self):
         for nm_config in self.consume(NetworkManagerConfig):
             if nm_config.dhcp != '' and nm_config.dhcp != 'dhclient':
-                self.log.info('DHCP client is {}, nothing to do'.format(nm_config.dhcp))
+                self.log.info('DHCP client is "{}", nothing to do'.format(nm_config.dhcp))
                 return
 
             try:
-                r = run(['nm-update-client-ids.py'], split=True)['stdout']
+                r = run(['/usr/bin/python3', 'tools/nm-update-client-ids.py'])['stdout']
+                self.log.info('Updated client-ids: {}'.format(r))
             except (OSError, CalledProcessError) as e:
-                self.log.warning('Error calling script: {}'.format(e))
-                return
+                self.log.warning('Error calling nm-update-client-ids script: {}'.format(e))
 
-            self.log.info('Updated client-ids: {}'.format(r))
-
-
+            break
