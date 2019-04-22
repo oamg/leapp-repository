01a01216e7c2675e2af2e6baeeab26c7085e5200
Arthur Mello <amello@redhat.com>
2019-04-22 22:36:07 +0000

    Notify user if some enabled repository is unknown to Leapp
    - Fix check for enabled repos at SystemFactsActor

--- repos/system_upgrade/el7toel8/actors/rpmscanner/actor.py
+++ repos/system_upgrade/el7toel8/actors/rpmscanner/actor.py
@@ -1,5 +1,8 @@
+import yum
+
 from leapp.actors import Actor
 from leapp.libraries.common.rpms import get_installed_rpms
+from leapp.libraries.stdlib import run
 from leapp.models import InstalledRPM, RPM
 from leapp.tags import IPUWorkflowTag, FactsPhaseTag
 
@@ -16,8 +19,19 @@ class RpmScanner(Actor):
     produces = (InstalledRPM,)
     tags = (IPUWorkflowTag, FactsPhaseTag)
 
+    def get_package_repository_data(self):
+        """ Return dictionary mapping package name with repository from which it was installed """
+        yum_base = yum.YumBase()
+        pkg_repos = {}
+        for pkg in yum_base.doPackageLists().installed:
+            pkg_repos[pkg.name] = pkg.ui_from_repo.lstrip('@')
+
+        return pkg_repos
+
+
     def process(self):
         output = get_installed_rpms()
+        pkg_repos = self.get_package_repository_data()
 
         result = InstalledRPM()
         for entry in output:
@@ -25,6 +39,7 @@ class RpmScanner(Actor):
             if not entry:
                 continue
             name, version, release, epoch, packager, arch, pgpsig = entry.split('|')
+            repository = pkg_repos.get(name, '')
             result.items.append(RPM(
                 name=name,
                 version=version,
@@ -32,5 +47,6 @@ class RpmScanner(Actor):
                 packager=packager,
                 arch=arch,
                 release=release,
-                pgpsig=pgpsig))
+                pgpsig=pgpsig,
+                repository=repository))
         self.produce(result)
---
+++ repos/system_upgrade/el7toel8/actors/usedrepositoriesscanner/tests/test_usedrepositoriesscanner.py
@@ -0,0 +1,52 @@
+from leapp.snactor.fixture import current_actor_context
+from leapp.models import RPM, InstalledRedHatSignedRPM, RepositoryFile, RepositoryData, \
+    RepositoriesFacts, UsedRepositories
+
+
+def get_sample_rpm(name, repository):
+    return RPM(
+        name=name,
+        epoch='1',
+        packager='Red Hat Inc.',
+        version='0.0.1',
+        release='1.el7',
+        arch='x86_64',
+        pgpsig='RSA/SHA256, Mon 01 Jan 1970 00:00:00 AM UTC, Key ID 0123456789abcdef',
+        repository=repository)
+
+
+def get_sample_installed_pkgs(pkgs):
+    return InstalledRedHatSignedRPM(items=[get_sample_rpm(*p) for p in pkgs])
+
+
+def get_sample_repository(repoid, name):
+    return RepositoryFile(file='/etc/yum.d/sample.repo', data=[RepositoryData(
+        repoid=repoid,
+        name=name,
+        enabled=True)])
+
+
+def get_sample_repositories(repos):
+    return RepositoriesFacts(
+        repositories=[get_sample_repository(*r) for r in repos])
+
+
+def test_actor_execution(current_actor_context):
+    installed = get_sample_installed_pkgs([
+        ('pkg1', 'rhel-7-server-rpms'),
+        ('pkg2', 'rhel-7-server-rpms')])
+
+    repos = get_sample_repositories([
+        ('rhel-7-server-rpms', 'RHEL 7 Server'),
+        ('rhel-7-unused-rpms', 'RHEL 7 Unused')])
+
+
+    current_actor_context.feed(installed)
+    current_actor_context.feed(repos)
+    current_actor_context.run()
+    assert current_actor_context.consume(UsedRepositories)
+    used_repos = current_actor_context.consume(UsedRepositories)[0].repositories
+    assert len(used_repos) == 1
+    assert used_repos[0].repository == 'rhel-7-server-rpms'
+    assert len(used_repos[0].packages) == 2
+    assert used_repos[0].packages == ['pkg1', 'pkg2']
