1c2a92bc2c9b37f5692adf25a5d22a0d60421d7f
Ondřej Lysoněk <olysonek@redhat.com>
2019-05-02 09:14:40 +0200

    Add an actor for checking the vsftpd configuration is supported
    tcp_wrappers support has been removed in RHEL-8, so warn the user if
    they appear to have been using it. In such a case, the upgrade is in
    fact inhibited, so that the resulting upgraded system is not insecure.
    
    v2: Used report_with_links instead of report_generic, improved format
        of the list of bad configs. Thanks to Petr Stodůlka for the patch.
    v3: Simplified check_config_supported, extended test coverage.

---
+++ repos/system_upgrade/el7toel8/actors/vsftpdconfigcheck/libraries/library.py
@@ -0,0 +1,20 @@
+from leapp.libraries.common.reporting import report_with_links
+from leapp.libraries.common.tcpwrappersutils import config_applies_to_daemon
+
+
+def check_config_supported(tcpwrap_facts, vsftpd_facts):
+    bad_configs = [config.path for config in vsftpd_facts.configs if config.tcp_wrappers]
+    if len(bad_configs) > 0 and config_applies_to_daemon(tcpwrap_facts, 'vsftpd'):
+        list_separator_fmt = '\n    - '
+        report_with_links(title='Unsupported vsftpd configuration',
+                          summary=('tcp_wrappers support has been removed in RHEL-8. '
+                                   'Some configuration files set the tcp_wrappers option to true and '
+                                   'there is some vsftpd-related configuration in /etc/hosts.deny '
+                                   'or /etc/hosts.allow. Please migrate it manually. '
+                                   'The list of problematic configuration files:{}{}'
+                                  ).format(list_separator_fmt,
+                                           list_separator_fmt.join(bad_configs)),
+                          links=[{'title': 'Replacing TCP Wrappers in RHEL 8',
+                                  'href': 'https://access.redhat.com/solutions/3906701'}],
+                          severity='high',
+                          flags=['inhibitor'])
---
+++ repos/system_upgrade/el7toel8/actors/vsftpdconfigcheck/tests/test_library.py
@@ -0,0 +1,81 @@
+from leapp.models import DaemonList, TcpWrappersFacts, VsftpdConfig, VsftpdFacts
+from leapp.reporting import Report
+from leapp.snactor.fixture import current_actor_context
+
+
+def test_actor_with_unsupported_tcpwrap_and_vsftpd_config(current_actor_context):
+    config1 = VsftpdConfig(path='/etc/vsftpd/foo.conf', tcp_wrappers=False)
+    config2 = VsftpdConfig(path='/etc/vsftpd/bar.conf', tcp_wrappers=True)
+    vsftpd_facts = VsftpdFacts(configs=[config1, config2])
+    daemon_list = DaemonList(value=['vsftpd'])
+    tcpwrap_facts = TcpWrappersFacts(daemon_lists=[daemon_list])
+
+    current_actor_context.feed(vsftpd_facts)
+    current_actor_context.feed(tcpwrap_facts)
+    current_actor_context.run()
+    report = current_actor_context.consume(Report)[0]
+
+    assert 'inhibitor' in report.flags
+    assert 'foo.conf' not in report.detail['summary']
+    assert 'bar.conf' in report.detail['summary']
+
+
+def test_actor_with_unsupported_tcpwrap_multiple_unsupported_vsftpd_configs(current_actor_context):
+    config1 = VsftpdConfig(path='/etc/vsftpd/foo.conf', tcp_wrappers=True)
+    config2 = VsftpdConfig(path='/etc/vsftpd/bar.conf', tcp_wrappers=False)
+    config3 = VsftpdConfig(path='/etc/vsftpd/goo.conf', tcp_wrappers=True)
+    vsftpd_facts = VsftpdFacts(configs=[config1, config2, config3])
+    daemon_list = DaemonList(value=['vsftpd'])
+    tcpwrap_facts = TcpWrappersFacts(daemon_lists=[daemon_list])
+
+    current_actor_context.feed(vsftpd_facts)
+    current_actor_context.feed(tcpwrap_facts)
+    current_actor_context.run()
+    report = current_actor_context.consume(Report)[0]
+
+    assert 'inhibitor' in report.flags
+    assert 'foo.conf' in report.detail['summary']
+    assert 'bar.conf' not in report.detail['summary']
+    assert 'goo.conf' in report.detail['summary']
+
+
+def test_actor_with_unsupported_tcpwrap_config(current_actor_context):
+    config1 = VsftpdConfig(path='/etc/vsftpd/foo.conf', tcp_wrappers=False)
+    config2 = VsftpdConfig(path='/etc/vsftpd/bar.conf', tcp_wrappers=None)
+    vsftpd_facts = VsftpdFacts(configs=[config1, config2])
+    daemon_list = DaemonList(value=['vsftpd'])
+    tcpwrap_facts = TcpWrappersFacts(daemon_lists=[daemon_list])
+
+    current_actor_context.feed(vsftpd_facts)
+    current_actor_context.feed(tcpwrap_facts)
+    current_actor_context.run()
+
+    assert not current_actor_context.consume(Report)
+
+
+def test_actor_with_unsupported_vsftpd_config(current_actor_context):
+    config1 = VsftpdConfig(path='/etc/vsftpd/foo.conf', tcp_wrappers=False)
+    config2 = VsftpdConfig(path='/etc/vsftpd/bar.conf', tcp_wrappers=True)
+    vsftpd_facts = VsftpdFacts(configs=[config1, config2])
+    daemon_list = DaemonList(value=['all', 'except', 'vsftpd'])
+    tcpwrap_facts = TcpWrappersFacts(daemon_lists=[daemon_list])
+
+    current_actor_context.feed(vsftpd_facts)
+    current_actor_context.feed(tcpwrap_facts)
+    current_actor_context.run()
+
+    assert not current_actor_context.consume(Report)
+
+
+def test_actor_with_supported_tcpwrap_and_vsftpd_config(current_actor_context):
+    config1 = VsftpdConfig(path='/etc/vsftpd/foo.conf', tcp_wrappers=False)
+    config2 = VsftpdConfig(path='/etc/vsftpd/bar.conf', tcp_wrappers=False)
+    vsftpd_facts = VsftpdFacts(configs=[config1, config2])
+    daemon_list = DaemonList(value=['all', 'except', 'vsftpd'])
+    tcpwrap_facts = TcpWrappersFacts(daemon_lists=[daemon_list])
+
+    current_actor_context.feed(vsftpd_facts)
+    current_actor_context.feed(tcpwrap_facts)
+    current_actor_context.run()
+
+    assert not current_actor_context.consume(Report)
