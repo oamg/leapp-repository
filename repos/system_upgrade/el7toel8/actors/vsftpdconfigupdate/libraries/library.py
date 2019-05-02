d8f12ad5cb00fb7df9b87d0ab794e44d815c364d
Ondřej Lysoněk <olysonek@redhat.com>
2019-05-02 09:19:43 +0200

    Add an actor for modifying the vsftpd config file
    This actor changes the vsftpd config files so that they still work in
    RHEL-8, and so that the effective configuration is as close to the
    original as possible.
    
    Namely, this actor explicitly sets the strict_ssl_read_eof option in
    config files, if it was not set, because the default value has changed
    between RHEL-7 and RHEL-8.
    
    The actor also restores the value of the anonymous_enable option, if it
    changed during the upgrade - this can happen if the
    /etc/vsftpd/vsftpd.conf config file was unmodified on the source system,
    and therefore the file got replaced with the new version during the RPM
    upgrade.
    
    The actor also sets the tcp_wrappers option to "NO", as tcp_wrappers is
    not supported in RHEL-8.
    
    v2: Renamed private functions to begin with an underscore.
    v3: Fix creating IOError in tests.

---
+++ repos/system_upgrade/el7toel8/actors/vsftpdconfigupdate/libraries/library.py
@@ -0,0 +1,79 @@
+import re
+
+from leapp.libraries.common.vsftpdutils import get_default_config_hash, \
+                                               STRICT_SSL_READ_EOF, TCP_WRAPPERS, \
+                                               VSFTPD_DEFAULT_CONFIG_PATH
+from leapp.libraries.stdlib import api
+
+ANONYMOUS_ENABLE = 'anonymous_enable'
+
+
+class FileOperations(object):
+    def read(self, path):
+        with open(path, 'r') as f:
+            return f.read()
+
+    def write(self, path, content):
+        with open(path, 'w') as f:
+            f.write(content)
+
+
+def _replace_in_config(config_lines, option, value):
+    res = []
+    for line in config_lines:
+        if re.match(r'^\s*' + option, line) is None:
+            res.append(line)
+        else:
+            res.append('# Commented out by Leapp:')
+            res.append('#' + line)
+    if value is not None:
+        res.append('# Added by Leapp:')
+        res.append('%s=%s' % (option, value))
+    return res
+
+
+def _restore_default_config_file(fileops=FileOperations()):
+    try:
+        content = fileops.read(VSFTPD_DEFAULT_CONFIG_PATH)
+    except IOError as e:
+        api.current_logger().warning('Failed to read vsftpd configuration file: %s' % e)
+        return
+    lines = content.split('\n')
+    lines = _replace_in_config(lines, ANONYMOUS_ENABLE, 'YES')
+    content = '\n'.join(lines)
+    content += '\n'
+    fileops.write(VSFTPD_DEFAULT_CONFIG_PATH, content)
+
+
+def _migrate_config(config, fileops=FileOperations()):
+    if not config.tcp_wrappers and config.strict_ssl_read_eof is not None:
+        return
+    try:
+        content = fileops.read(config.path)
+    except IOError as e:
+        api.current_logger().warning('Failed to read vsftpd configuration file %s: %s'
+                                     % (config.path, e))
+        return
+    lines = content.split('\n')
+    if config.tcp_wrappers:
+        lines = _replace_in_config(lines, TCP_WRAPPERS, 'NO')
+    if config.strict_ssl_read_eof is None:
+        lines = _replace_in_config(lines, STRICT_SSL_READ_EOF, 'NO')
+    content = '\n'.join(lines)
+    content += '\n'
+    try:
+        fileops.write(config.path, content)
+    except IOError as e:
+        api.current_logger().warning('Failed to write vsftpd configuration file %s: %s'
+                                     % (config.path, e))
+
+
+def migrate_configs(facts, fileops=FileOperations()):
+    if facts.default_config_hash is not None:
+        new_hash = get_default_config_hash(read_func=fileops.read)
+        # If the default config file was unmodified, it got replaced during the RPM upgrade,
+        # so we have to change it back.
+        if facts.default_config_hash != new_hash:
+            _restore_default_config_file(fileops=fileops)
+    for config in facts.configs:
+        _migrate_config(config, fileops=fileops)
