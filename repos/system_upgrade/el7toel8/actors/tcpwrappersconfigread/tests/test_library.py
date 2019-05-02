8663abe8fd5a55b5eaeafdb0313208d58acb40ad
Ondřej Lysoněk <olysonek@redhat.com>
2019-05-02 09:06:51 +0200

    Add an actor for reading tcp_wrappers config files
    This adds an actor for reading tcp_wrappers config files (/etc/hosts.allow
    and /etc/hosts.deny). It also adds a function to check whether the
    configuration has any effect on the specified daemon.
    
    This will be used by (at least) a vsftpd actor. In the vsftpd actor, we
    will inhibit the upgrade if tcp_wrappers=YES in /etc/vsftpd/vsftpd.conf
    and if there's some configuration applicable to vsftpd in the
    tcp_wrappers config files, so that the upgraded system is not insecure.
    
    v2: Simplified get_lines(), config_applies_to_daemon()
    v3: Simplified get_daemon_list_in_line() and get_daemon_lists_in_file(),
        extended test coverage.
    v4: Renamed private functions to begin with an underscore.
    v5: Improved the public API documentation.
    v6: Fixed creating IOError in tests.

---
+++ repos/system_upgrade/el7toel8/actors/tcpwrappersconfigread/tests/test_library.py
@@ -0,0 +1,168 @@
+import errno
+
+from leapp.libraries.actor import library
+from leapp.libraries.common.testutils import make_IOError
+
+
+class MockFileReader(object):
+    def __init__(self):
+        self.files = {}
+        self.files_read = {}
+        self.read_called = 0
+
+    def _increment_read_counters(self, path):
+        self.read_called += 1
+        self.files_read.setdefault(path, 0)
+        self.files_read[path] += 1
+
+    def read(self, path):
+        self._increment_read_counters(path)
+        try:
+            return self.files[path]
+        except KeyError:
+            raise make_IOError(errno.ENOENT)
+
+
+def test_get_daemon_list_in_line_simple():
+    line = 'vsftpd : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['vsftpd']
+
+
+def test_get_daemon_list_in_line_multiple():
+    line = 'vsftpd, sendmail : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['vsftpd', 'sendmail']
+
+    line = 'ALL EXCEPT sendmail : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['ALL', 'EXCEPT', 'sendmail']
+
+    # different separators
+    line = 'vsftpd,sendmail : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['vsftpd', 'sendmail']
+
+    line = 'vsftpd\tsendmail : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['vsftpd', 'sendmail']
+
+    line = 'vsftpd, \t sendmail : 192.168.2.*'
+    daemon_list = library._get_daemon_list_in_line(line)
+    assert daemon_list.value == ['vsftpd', 'sendmail']
+
+
+def test_get_daemon_list_in_line_malformed():
+    line = 'foo'
+    daemon_list = library._get_daemon_list_in_line(line)
+    # tcp_wrappers actually ignores lines like this, but there's no harm in being
+    # over-sensitive here.
+    assert daemon_list.value == ['foo']
+
+
+def test_get_lines_empty():
+    content = ''
+    lines = library._get_lines(content)
+    assert lines == ['']
+
+
+def test_get_lines_simple():
+    content = 'vsftpd : 192.168.2.*\n' \
+              'ALL : 192.168.1.*\n'
+    lines = library._get_lines(content)
+    assert lines == content.split('\n')
+
+
+def test_get_lines_continued_line():
+    content = 'vsftpd : 192.168\\\n.2.*'
+    lines = library._get_lines(content)
+    expected = ['vsftpd : 192.168.2.*']
+    assert lines == expected
+
+
+def test_get_lines_backslash_followed_by_whitespace():
+    content = 'foo \\ \n' \
+              'this is not a continuation line'
+    lines = library._get_lines(content)
+    expected = ['foo \\ ', 'this is not a continuation line']
+    assert lines == expected
+
+
+def test_get_lines_continued_comment():
+    content = '# foo \\\n' \
+              'this is still a comment'
+    lines = library._get_lines(content)
+    expected = ['# foo this is still a comment']
+    assert lines == expected
+
+
+def test_is_comment():
+    assert library._is_comment('') is True
+    assert library._is_comment('  ') is True
+    assert library._is_comment('# foo') is True
+    assert library._is_comment('#') is True
+    assert library._is_comment(' # foo') is False
+    assert library._is_comment('foo') is False
+    assert library._is_comment(' foo') is False
+
+
+def test_get_daemon_lists_in_file():
+    path = '/etc/hosts.allow'
+    reader = MockFileReader()
+    reader.files[path] = 'vsftpd : 192.168.2.*\n' \
+                         'ALL : 192.168.1.*\n'
+
+    daemon_lists = library._get_daemon_lists_in_file(path, read_func=reader.read)
+
+    num_lines = 2
+    assert len(daemon_lists) == num_lines
+    assert daemon_lists[0].value == ['vsftpd']
+    assert daemon_lists[1].value == ['ALL']
+
+
+def test_get_daemon_lists_in_file_nonexistent():
+    reader = MockFileReader()
+    daemon_lists = library._get_daemon_lists_in_file('/etc/hosts.allow', read_func=reader.read)
+    assert len(daemon_lists) == 0
+
+
+def test_get_daemon_lists():
+    reader = MockFileReader()
+    reader.files['/etc/hosts.allow'] = 'vsftpd : 192.168.2.*\n' \
+                                       'ALL : 192.168.1.*\n'
+    reader.files['/etc/hosts.deny'] = 'sendmail : 192.168.2.*\n'
+
+    daemon_lists = library._get_daemon_lists(read_func=reader.read)
+
+    num_lines = 3
+    assert len(daemon_lists) == num_lines
+    assert daemon_lists[0].value == ['vsftpd']
+    assert daemon_lists[1].value == ['ALL']
+    assert daemon_lists[2].value == ['sendmail']
+
+
+def test_get_daemon_lists_nonexistent_config():
+    reader = MockFileReader()
+    daemon_lists = library._get_daemon_lists(read_func=reader.read)
+    assert len(daemon_lists) == 0
+
+
+def test_get_tcp_wrappers_facts():
+    reader = MockFileReader()
+    reader.files['/etc/hosts.allow'] = 'vsftpd : 192.168.2.*\n' \
+                                       'ALL : 192.168.1.*\n'
+    reader.files['/etc/hosts.deny'] = 'sendmail : 192.168.2.*\n'
+
+    facts = library.get_tcp_wrappers_facts(read_func=reader.read)
+
+    num_lines = 3
+    assert len(facts.daemon_lists) == num_lines
+    assert facts.daemon_lists[0].value == ['vsftpd']
+    assert facts.daemon_lists[1].value == ['ALL']
+    assert facts.daemon_lists[2].value == ['sendmail']
+
+
+def test_get_tcp_wrappers_facts_nonexistent_config():
+    reader = MockFileReader()
+    facts = library.get_tcp_wrappers_facts(read_func=reader.read)
+    assert len(facts.daemon_lists) == 0
