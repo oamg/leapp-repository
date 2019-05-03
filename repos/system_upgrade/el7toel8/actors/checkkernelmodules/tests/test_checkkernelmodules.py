6668dcb4b256fc60528880d13063a03e2f146e5e
Tomas Meszaros <exo@tty.sk>
2019-05-03 15:28:37 +0200

    added tests

---
+++ repos/system_upgrade/el7toel8/actors/checkkernelmodules/tests/test_checkkernelmodules.py
@@ -0,0 +1,25 @@
+from leapp.snactor.fixture import current_actor_context
+from leapp.models import ActiveKernelModulesFacts, ActiveKernelModule, KernelModuleParameter
+from leapp.reporting import Report
+
+
+def test_actor_with_missing_modules(current_actor_context):
+    """ Tests CheckKernelModule actor by feeding it kernel modules that
+    are not available on the RHEL8 system. Actor should produce inhibior.
+    """
+    modules = [ActiveKernelModule(filename="virtio", parameters=[]),
+               ActiveKernelModule(filename="aoe", parameters=[])]
+    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=modules))
+    current_actor_context.run()
+    assert "inhibitor" in current_actor_context.consume(Report)[0].flags
+
+
+def test_actor_without_missing_modules(current_actor_context):
+    """ Tests CheckKernelModule actor by feeding it kernel modules that
+    are available on the RHEL8 system. Actor should NOT produce any report.
+    """
+    modules = [ActiveKernelModule(filename="foobar", parameters=[]),
+               ActiveKernelModule(filename="barfoo", parameters=[])]
+    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=modules))
+    current_actor_context.run()
+    assert len(current_actor_context.consume(Report)) == 0
