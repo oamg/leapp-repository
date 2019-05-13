4a8044f91ddf86955e209138a82a165b070641dc
Tomas Meszaros <exo@tty.sk>
2019-05-13 10:58:54 +0200

    fixed nested docstring syntax error

--- repos/system_upgrade/el7toel8/actors/checkkernelmodules/tests/test_checkkernelmodules.py
+++ repos/system_upgrade/el7toel8/actors/checkkernelmodules/tests/test_checkkernelmodules.py
@@ -6,17 +6,16 @@ from leapp.reporting import Report
 # FIXME:
 # - Enable this test after we handle the missing modules. Otherwise, this test
 #    will always fail (when each missing module is whitelisted).
-"""
-def test_actor_with_missing_modules(current_actor_context):
-    """ Tests CheckKernelModule actor by feeding it kernel modules that
-    are not available on the RHEL8 system. Actor should produce inhibior.
-    """
-    modules = [ActiveKernelModule(filename="virtio", parameters=[]),
-               ActiveKernelModule(filename="aoe", parameters=[])]
-    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=modules))
-    current_actor_context.run()
-    assert "inhibitor" in current_actor_context.consume(Report)[0].flags
-"""
+
+#def test_actor_with_missing_modules(current_actor_context):
+#    """ Tests CheckKernelModule actor by feeding it kernel modules that
+#    are not available on the RHEL8 system. Actor should produce inhibior.
+#    """
+#    modules = [ActiveKernelModule(filename="virtio", parameters=[]),
+#               ActiveKernelModule(filename="aoe", parameters=[])]
+#    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=modules))
+#    current_actor_context.run()
+#    assert "inhibitor" in current_actor_context.consume(Report)[0].flags
 
 
 def test_actor_without_missing_modules(current_actor_context):
