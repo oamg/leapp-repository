bbdf29a2f595cb0c69779af3a6e4afb128933abf
Jaroslav Škarvada <jskarvad@redhat.com>
2019-05-23 14:22:38 +0200

    Added actor for postfix migration
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/checkpostfix/actor.py
@@ -0,0 +1,57 @@
+from leapp.actors import Actor
+from leapp.libraries.common.reporting import report_generic
+from leapp.models import InstalledRedHatSignedRPM
+from leapp.reporting import Report
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+class CheckPostfix(Actor):
+    """
+    Check if postfix is installed, check whether configuration update is needed.
+    """
+
+    name = 'check_postfix'
+    consumes = (InstalledRedHatSignedRPM,)
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        for fact in self.consume(InstalledRedHatSignedRPM):
+            for rpm in fact.items:
+                if rpm.name == 'postfix':
+                    report_generic(
+                        title='Postfix has incompatible changes in the next major version',
+                        summary='Postfix 3.x has so called "compatibility safety net" that runs Postfix programs '
+                                'with backwards-compatible default settings. It will log a warning whenever '
+                                'backwards-compatible default setting may be required for continuity of service. '
+                                'Based on this logging the system administrator can decide if any '
+                                'backwards-compatible settings need to be made permanent in main.cf or master.cf, '
+                                'before turning off the backwards-compatibility safety net.\n'
+                                'The backward compatibility safety net is by default turned off in Red Hat '
+                                'Enterprise Linux 8.\n'
+                                'It can be turned on by running:  "postconf -e compatibility_level=0\n'
+                                'It can be turned off by running: "postconf -e compatibility_level=2\n\n'
+                                'In the Postfix MySQL database client, the default "option_group" value has changed '
+                                'to "client", i.e. it now reads options from the [client] group from the MySQL '
+                                'configuration file. To disable it, set "option_group" to the empty string.\n\n'
+                                'The postqueue command no longer forces all message arrival times to be reported '
+                                'in UTC. To get the old behavior, set TZ=UTC in main.cf:import_environment.\n\n'
+                                'Postfix 3.2 enables elliptic curve negotiation. This changes the default '
+                                'smtpd_tls_eecdh_grade setting to "auto", and introduces a new parameter '
+                                '"tls_eecdh_auto_curves" with the names of curves that may be negotiated.\n\n'
+                                'The "master.cf" chroot default value has changed from "y" (yes) to "n" (no). '
+                                'This applies to master.cf services where chroot field is not explicitly '
+                                'specified.\n\n'
+                                'The "append_dot_mydomain" default value has changed from "yes" to "no". You may '
+                                'need changing it to "yes" if senders cannot use complete domain names in e-mail '
+                                'addresses.\n\n'
+                                'The "relay_domains" default value has changed from "$mydestination" to the empty '
+                                'value. This could result in unexpected "Relay access denied" errors or ETRN errors, '
+                                'because now will postfix by default relay only for the localhost.\n\n'
+                                'The "mynetworks_style" default value has changed from "subnet" to "host". '
+                                'This parameter is used to implement the "permit_mynetworks" feature. The change '
+                                'could result in unexpected "access denied" errors, because postfix will now by '
+                                'default trust only the local machine, not the remote SMTP clients on the '
+                                'same IP subnetwork.\n',
+                        severity='low')
+                    return
---
+++ repos/system_upgrade/el7toel8/actors/checkpostfix/tests/component_test.py
@@ -0,0 +1,31 @@
+from leapp.models import InstalledRedHatSignedRPM, RPM
+from leapp.reporting import Report
+from leapp.snactor.fixture import current_actor_context
+
+
+RH_PACKAGER = 'Red Hat, Inc. <http://bugzilla.redhat.com/bugzilla>'
+
+with_postfix = [
+    RPM(name='grep', version='0.1', release='1.sm01', epoch='1', packager=RH_PACKAGER, arch='noarch',
+        pgpsig='RSA/SHA256, Mon 01 Jan 1970 00:00:00 AM -03, Key ID 199e2f91fd431d51'),
+    RPM(name='postfix', version='0.1', release='1.sm01', epoch='1', packager=RH_PACKAGER, arch='noarch',
+        pgpsig='RSA/SHA256, Mon 01 Jan 1970 00:00:00 AM -03, Key ID 199e2f91fd431d51')]
+
+without_postfix = [
+    RPM(name='grep', version='0.1', release='1.sm01', epoch='1', packager=RH_PACKAGER, arch='noarch',
+        pgpsig='RSA/SHA256, Mon 01 Jan 1970 00:00:00 AM -03, Key ID 199e2f91fd431d51'),
+    RPM(name='sed', version='0.1', release='1.sm01', epoch='1', packager=RH_PACKAGER, arch='noarch',
+        pgpsig='RSA/SHA256, Mon 01 Jan 1970 00:00:00 AM -03, Key ID 199e2f91fd431d51')]
+
+def create_modulesfacts(installed_rpm):
+    return InstalledRedHatSignedRPM(items=installed_rpm)
+
+def test_actor_without_postfix_package(current_actor_context):
+    current_actor_context.feed(create_modulesfacts(installed_rpm=without_postfix))
+    current_actor_context.run()
+    assert not current_actor_context.consume(Report)
+
+def test_actor_with_postfix_package(current_actor_context):
+    current_actor_context.feed(create_modulesfacts(installed_rpm=with_postfix))
+    current_actor_context.run()
+    assert len(current_actor_context.consume(Report)) > 0
