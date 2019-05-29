980283fe1c1ed692d5bf136f5e2c9b9d3affd212
Michal Bocek <mbocek@redhat.com>
2019-05-29 19:21:14 +0200

    Add RHEL 8 Supplementary repo to PES repo mapping
    - this will make sure that any PES event pointing to the RHEL 8
    Supplementary repo will get processed correctly and that the repo will
    get enabled during the upgrade process
    
    - related: https://bugzilla.redhat.com/show_bug.cgi?id=1621775

--- repos/system_upgrade/el7toel8/actors/peseventsscanner/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/peseventsscanner/libraries/library.py
@@ -13,7 +13,8 @@ from leapp.models import (InstalledRedHatSignedRPM, PESRpmTransactionTasks,
 REPOSITORIES_MAPPING = {
     'rhel8-appstream': 'rhel-8-for-x86_64-appstream-rpms',
     'rhel8-baseos': 'rhel-8-for-x86_64-baseos-rpms',
-    'rhel8-crb': 'codeready-builder-for-rhel-8-x86_64-rpms'}
+    'rhel8-crb': 'codeready-builder-for-rhel-8-x86_64-rpms',
+    'rhel8-supplementary': 'rhel-8-for-x86_64-supplementary-rpms'}
 
 Event = namedtuple('Event', ['action',   # A string representing an event type (see EVENT_TYPES)
                              'in_pkgs',  # A dictionary with packages in format {<pkg_name>: <repository>}
@@ -30,9 +31,9 @@ def pes_events_scanner(pes_json_filepath):
     events = get_events(pes_json_filepath)
     add_output_pkgs_to_transaction_conf(transaction_configuration, events)
     filtered_events = get_events_for_installed_pkgs_only(events, installed_pkgs)
-    to_install, to_remove = process_events(filtered_events, installed_pkgs)
-    filter_out_transaction_conf_pkgs(to_install, to_remove, transaction_configuration)
-    produce_messages(to_install, to_remove)
+    tasks = process_events(filtered_events, installed_pkgs)
+    filter_out_transaction_conf_pkgs(tasks, transaction_configuration)
+    produce_messages(tasks)
 
 
 def get_installed_pkgs():
@@ -193,47 +194,63 @@ def is_at_least_one_event_input_pkg_installed(installed_rh_pkgs, event_in_pkgs):
 
 def process_events(events, installed_pkgs):
     """
-    Process PES events to get lists of pkgs to install or to remove.
-
-    Compare the "to install" list to the already installed pkgs to not mandate installation of what is already
-    installed. Also, filter out installation of pkgs in blacklisted repos.
+    Process PES events to get lists of pkgs to keep, to install and to remove.
 
     :param events: List of Event tuples, not including those events with their "input" packages not installed
     :param installed_pkgs: Set of names of the installed Red Hat-signed packages
-    :return: A tuple with two dicts {<pkg_name>: <repository>} - one with pkgs to install and one with pkgs to remove
+    :return: A dict with three dicts holding pkgs to keep, to install and to remove
     """
-    # Dicts in format {<pkg_name>: <repository>}
-    to_install = {}
-    to_remove = {}
+
+    tasks = {  # Contains dicts in format {<pkg_name>: <repository>}
+        'to_keep': {},
+        'to_install': {},
+        'to_remove': {}}
 
     for event in events:
-        if event.action in ('Deprecated', 'Moved', 'Present'):
-            # No need to mandate installation of these. If they are installed on the system, the default in the dnf
-            # plugin is to add them to the "to upgrade" group, which is correct in this case.
-            continue
+        if event.action in ('Deprecated', 'Present'):
+            # Add these packages to to_keep to make sure the repo they're in on RHEL 8 gets enabled
+            tasks['to_keep'].update(event.in_pkgs)
 
-        if event.action in ('Renamed', 'Replaced'):
-            non_installed_out_pkgs = filter_out_installed_pkgs(event.out_pkgs, installed_pkgs)
-            to_install.update(non_installed_out_pkgs)
+        if event.action == 'Moved':
+            # Add these packages to to_keep to make sure the repo they're in on RHEL 8 gets enabled
+            # We don't care about the "in_pkgs" as it contains always just one pkg - the same as the "out" pkg
+            tasks['to_keep'].update(event.out_pkgs)
 
-        if event.action in ('Split', 'Merged'):
+        if event.action in ('Split', 'Merged', 'Renamed', 'Replaced'):
             non_installed_out_pkgs = filter_out_installed_pkgs(event.out_pkgs, installed_pkgs)
-            to_install.update(non_installed_out_pkgs)
-            in_pkgs_without_out_pkgs = filter_out_out_pkgs(event.in_pkgs, event.out_pkgs)
-            to_remove.update(in_pkgs_without_out_pkgs)
+            tasks['to_install'].update(non_installed_out_pkgs)
+            # Add the already installed "out" pkgs to to_keep to make sure the repo they're in on RHEL 8 gets enabled
+            installed_out_pkgs = get_installed_out_pkgs(event.out_pkgs, installed_pkgs)
+            tasks['to_keep'].update(installed_out_pkgs)
+            if event.action in ('Split', 'Merged'):
+                # Uninstall those RHEL 7 pkgs that are no longer on RHEL 8
+                in_pkgs_without_out_pkgs = filter_out_out_pkgs(event.in_pkgs, event.out_pkgs)
+                tasks['to_remove'].update(in_pkgs_without_out_pkgs)
 
         if event.action in ('Renamed', 'Replaced', 'Removed'):
-            to_remove.update(event.in_pkgs)
+            # Uninstall those RHEL 7 pkgs that are no longer on RHEL 8
+            tasks['to_remove'].update(event.in_pkgs)
 
-    map_repositories(to_install)
-    filter_out_pkgs_in_blacklisted_repos(to_install)
+    map_repositories(tasks['to_install'])
+    map_repositories(tasks['to_keep'])
+    filter_out_pkgs_in_blacklisted_repos(tasks['to_install'])
 
-    return to_install, to_remove
+    return tasks
 
 
 def filter_out_installed_pkgs(event_out_pkgs, installed_pkgs):
-    """Do not try to install those packages that are already installed."""
+    """Do not install those packages that are already installed."""
     return {k: v for k, v in event_out_pkgs.items() if k not in installed_pkgs}
+
+
+def get_installed_out_pkgs(event_out_pkgs, installed_pkgs):
+    """
+    Get those event's "out" packages which are already installed.
+
+    Even though we don't want to install the already installed pkgs, to be able to upgrade them to their RHEL 8 version
+    we need to know in which repos they are and enable such repos.
+    """
+    return {k: v for k, v in event_out_pkgs.items() if k in installed_pkgs}
 
 
 def filter_out_out_pkgs(event_in_pkgs, event_out_pkgs):
@@ -244,18 +261,25 @@ def filter_out_out_pkgs(event_in_pkgs, event_out_pkgs):
     to gdbm and gdbm-libs, we would incorrectly mandate removing gdbm without this filter. But for example in case of
     a split of Cython to python2-Cython and python3-Cython, we will correctly mandate removing Cython.
     """
-    return {k: v for k, v in event_in_pkgs.items() if k not in event_out_pkgs.keys()}
+    return {k: v for k, v in event_in_pkgs.items() if k not in event_out_pkgs}
 
 
 def filter_out_pkgs_in_blacklisted_repos(to_install):
-    """Do not install packages that are available in blacklisted repositories"""
+    """
+    Do not install packages that are available in blacklisted repositories
+
+    No need to filter out the to_keep packages as the blacklisted repos won't get enabled - that is ensured in the
+    setuptargetrepos actor. So even if they fall into the 'yum upgrade' bucket, they won't be available thus upgraded.
+    """
     # FIXME The to_install contains just a limited subset of packages - those that are *not* currently installed and
     # are to be installed. But we should also warn about the packages that *are* installed.
     blacklisted_pkgs = set()
     for pkg, repo in to_install.items():
         if repo in get_repositories_blacklisted():
             blacklisted_pkgs.add(pkg)
-            del to_install[pkg]
+
+    for pkg in blacklisted_pkgs:
+        del to_install[pkg]
 
     if blacklisted_pkgs:
         report_skipped_packages('packages will not be installed due to blacklisted repositories:',
@@ -270,20 +294,26 @@ def get_repositories_blacklisted():
     return repos_blacklisted
 
 
-def map_repositories(to_install):
-    """Map repositories from PES data to RHSM repository id"""
-    repo_not_mapped_pkgs = set()
-    for pkg, repo in to_install.items():
+def map_repositories(packages):
+    """
+    Map repositories from PES data to RHSM repository id
+
+    :param packages: A dictionary in format {<pkg_name>: <repository>}
+    """
+    repo_without_mapping = set()
+    for pkg, repo in packages.items():
         if repo not in REPOSITORIES_MAPPING:
-            repo_not_mapped_pkgs.add(pkg)
-            del to_install[pkg]
+            repo_without_mapping.add(pkg)
             continue
 
-        to_install[pkg] = REPOSITORIES_MAPPING[repo]
+        packages[pkg] = REPOSITORIES_MAPPING[repo]
 
-    if repo_not_mapped_pkgs:
-        report_skipped_packages('packages will not be installed due to not mapped repositories:',
-                                repo_not_mapped_pkgs)
+    for pkg in repo_without_mapping:
+        del packages[pkg]
+
+    if repo_without_mapping:
+        report_skipped_packages('packages will not be installed or upgraded due to repositories unknown to leapp:',
+                                repo_without_mapping)
 
 
 def report_skipped_packages(message, packages):
@@ -306,11 +336,11 @@ def add_output_pkgs_to_transaction_conf(transaction_configuration, events):
 
     Output packages from an event are added to packages for removal only if all input packages are already there.
 
-    :param events: List of Event tuples, where each event contains event type and input/output pkgs
     :param transaction_configuration: RpmTransactionTasks model instance with pkgs to install, keep and remove based
                                       on the user configuration files
+    :param events: List of Event tuples, where each event contains event type and input/output pkgs
     """
-    message = 'Marking packages for removal as a result of events:\n'
+    message = 'Marking packages for removal:\n'
 
     for event in events:
         if event.action in ('Split', 'Merged', 'Replaced', 'Renamed'):
@@ -325,46 +355,55 @@ def add_output_pkgs_to_transaction_conf(transaction_configuration, events):
     api.current_logger().debug(message)
 
 
-def filter_out_transaction_conf_pkgs(to_install, to_remove, transaction_configuration):
+def filter_out_transaction_conf_pkgs(tasks, transaction_configuration):
     """
-    Filter out those PES events that conflict with the higher priority transaction configuration files.
+    Filter out those PES events conflicting with the higher priority transaction configuration files.
 
-    :param to_install: A dict {<pkg_name>: <repository>} with pkgs to install based on PES data
-    :param to_remove: A dict {<pkg_name>: <repository>} with pkgs to remove based on PES data
+    :param tasks: A dict with three dicts holding pkgs to keep, to install and to remove
     :param transaction_configuration: RpmTransactionTasks model instance with pkgs to install, keep and remove based
                                       on the user configuration files
     """
-    # The two lists below are for debug message purposes only
-    pes_based_pkgs_not_to_be_installed = []
-    pes_based_pkgs_not_to_be_removed = []
+    pkgs_not_to_be_kept = []
+    pkgs_not_to_be_installed = []
+    pkgs_not_to_be_removed = []
 
-    for pes_based_pkg_to_install in to_install.keys():
-        if pes_based_pkg_to_install in transaction_configuration.to_remove:
-            to_install.pop(pes_based_pkg_to_install)
-            pes_based_pkgs_not_to_be_installed.append(pes_based_pkg_to_install)
-    for pes_based_pkg_to_remove in to_remove.keys():
-        if pes_based_pkg_to_remove in transaction_configuration.to_install + transaction_configuration.to_keep:
-            to_remove.pop(pes_based_pkg_to_remove)
-            pes_based_pkgs_not_to_be_removed.append(pes_based_pkg_to_remove)
+    for pkg_to_keep in tasks['to_keep']:
+        if pkg_to_keep in transaction_configuration.to_remove:
+            pkgs_not_to_be_kept.append(pkg_to_keep)
+    for pkg_to_install in tasks['to_install']:
+        if pkg_to_install in transaction_configuration.to_remove:
+            pkgs_not_to_be_installed.append(pkg_to_install)
+    for pkg_to_remove in tasks['to_remove']:
+        if pkg_to_remove in transaction_configuration.to_install + transaction_configuration.to_keep:
+            pkgs_not_to_be_removed.append(pkg_to_remove)
 
-    if pes_based_pkgs_not_to_be_installed:
+    for pkg in pkgs_not_to_be_kept:
+        # Removing a package from the to_keep dict may cause that some repositories won't get enabled
+        tasks['to_keep'].pop(pkg)
+
+    if pkgs_not_to_be_installed:
+        for pkg in pkgs_not_to_be_installed:
+            tasks['to_install'].pop(pkg)
         api.current_logger().debug('The following packages will not be installed because of the'
                                    ' /etc/leapp/transaction/to_remove transaction configuration file:'
-                                   '\n- ' + '\n- '.join(pes_based_pkgs_not_to_be_installed))
-    if pes_based_pkgs_not_to_be_removed:
+                                   '\n- ' + '\n- '.join(pkgs_not_to_be_installed))
+    if pkgs_not_to_be_removed:
+        for pkg in pkgs_not_to_be_removed:
+            tasks['to_remove'].pop(pkg)
         api.current_logger().debug('The following packages will not be removed because of the to_keep and to_install'
                                    ' transaction configuration files in /etc/leapp/transaction/:'
-                                   '\n- ' + '\n- '.join(pes_based_pkgs_not_to_be_removed))
+                                   '\n- ' + '\n- '.join(pkgs_not_to_be_removed))
 
 
-def produce_messages(to_install, to_remove):
-    to_install_pkgs = set(to_install.keys())
-    to_remove_pkgs = set(to_remove.keys())
-    to_enable_repos = set(to_install.values())
+def produce_messages(tasks):
+    # Type casting to list to be Py2&Py3 compatible as on Py3 keys() returns dict_keys(), not a list
+    to_install_pkgs = list(tasks['to_install'].keys())
+    to_remove_pkgs = list(tasks['to_remove'].keys())
+    to_enable_repos = list(set(tasks['to_install'].values() + tasks['to_keep'].values()))
 
     if to_install_pkgs or to_remove_pkgs:
-        api.produce(PESRpmTransactionTasks(to_install=list(to_install_pkgs),
-                                           to_remove=list(to_remove_pkgs)))
+        api.produce(PESRpmTransactionTasks(to_install=to_install_pkgs,
+                                           to_remove=to_remove_pkgs))
 
     if to_enable_repos:
-        api.produce(RepositoriesSetupTasks(to_enable=list(to_enable_repos)))
+        api.produce(RepositoriesSetupTasks(to_enable=to_enable_repos))
