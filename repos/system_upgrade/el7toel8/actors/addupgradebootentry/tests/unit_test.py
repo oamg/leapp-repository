8027b42a04b79b28d4afb0ed00e872f14aed132d
Jozef Mikovic <jmikovic@redhat.com>
2019-06-07 13:20:16 +0200

    Avoid possible misinterpratation of grubby --title parameter
    value of --title wasn't in quotes which could result in it being split
    and cause a failure of grubby

--- repos/system_upgrade/el7toel8/actors/addupgradebootentry/tests/unit_test.py
+++ repos/system_upgrade/el7toel8/actors/addupgradebootentry/tests/unit_test.py
@@ -24,8 +24,8 @@ def test_add_boot_entry(monkeypatch):
 
     library.add_boot_entry()
 
-    assert ' '.join(library.run.args) == ('/usr/sbin/grubby --add-kernel=/abc --initrd=/def --title=RHEL'
-                                          ' Upgrade Initramfs --copy-default --make-default --args="debug'
+    assert ' '.join(library.run.args) == ('/usr/sbin/grubby --add-kernel=/abc --initrd=/def --title="RHEL'
+                                          ' Upgrade Initramfs" --copy-default --make-default --args="debug'
                                           ' enforcing=0 rd.plymouth=0 plymouth.enable=0"')
 
 
