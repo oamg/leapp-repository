7136b945a05fe2f7a6e68b19649cac6069946658
Jozef Mikovic <jmikovic@redhat.com>
2019-06-10 15:47:17 +0200

    Avoid possible misinterpratation of grubby --title parameter (#237)
    * add vscode config to gitignore
    
    * Avoid possible misinterpratation of grubby --title parameter
    
    value of --title wasn't in quotes which could result in it being split
    and cause a failure of grubby

--- repos/system_upgrade/el7toel8/actors/addupgradebootentry/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/addupgradebootentry/libraries/library.py
@@ -14,7 +14,7 @@ def add_boot_entry():
         '/usr/sbin/grubby',
         '--add-kernel={0}'.format(kernel_dst_path),
         '--initrd={0}'.format(initram_dst_path),
-        '--title=RHEL Upgrade Initramfs',
+        '--title="RHEL Upgrade Initramfs"',
         '--copy-default',
         '--make-default',
         '--args="{DEBUG} enforcing=0 rd.plymouth=0 plymouth.enable=0"'.format(DEBUG=debug)
