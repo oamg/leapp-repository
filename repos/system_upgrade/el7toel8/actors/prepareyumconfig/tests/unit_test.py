695dd00f62eac97d5b06573bd6b9474c3ed6d1b8
Vinzenz Feenstra <vfeenstr@redhat.com>
2019-06-13 16:03:50 +0200

    Refactor prepareyumconfig to reuse the common function
    There's now a common function in the leapp.libraries.common.utils module
    that allows to reuse the same code for multiple actors. This patch
    ensures it is going to be used.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- repos/system_upgrade/el7toel8/actors/prepareyumconfig/tests/unit_test.py
+++ repos/system_upgrade/el7toel8/actors/prepareyumconfig/tests/unit_test.py
@@ -1,4 +1,6 @@
-from leapp.libraries.actor import library
+import os
+
+import leapp.libraries.stdlib
 
 
 class run_mocked(object):
@@ -6,14 +8,26 @@ class run_mocked(object):
         self.called = 0
         self.args = None
 
-    def __call__(self, args, split=True):
+    def __call__(self, args, split=False):
         self.called += 1
         self.args = args
-        return True
+        return {'stdout': ''}
+
+
+def _get_tool_path(name):
+    for directory in os.getenv('LEAPP_COMMON_TOOLS', '').split(':'):
+        full_path = os.path.join(directory, name)
+        if os.path.isfile(full_path):
+            return full_path
+    return None
 
 
 def test_prepare_yum_config(monkeypatch):
-    monkeypatch.setattr(library, 'run', run_mocked())
-    library.prepare_yum_config()
-    assert library.run.called == 1
-    assert library.run.args == ['handleyumconfig']
+    run = run_mocked()
+    with monkeypatch.context() as context:
+        context.setattr(leapp.libraries.stdlib, 'run', run)
+        context.setattr(leapp.libraries.stdlib.api, 'get_tool_path', _get_tool_path)
+        from leapp.libraries.common import utils  # Needed locally to allow monkey patching to actually work
+        utils.apply_yum_workaround()
+    assert run.called == 1
+    assert os.path.basename(run.args[-1]) == 'handleyumconfig'
