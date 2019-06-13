f1a0d422de93940f213ba8c2206c3d40d4ec452b
Vinzenz Feenstra <vfeenstr@redhat.com>
2019-06-13 22:31:41 +0200

    Actor that generates the initial target userspace
    The TargetUserSpaceCreator actor creates a directory where it installs
    dnf with all its depedencies from the target system.
    
    To be able to retrieve the packages from the target system it switches
    the product certificate in an overlay over the real system, unsets the
    release and refreshes the subscription. It will copy the certificates
    and repository files to the userspace to make them available for later
    usage. Then it will deploy dnf and all its dependencies to
    /var/lib/leapp/el8userspace.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/targetuserspacecreator/actor.py
@@ -0,0 +1,25 @@
+from leapp.actors import Actor
+from leapp.libraries.actor import userspacegen
+from leapp.models import (OSReleaseFacts, RequiredTargetUserspacePackages, SourceRHSMInfo, TargetRepositories,
+                          TargetRHSMInfo, TargetUserSpaceInfo, UsedTargetRepositories, XFSPresence)
+from leapp.tags import FactsPhaseTag, IPUWorkflowTag
+
+
+class TargetUserspaceCreator(Actor):
+    """
+    Initializes a directory to be populated as a minimal environment to run binaries from the target system.
+
+    The target userspace is set up in a directory so one can run it in a containerized environment to perform tasks
+    as if the system running would be the target system. This allows us to use the target system RPM stack including
+    DNF which gives us the ability to use RPM features from the target system.
+    The userspace environment is also used to generate a initram disk with dracut using the target system binaries
+    such as kernel, systemd etc etc
+    """
+
+    name = 'target_userspace_creator'
+    consumes = (OSReleaseFacts, RequiredTargetUserspacePackages, SourceRHSMInfo, TargetRepositories, XFSPresence)
+    produces = (TargetRHSMInfo, TargetUserSpaceInfo, UsedTargetRepositories)
+    tags = (IPUWorkflowTag, FactsPhaseTag)
+
+    def process(self):
+        userspacegen.perform()
