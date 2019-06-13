1fe73b162bb0321ab75be77f15e44745d8832756
Jakub Jelen <jjelen@redhat.com>
2019-06-13 14:51:12 +0200

    TcpWrappersCheck: New actor checking all daemons possibly affected by removal of the tcp wrappers package
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/tcpwrapperscheck/actor.py
@@ -0,0 +1,66 @@
+from leapp.actors import Actor
+from leapp.models import Report, TcpWrappersFacts, InstalledRedHatSignedRPM
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+from leapp.libraries.stdlib import api
+from leapp.libraries.actor.library import config_affects_daemons
+from leapp.libraries.common.rpms import create_lookup
+from leapp.libraries.common.reporting import report_with_links
+
+
+DAEMONS = [
+    ("audit", ["auditd"]),
+    ("bacula", ["bacula"]),
+    ("conserver", ["conserver"]),
+    ("cyrus-imapd", ["imap", "pop3", "imaps", "pop3s", "sieve", "lmtp"]),
+    ("dovecot", ["imap", "pop3", "imaps", "pop3s", "sieve", "lmtp"]),
+    ("nfs-utils", ["mountd", "statd"]),
+    ("openssh-server", ["sshd"]),
+    ("proftpd", ["proftpd"]),
+    ("quota", ["rquotad"]),
+    ("rpcbind", ["rpcbind"]),
+    # ("sendmail", ["sendmail"]),  # Handled in CheckSendmail Actor
+    ("slapi-nis", ["slapi-nis"]),
+    ("socat", ["socat"]),
+    ("stunnel", ["stunnel"]),
+    ("tftp-server", ["tftpd"]),
+    # ("vsftpd", ["vsftpd"]),  # Handled in VsftpdConfigCheck Actor
+    ("xinetd", ["xinetd"]),
+]
+
+
+class TcpWrappersCheck(Actor):
+    """
+    Check the list of packages previously compiled with TCP wrappers support
+    and check whether they have some rules configured in
+    /etc/hosts.{allow,deny} that are no longer honored by the daemons and
+    might make the service more accessible than expected.
+    """
+
+    name = 'tcp_wrappers_check'
+    consumes = (TcpWrappersFacts, InstalledRedHatSignedRPM,)
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        # Consume a single TCP Wrappers message
+        tcp_wrappers_messages = self.consume(TcpWrappersFacts)
+        tcp_wrappers_facts = next(tcp_wrappers_messages)
+        if list(tcp_wrappers_messages):
+            api.current_logger().warning('Unexpectedly received more than one TcpWrappersFacts message.')
+
+        # Convert installed packages message to list
+        packages = create_lookup(InstalledRedHatSignedRPM, field='items', key='name')
+
+        found_packages = config_affects_daemons(tcp_wrappers_facts, packages, DAEMONS)
+
+        if found_packages:
+            report_with_links(title='TCP Wrappers configuration affects some installed packages',
+                              summary=('tcp_wrappers support has been removed in RHEL-8. '
+                                       'There is some configuration affecting installed packages (namely {}) '
+                                       'in /etc/hosts.deny or /etc/hosts.allow, which '
+                                       'is no longer going to be effective after update. '
+                                       'Please migrate it manually.'.format(', '.join(found_packages))),
+                              links=[{'title': 'Replacing TCP Wrappers in RHEL 8',
+                                      'href': 'https://access.redhat.com/solutions/3906701'}],
+                              severity='high',
+                              flags=['inhibitor'])
---
+++ repos/system_upgrade/el7toel8/actors/tcpwrapperscheck/libraries/library.py
@@ -0,0 +1,30 @@
+from leapp.libraries.common.tcpwrappersutils import config_applies_to_daemon
+
+
+def config_affects_daemons(tcp_wrappers_facts, packages_list, daemons):
+    """
+    Check whether some of the daemons is installed and affected by existing
+    configuration of tcp_wrappers based on the.
+
+    :param tcp_wrappers_facts: Facts provided by the TcpWrappersFacts
+    :param packages_list: List of packages provided by InstalledRedHatSignedRPM
+    :param deamons: List of packages and keywords affecting daemons in this format:
+                    [{"package-name", ["daemon1", "daemon2", ...], ...}]
+    """
+    found_packages = set()
+
+    for (package, keywords) in daemons:
+        # We do not care for particular deamon if the providing package is not installed
+        if package not in packages_list:
+            continue
+
+        # Every package can have several deamons or deamons reacting to several keywords
+        for daemon in keywords:
+            # Is this daemon/keyword affected by the current configuration?
+            if not config_applies_to_daemon(tcp_wrappers_facts, daemon):
+                continue
+
+            # We do not report particular daemons, but just the high-level list of packages
+            found_packages.add(package)
+
+    return found_packages
