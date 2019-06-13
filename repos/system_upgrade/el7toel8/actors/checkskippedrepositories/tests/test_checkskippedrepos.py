fe63b048c98c00026419af6e69587943af95a2b2
Vinzenz Feenstra <vfeenstr@redhat.com>
2019-06-13 15:41:33 +0200

    Split check from SetupTargetRepos to new actor
    This patch splits the check part of the SetupTargetRepos actor to a
    separate actor. To split the logic of check and data generation.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/checkskippedrepositories/tests/test_checkskippedrepos.py
@@ -0,0 +1,70 @@
+from leapp.models import Report, SkippedRepositories
+
+
+def test_skipped_repos(current_actor_context):
+    reported_packages = ['pkg_a', 'pkg_b', 'pkg_c']
+    reported_repos = ['repo_a', 'repo_b', 'repo_c']
+    current_actor_context.feed(
+        SkippedRepositories(
+            packages=list(reported_packages),
+            repos=list(reported_repos)
+        )
+    )
+
+    current_actor_context.run()
+    reports = list(current_actor_context.consume(Report))
+    assert reports
+    assert len(reports) == 1
+    report = reports[0]
+    for pkg in reported_packages:
+        assert '\n- {}'.format(pkg) in report.detail.get('summary')
+    for repo in reported_repos:
+        assert '\n- {}'.format(repo) in report.detail.get('summary')
+
+
+def test_skipped_just_repos(current_actor_context):
+    reported_repos = ['repo_a', 'repo_b', 'repo_c']
+    current_actor_context.feed(
+        SkippedRepositories(
+            packages=[],
+            repos=list(reported_repos)
+        )
+    )
+
+    current_actor_context.run()
+    reports = list(current_actor_context.consume(Report))
+    assert reports
+    assert len(reports) == 1
+    report = reports[0]
+    for repo in reported_repos:
+        assert '\n- {}'.format(repo) in report.detail.get('summary')
+
+
+def test_skipped_repos_empty(current_actor_context):
+    current_actor_context.feed(
+        SkippedRepositories(
+            packages=[],
+            repos=[]
+        )
+    )
+    current_actor_context.run()
+    reports = list(current_actor_context.consume(Report))
+    assert not reports
+
+
+def test_skipped_repos_no_repos(current_actor_context):
+    current_actor_context.feed(
+        SkippedRepositories(
+            packages=['woot'],
+            repos=[]
+        )
+    )
+    current_actor_context.run()
+    reports = list(current_actor_context.consume(Report))
+    assert not reports
+
+
+def test_skipped_repos_no_message(current_actor_context):
+    current_actor_context.run()
+    reports = list(current_actor_context.consume(Report))
+    assert not reports
