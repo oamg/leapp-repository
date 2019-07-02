e4e0de20e9a9a8569f19284cd76945876d68966b
Vojtech Sokol <vsokol@redhat.com>
2019-07-02 14:47:09 +0200

    Simplyfy arguments to  functions and add more tests

--- repos/system_upgrade/el7toel8/libraries/config/architecture.py
+++ repos/system_upgrade/el7toel8/libraries/config/architecture.py
@@ -10,7 +10,7 @@ ARCH_S390X = 's390x'
 ARCH_ACCEPTED = (ARCH_X86_64, ARCH_ARM64, ARCH_PPC64LE, ARCH_S390X)
 
 
-def matches_architecture(match_list):
+def matches_architecture(*match_list):
     """
     Check if one of provided architectures matches the system's one.
 
@@ -19,11 +19,8 @@ def matches_architecture(match_list):
     :return: ``True`` if system's architecture matches one of the values in match_list, ``False`` otherwise
     :rtype: bool
     """
-    if not isinstance(match_list, (list, tuple)):
-        raise TypeError("Architectures to check against have to be a list or tuple "
-                        "but provided was {}: '{}'".format(type(match_list), match_list))
     if not all(isinstance(e, six.string_types) for e in match_list):
-        raise TypeError("Architectures to check against have to be a list or tuple of strings "
+        raise TypeError("Architectures to check against have to strings "
                         "but provided was {}: '{}'".format([type(e) for e in match_list], match_list))
     unsupported = set(match_list).difference(ARCH_ACCEPTED)
     if unsupported:
