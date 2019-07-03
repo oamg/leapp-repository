b6393e1113d9c4fb1590c5ac3ec086e8a16ad6a2
Tomas Meszaros <exo@tty.sk>
2019-07-03 16:18:04 +0200

    removed test_actor_with_missing_drivers

--- repos/system_upgrade/el7toel8/actors/kernel/checkkerneldrivers/tests/test_checkkernelmodules.py
+++ repos/system_upgrade/el7toel8/actors/kernel/checkkerneldrivers/tests/test_checkkernelmodules.py
@@ -3,17 +3,6 @@ from leapp.reporting import Report
 from leapp.snactor.fixture import current_actor_context
 
 
-def test_actor_with_missing_drivers(current_actor_context):
-    """ Tests CheckKernelDrivers actor by feeding it kernel drivers that
-    are not available on the RHEL8 system. Actor should produce inhibitor.
-    """
-    modules = [ActiveKernelModule(filename="virtio", parameters=[]),
-               ActiveKernelModule(filename="aoe", parameters=[])]
-    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=modules))
-    current_actor_context.run()
-    assert "inhibitor" in current_actor_context.consume(Report)[0].flags
-
-
 def test_actor_without_missing_drivers(current_actor_context):
     """ Tests CheckKernelDrivers actor by feeding it kernel drivers that
     are available on the RHEL8 system. Actor should NOT produce any report.
