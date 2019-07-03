2eb6770af7bd27bf926670ca2907e396af30a077
Vojtech Sokol <vsokol@redhat.com>
2019-07-03 08:49:22 +0200

    Add support for linting and testing repository libraries

--- conftest.py
+++ conftest.py
@@ -1,26 +1,32 @@
 import os
+
 from leapp.utils.repository import find_repository_basedir
 from leapp.repository.scan import find_and_scan_repositories
 
 
 def pytest_sessionstart(session):
     actor_path = os.environ.get('LEAPP_TESTED_ACTOR', None)
-    if not actor_path:
-        return
-    repo = find_and_scan_repositories(find_repository_basedir(actor_path), include_locals=True)
-    repo.load()
+    library_path = os.environ.get('LEAPP_TESTED_LIBRARY', None)
 
-    actor = None
-    # find which actor is being tested
-    for a in repo.actors:
-        if a.full_path == actor_path.rstrip('/'):
-            actor = a
-            break
+    if actor_path:
+        repo = find_and_scan_repositories(find_repository_basedir(actor_path), include_locals=True)
+        repo.load()
 
-    if not actor:
-        return
+        actor = None
+        # find which actor is being tested
+        for a in repo.actors:
+            if a.full_path == actor_path.rstrip('/'):
+                actor = a
+                break
 
-    # load actor context so libraries can be imported on module level
-    session.leapp_repository = repo
-    session.actor_context = actor.injected_context()
-    session.actor_context.__enter__()
+        if not actor:
+            return
+
+        # load actor context so libraries can be imported on module level
+        session.leapp_repository = repo
+        session.actor_context = actor.injected_context()
+        session.actor_context.__enter__()
+    elif library_path:
+        repo = find_and_scan_repositories(find_repository_basedir(library_path), include_locals=True)
+        repo.load()
+        os.chdir(library_path)
