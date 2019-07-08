04cfba70e402cfa0a40aa5a605bd91312c2a0511
Inessa Vasilevskaya <ivasilev@redhat.com>
2019-07-08 17:29:22 -0400

    component testing: selinuxprepare actor
    Suggested conditional skipping of a destructive test
    and some refactoring.
    A bug with wrong cil files paths have been fixed which
    uncovered test was failing.
    
    Conflicts:
    	repos/system_upgrade/el7toel8/actors/selinux/selinuxprepare/tests/component_test.py

--- repos/system_upgrade/el7toel8/actors/selinux/selinuxprepare/tests/component_test.py
+++ repos/system_upgrade/el7toel8/actors/selinux/selinuxprepare/tests/component_test.py
@@ -7,9 +7,8 @@ from leapp.models import SELinuxModule, SELinuxModules, SELinuxCustom
 from leapp.libraries.stdlib import api, run, CalledProcessError
 from leapp.reporting import Report
 
-enabled = False
 
-test_modules = [
+TEST_MODULES = [
     ["400", "mock1"],
     ["99", "mock1"],
     ["300", "mock1"],
@@ -17,85 +16,77 @@ test_modules = [
     ["999", "mock3"]
 ]
 
-semanage_commands = [
+SEMANAGE_COMMANDS = [
     ['fcontext', '-t', 'httpd_sys_content_t', '"/web(/.*)?"'],
     ['fcontext', '-t', 'ganesha_var_run_t', '"/ganesha(/.*)?"'],
     ['port', '-t', 'http_port_t', '-p', 'udp', '81'],
     ['permissive', 'abrt_t']
 ]
 
-# save value of semodule -lfull for comparison
-semodule_lfull = ""
-semanage_export = ""
+testmoduledir = "tests/mock_modules/"
 
-if enabled:
+
+def _run_cmd(cmd, split=False, logmsg=""):
     try:
-        semodule = run(["semodule", "-lfull"], split=False)
-        semodule_lfull = semodule.get("stdout", "")
-        semanage = run(["semanage", "export"], split=False)
-        semanage_export = semanage.get("stdout", "")
+        return run(cmd, split=split).get("stdout", "")
     except CalledProcessError as e:
-        api.current_logger().warning("Error listing SELinux customizations: %s", str(e.stderr))
+        # XXX FIXME not sure logging in tests makes sense
+        api.current_logger().warning("%s: %s", logmsg, str(e.stderr))
 
-testmoduledir = os.path.join(os.getcwd(), "tests/mock_modules/")
+
+SEMODULE_LFULL_INITIAL = _run_cmd(["semodule", "-lfull"], logmsg="Error listing SELinux customizations")
+SEMANAGE_EXPORT_INITIAL = _run_cmd(["semanage", "export"], logmsg="Error listing SELinux customizations")
 
 
 def setup():
-    if enabled:
-        for priority, module in test_modules:
-            try:
-                run(["semodule", "-X", priority, "-i", os.path.join(testmoduledir, module + ".cil")])
-            except CalledProcessError as e:
-                api.current_logger().warning("Error installing mock module: %s", str(e.stderr))
-                continue
+    # NOTE(ivasilev) Maybe there is a more elegant way to conditionally run setup/teardown
+    enabled = os.environ.get("DESTRUCTIVE_TESTING", False)
+    if not enabled:
+        return
 
-        for command in semanage_commands:
-            try:
-                run(["semanage", command[0], "-a"] + command[1:])
-            except CalledProcessError as e:
-                api.current_logger().warning("Error applying selinux customizations %s", str(e.stderr))
-                continue
+    for priority, module in TEST_MODULES:
+        tests_dir = os.path.join(os.getenv('PYTEST_CURRENT_TEST').rsplit(os.path.sep, 2)[0], testmoduledir)
+        _run_cmd(["semodule", "-X", priority, "-i", os.path.join(tests_dir, module + ".cil")],
+                 logmsg="Error installing mock module")
+
+    for command in SEMANAGE_COMMANDS:
+        _run_cmd(["semanage", command[0], "-a"] + command[1:], logmsg="Error applying selinux customizations")
 
 
-@pytest.mark.skip(reason='Test disabled because it would modify the system')
+@pytest.mark.skipif(not os.environ.get("DESTRUCTIVE_TESTING", False),
+                    reason='Test disabled by default because it would modify the system')
 def test_SELinuxPrepare(current_actor_context):
-    try:
-        semodule = run(["semodule", "-lfull"], split=False)
-        api.current_logger().info("Before test:" + semodule.get("stdout", ""))
-        semanage = run(["semanage", "export"], split=False)
-        api.current_logger().info("Before test:" + semanage.get("stdout", ""))
-    except CalledProcessError as e:
-        api.current_logger().warning("Error listing SELinux customizations: %s", str(e.stderr))
+    for cmd in (["semodule", "-lfull"], ["semanage", "export"]):
+        res = _run_cmd(cmd)
+        api.current_logger().info("Before test:%s", res)
 
+    # XXX FIXME test_modules.reverse() returns None and changes underlying list which should not happen
+    # to global vars. Removing that reversing as it is broken anyway
     semodule_list = [SELinuxModule(name=module, priority=int(prio), content="", removed=[])
-                     for (prio, module) in test_modules.reverse() + [["400", "permissive_abrt_t"]]]
+                     for (prio, module) in TEST_MODULES + [["400", "permissive_abrt_t"]]]
 
     current_actor_context.feed(SELinuxModules(modules=semodule_list))
     current_actor_context.run()
 
     # check if all given modules and local customizations where removed
-    try:
-        semodule = run(["semodule", "-lfull"], split=False)
-        assert semodule_lfull == semodule.get("stdout", "")
-        semanage = run(["semanage", "export"], split=False)
-        assert semanage_export == semanage.get("stdout", "")
-    except CalledProcessError as e:
-        api.current_logger().warning("Error listing SELinux customizations: %s", str(e.stderr))
-        assert False
+    semodule_res = _run_cmd(["semodule", "-lfull"])
+    assert SEMODULE_LFULL_INITIAL == semodule_res
+    semanage_res = _run_cmd(["semanage", "export"])
+    assert SEMANAGE_EXPORT_INITIAL == semanage_res
 
 
 def teardown():
-    if enabled:
-        for priority, module in test_modules + [["400", "permissive_abrt_t"]]:
-            try:
-                run(["semodule", "-X", priority, "-r", module])
-            except CalledProcessError:
-                # expected -- should be removed by the actor
-                pass
+    # NOTE(ivasilev) Maybe there is a more elegant way to conditionally run setup/teardown
+    enabled = os.environ.get("DESTRUCTIVE_TESTING", False)
+    if not enabled:
+        return
 
-        for command in semanage_commands:
-            try:
-                run(["semanage", command[0], "-d"] + command[1:])
-            except CalledProcessError:
-                # expected -- should be removed by the actor
-                continue
+    for priority, module in TEST_MODULES + [["400", "permissive_abrt_t"]]:
+        # failure is expected -- should be removed by the actor
+        # XXX FIXME if failure is 100% expected - use assertRaises
+        _run_cmd(["semodule", "-X", priority, "-r", module])
+
+    for command in SEMANAGE_COMMANDS:
+        # failure is expected -- should be removed by the actor
+        # XXX FIXME if failure is 100% expected - use assertRaises
+        _run_cmd(["semanage", command[0], "-d"] + command[1:])
