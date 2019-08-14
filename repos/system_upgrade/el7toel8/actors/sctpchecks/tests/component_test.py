40dbb1eb831fd55c47ee32df7aea7a2548af838c
Andrea Fickova <andre4.fickova@gmail.com>
2019-08-14 15:23:44 +0200

    add first little test

---
+++ repos/system_upgrade/el7toel8/actors/sctpchecks/tests/component_test.py
@@ -0,0 +1,10 @@
+from leapp.actors import Actor
+from leapp.models import SCTPConfig, RpmTransactionTasks
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+def test_actor_true(current_actor_context):
+    current_actor_context.feed(SCTPConfig(wanted=True))
+    current_actor_context.run()
+    assert current_actor_context.consume(RpmTransactionTasks)
+    # assert current_actor_context.consume(RpmTransactionTasks)[0].value == 3 3 3
