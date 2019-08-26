c424d8455b1ff5eb67fef9dd816753799c6bcc17
Alexandre Possebom <alexandrepossebom@gmail.com>
2019-08-26 08:39:40 -0300

    Add tests for BiosDevName actor. (#308)
    - Refactored to library
    - Added tests

--- repos/system_upgrade/el7toel8/actors/biosdevname/actor.py
+++ repos/system_upgrade/el7toel8/actors/biosdevname/actor.py
@@ -1,9 +1,6 @@
-import re
-
-import pyudev
-
 from leapp.actors import Actor
-from leapp.models import PersistentNetNamesFacts, KernelCmdlineArg
+from leapp.libraries.actor.library import check_biosdevname
+from leapp.models import KernelCmdlineArg, PersistentNetNamesFacts
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 
 
@@ -17,42 +14,5 @@ class Biosdevname(Actor):
     produces = (KernelCmdlineArg,)
     tags = (FactsPhaseTag, IPUWorkflowTag)
 
-    def is_biosdevname_disabled(self):
-        with open('/proc/cmdline') as cmdline:
-            if 'biosdevname=0' in cmdline.read():
-                return True
-
-        return False
-
-    def is_vendor_dell(self):
-        context = pyudev.Context()
-
-        # There should be only one dmi/id device
-        for dev in pyudev.Enumerator(context).match_subsystem('dmi').match_sys_name('id'):
-            vendor = dev.attributes.get('sys_vendor')
-            return re.search('Dell.*', str(vendor)) is not None
-        return False
-
-    def all_interfaces_biosdevname(self, interfaces):
-        # Biosdevname supports two naming schemes
-        emx = re.compile('em[0-9]+')
-        pxpy = re.compile('p[0-9]+p[0-9]+')
-
-        for i in interfaces:
-            if emx.match(i.name) is None and pxpy.match(i.name) is None:
-                return False
-
-        return True
-
-    def enable_biosdevname(self):
-        self.log.info("Biosdevname naming scheme in use, explicitely enabling biosdevname on RHEL-8")
-        self.produce(KernelCmdlineArg(**{'key': 'biosdevname', 'value': '1'}))
-
     def process(self):
-        interfaces = next(self.consume(PersistentNetNamesFacts)).interfaces
-
-        if self.is_biosdevname_disabled():
-            return
-
-        if self.is_vendor_dell() and self.all_interfaces_biosdevname(interfaces):
-            self.enable_biosdevname()
+        check_biosdevname()
