01a4e9b56f5da7c6ab11469851cd5566ac65f090
Tomas Meszaros <exo@tty.sk>
2019-09-09 11:52:48 +0200

    removed udevadminfo actor

--- repos/system_upgrade/el7toel8/actors/kernel/checkkerneldrivers/actor.py
+++ repos/system_upgrade/el7toel8/actors/kernel/checkkerneldrivers/actor.py
@@ -1,5 +1,5 @@
 from leapp.actors import Actor
-from leapp.models import ActiveKernelModulesFacts, WhitelistedKernelModules, UdevAdmInfoData
+from leapp.models import ActiveKernelModulesFacts, WhitelistedKernelModules
 from leapp.reporting import Report, create_report
 from leapp import reporting
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
@@ -20,7 +20,7 @@ class CheckKernelDrivers(Actor):
         the WhitelistedKernelModules
     """
     name = 'check_kernel_drivers'
-    consumes = (ActiveKernelModulesFacts, WhitelistedKernelModules, UdevAdmInfoData)
+    consumes = (ActiveKernelModulesFacts, WhitelistedKernelModules)
     produces = (Report,)
     tags = (ChecksPhaseTag, IPUWorkflowTag)
 
@@ -29,7 +29,6 @@ class CheckKernelDrivers(Actor):
             removed_drivers = []
             whitelisted_modules = set()
             collected_drivers = set()
-            drivers_to_report = set()
 
             # Extracting kernel drivers from the files/removed_drivers.txt.
             for line in removed.readlines():
@@ -52,29 +51,17 @@ class CheckKernelDrivers(Actor):
                     if active_module.filename in removed_drivers:
                         collected_drivers.add(active_module.filename)
 
-            # Going over the collected drivers and considering for reporting only
-            # those drivers that are currently used by some device.
-            udevadm_db = ''
-            for fact in self.consume(UdevAdmInfoData):
-                udevadm_db += fact.db
-            for line in udevadm_db.split('\n'):
-                if 'E: DRIVER=' in line:
-                    _, driver = line.split('=')
-                    if driver in collected_drivers:
-                        drivers_to_report.add(driver)
-
             # In the end, we are only going to report drivers that are:
             # - removed in the RHEL8 (are part of files/removed_drivers.txt)
             # - not whitelisted
-            # - currently being used by some device
-            if drivers_to_report:
+            if collected_drivers:
                 title = ('Detected loaded kernel drivers which have been removed '
                          'in RHEL 8. Upgrade cannot proceed.')
                 URL = ('https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html-single/'
                        'considerations_in_adopting_rhel_8/index#removed-device-drivers_hardware-enablement')
-                summary = ('Support for the following currently loaded RHEL 7 '
+                summary = ('Support for the following RHEL 7 '
                            'device drivers has been removed in RHEL 8: \n     - {}'
-                           '\nPlease see {} for details.'.format('\n     - '.join(drivers_to_report), URL))
+                           '\nPlease see {} for details.'.format('\n     - '.join(collected_drivers), URL))
                 remediation = ('Please disable detected kernel drivers in '
                                'order to proceed with the upgrade process using the rmmod tool.')
                 create_report([
@@ -84,4 +71,4 @@ class CheckKernelDrivers(Actor):
                     reporting.Tags([reporting.Tags.KERNEL, reporting.Tags.DRIVERS]),
                     reporting.Flags([reporting.Flags.INHIBITOR]),
                     reporting.Remediation(hint=remediation)
-                ] + [reporting.RelatedResource('kernel-driver', km) for km in drivers_to_report])
+                ] + [reporting.RelatedResource('kernel-driver', km) for km in collected_drivers])
