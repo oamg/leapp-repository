5ae2d76ed9a54a1f1a40b3cb166ef5598cae2a34
Alexandre José Possebom Silveira <asilveir@redhat.com>
2019-09-10 10:37:32 -0300

    Addressing some review points

--- repos/system_upgrade/el7toel8/actors/checksystemarch/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/checksystemarch/libraries/library.py
@@ -1,11 +1,10 @@
 from leapp import reporting
 from leapp.libraries.common.config import architecture
-from leapp.libraries.stdlib import api
 
 
 def check_architecture():
     """Check if given architecture is supported by upgrade process"""
-    if not architecture.matches_architecture(*architecture.ARCH_ACCEPTED):
+    if not architecture.matches_architecture(*architecture.ARCH_SUPPORTED):
         inhibit_upgrade()
 
 
@@ -13,14 +12,10 @@ def inhibit_upgrade():
     """Generate an upgrade inhibitor"""
     reporting.create_report(
         [
-            reporting.Title(
-                'Unsupported architecture [{}]'.format(
-                    api.current_actor().configuration.architecture
-                )
-            ),
+            reporting.Title('Unsupported architecture'),
             reporting.Summary(
                 'Upgrade process is only supported on {} systems.'.format(
-                    ', '.join(architecture.ARCH_ACCEPTED)
+                    ', '.join(architecture.ARCH_SUPPORTED)
                 )
             ),
             reporting.Severity(reporting.Severity.HIGH),
