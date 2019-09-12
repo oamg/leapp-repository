eabf927e0945a960c79b035f87fad02a6d88463a
Alexandre Possebom <alexandrepossebom@gmail.com>
2019-09-12 10:03:32 -0300

    Enable upgrade on more architectures. (#333)
    * Enable upgrade on more architectures.
    
     - The upgrade process will be not inhibit on 'x86_64', 'aarch64' and 'ppc64le'
     - Using logic from library.
     - Added unit tests.

---
+++ repos/system_upgrade/el7toel8/actors/checksystemarch/libraries/library.py
@@ -0,0 +1,25 @@
+from leapp import reporting
+from leapp.libraries.common.config import architecture
+
+
+def check_architecture():
+    """Check if given architecture is supported by upgrade process"""
+    if not architecture.matches_architecture(*architecture.ARCH_SUPPORTED):
+        inhibit_upgrade()
+
+
+def inhibit_upgrade():
+    """Generate an upgrade inhibitor"""
+    reporting.create_report(
+        [
+            reporting.Title('Unsupported architecture'),
+            reporting.Summary(
+                'Upgrade process is only supported on {} systems.'.format(
+                    ', '.join(architecture.ARCH_SUPPORTED)
+                )
+            ),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Tags([reporting.Tags.SANITY]),
+            reporting.Flags([reporting.Flags.INHIBITOR]),
+        ]
+    )
--- repos/system_upgrade/el7toel8/actors/checksystemarch/tests/test_checksystemarch.py
+++ repos/system_upgrade/el7toel8/actors/checksystemarch/tests/test_checksystemarch.py
@@ -1,13 +1,34 @@
-import platform
+from collections import namedtuple
 
-from leapp.reporting import Report
-from leapp.snactor.fixture import current_actor_context
+from leapp import reporting
+from leapp.libraries.actor import library
+from leapp.libraries.common.config import architecture
+from leapp.libraries.common.testutils import create_report_mocked
+from leapp.libraries.stdlib import api
 
 
-def test_actor_execution(current_actor_context):
-    current_actor_context.run()
-    if platform.machine() == 'x86_64':
-        assert not current_actor_context.consume(Report)
-    else:
-        assert current_actor_context.consume(Report)
-        assert 'inhibitor' in current_actor_context.consume(Report)[0].flags
+def test_valid_architectures(monkeypatch):
+    class CurrentActorMocked(object):
+        configuration = namedtuple('configuration', ['architecture'])(architecture.ARCH_ACCEPTED[0])
+
+    monkeypatch.setattr(reporting, "create_report", create_report_mocked())
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked)
+
+    library.check_architecture()
+
+    assert reporting.create_report.called == 0
+
+
+def test_invalid_architecture(monkeypatch):
+    class CurrentActorMocked(object):
+        configuration = namedtuple('configuration', ['architecture'])('invalid_architecture')
+
+    monkeypatch.setattr(reporting, "create_report", create_report_mocked())
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked)
+
+    library.check_architecture()
+    assert reporting.create_report.called == 1
+    assert 'Unsupported architecture' in reporting.create_report.report_fields['title']
+    assert 'Upgrade process is only supported' in reporting.create_report.report_fields['summary']
+    assert reporting.create_report.report_fields['severity'] == 'high'
+    assert 'inhibitor' in reporting.create_report.report_fields['flags']
