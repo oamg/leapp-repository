096a544266ae353e89376f19e42aa86902f9fcad
Petr Stodulka <xstodu05@gmail.com>
2019-09-15 14:33:52 +0200

    dhclientreporting: use the new reporting implementation
    Currently Leapp provides new reporting functionality. The original
    implementation has been redesigned completely to satisfy these
    goals:
      - be able to aggregate reports from various machines (to be able to
        upgrade clusters with many systems)
      - be able to group reported messages regarding to area they are
        handling (networking, security, specific rpms,...)
      - improve remediation instructions, when use could apply executable
        commands or playbook if provided; read, provided higher level
        of upgrade automatization in cooperation with user
      - ...

--- repos/system_upgrade/el7toel8/actors/dhclientreport/actor.py
+++ repos/system_upgrade/el7toel8/actors/dhclientreport/actor.py
@@ -1,6 +1,6 @@
 from leapp.actors import Actor
-from leapp.libraries.common.reporting import report_with_remediation
-from leapp.reporting import Report
+from leapp.reporting import Report, create_report
+from leapp import reporting
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
 from leapp.models import InstalledRedHatSignedRPM
 from leapp.libraries.common.rpms import has_package
@@ -9,7 +9,7 @@ from leapp.libraries.common.rpms import has_package
 class CheckDhclientActor(Actor):
     """
     Check if dhclient is installed
-    and report backward incompatible change in CLI options
+    and report backward incompatible change in CLI options.
     """
 
     name = "dhclient"
@@ -18,19 +18,19 @@ class CheckDhclientActor(Actor):
     tags = (ChecksPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        if has_package(InstalledRedHatSignedRPM, 'dhclient'):
-            title = 'dhclient command line options changed'
-            summary = (
+        if not has_package(InstalledRedHatSignedRPM, 'dhclient'):
+            return
+
+        create_report([
+            reporting.Title('dhclient command line options changed'),
+            reporting.Summary(
                 'dhclient command line options have been changed '
-                'since RHEL 7.7 release.'
-            )
-            remediation = (
-                'If you have custom script which involves dhclient '
+                'since RHEL 7.7 release.'),
+            reporting.Remediation(
+                hint='If you have custom script which involves dhclient '
                 'please check dhclient(8) man page for '
-                '-I/-C options'
-            )
-            severity = 'low'
-            report_with_remediation(title=title,
-                                    severity=severity,
-                                    remediation=remediation,
-                                    summary=summary)
+                '-I/-C options'),
+            reporting.Severity(reporting.Severity.LOW),
+            reporting.Tags([reporting.Tags.NETWORK, reporting.Tags.TOOLS]),
+            reporting.RelatedResource('package', 'dhclient')
+        ])
