a64d4be3be730e9f1bd7c1318d8151a20a4bc1f3
Tomas Meszaros <exo@tty.sk>
2019-09-16 09:59:12 +0200

    restored udevadm actor and model

---
+++ repos/system_upgrade/el7toel8/actors/udev/udevadminfo/actor.py
@@ -0,0 +1,19 @@
+from leapp.actors import Actor
+from leapp.libraries.stdlib import run
+from leapp.models import UdevAdmInfoData
+from leapp.tags import IPUWorkflowTag, FactsPhaseTag
+
+
+class UdevAdmInfo(Actor):
+    """
+    Produces data exported by the "udevadm info" command.
+    """
+
+    name = 'udevadm_info'
+    consumes = ()
+    produces = (UdevAdmInfoData,)
+    tags = (IPUWorkflowTag, FactsPhaseTag,)
+
+    def process(self):
+        out = run(['udevadm', 'info', '-e'])['stdout']
+        self.produce(UdevAdmInfoData(db=out))
