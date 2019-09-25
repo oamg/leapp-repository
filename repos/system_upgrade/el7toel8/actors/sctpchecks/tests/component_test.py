01b6021c12b531e4e483f829ef6113a0ee9092a2
Alexandre José Possebom Silveira <asilveir@redhat.com>
2019-09-25 09:01:16 -0300

    Add unit test to actor SCTP checks.
     - just a quick unit test to check out the two possible scenarios.

---
+++ repos/system_upgrade/el7toel8/actors/sctpchecks/tests/component_test.py
@@ -0,0 +1,27 @@
+from leapp.actors import Actor
+from leapp.models import SCTPConfig, RpmTransactionTasks
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+def test_sctp_wanted(current_actor_context):
+    current_actor_context.feed(SCTPConfig(wanted=True))
+    current_actor_context.run()
+    assert current_actor_context.consume(RpmTransactionTasks)
+    assert current_actor_context.consume(RpmTransactionTasks)[0].to_install == ['kernel-modules-extra']
+
+
+def test_sctp_empty_config(current_actor_context):
+    current_actor_context.feed(SCTPConfig())
+    current_actor_context.run()
+    assert not current_actor_context.consume(RpmTransactionTasks)
+
+
+def test_sctp_no_config(current_actor_context):
+    current_actor_context.run()
+    assert not current_actor_context.consume(RpmTransactionTasks)
+
+
+def test_sctp_unwanted(current_actor_context):
+    current_actor_context.feed(SCTPConfig(wanted=False))
+    current_actor_context.run()
+    assert not current_actor_context.consume(RpmTransactionTasks)
