863bccf04283229590c75f9ceb03578292c58bfc
Petr Stodulka <pstodulk@redhat.com>
2019-09-30 13:23:23 +0200

    Add new target phases into IPUWorkflow
    We realized we cannot properly gather all information and check it
    just with one scan and check phase when we need to get and process
    data from target system (e.g. create target userspace). In some cases
    we cannot even get those information because current HW is not
    supported on target system, so all such actions fails and we cannot
    prevent such actors effectively to no fail.
    
    To resolve that, add new 'Target*' phases for actors working with
    with target userspace or data about calculated RPM transaction.

--- repos/system_upgrade/el7toel8/tags/checks.py
+++ repos/system_upgrade/el7toel8/tags/checks.py
@@ -3,3 +3,7 @@ from leapp.tags import Tag
 
 class ChecksPhaseTag(Tag):
     name = 'checks'
+
+
+class TargetTransactionChecksPhaseTag(Tag):
+    name = 'target_checks'
--- repos/system_upgrade/el7toel8/tags/facts.py
+++ repos/system_upgrade/el7toel8/tags/facts.py
@@ -3,3 +3,7 @@ from leapp.tags import Tag
 
 class FactsPhaseTag(Tag):
     name = 'facts'
+
+
+class TargetTransactionFactsPhaseTag(Tag):
+    name = 'target_transaction_facts'
