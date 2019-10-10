5295f386745be6136c2885367dc0ac46bed922d4
Pavel Odvody <podvody@redhat.com>
2019-10-10 12:15:52 +0200

    Add actor for NSS/glibc/samba issue
    Upgrade should be inhibited when wins/winbind providers are used
    in NSS config file

---
+++ repos/system_upgrade/el7toel8/actors/nssglibccheck/actor.py
@@ -0,0 +1,28 @@
+import os
+
+from leapp.actors import Actor
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+from leapp import reporting
+from leapp.libraries.actor import utils
+
+
+# Path to NSS configuration file
+NSS_CONFIG_PATH = '/etc/nsswitch.conf'
+
+# List of affected modules
+BLACKLIST = ('wins', 'winbind',)
+
+
+class NSSGlibcCheckActor(Actor):
+    produces = (reporting.Report,)
+    consumes = ()
+    name = 'nss_glibc_check_actor'
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        if not os.path.exists(NSS_CONFIG_PATH):
+            return
+
+        with open(NSS_CONFIG_PATH, 'r') as fp:
+            stripped = [line.strip() for line in fp.readlines()]
+            utils.process_lines(stripped, BLACKLIST, NSS_CONFIG_PATH)
