a920c53acbb3de2b789b713acb19acff54155632
Pavel Odvody <podvody@redhat.com>
2019-10-26 01:41:57 +0200

    Fix boot order on EFI systems
    We need to make sure that we boot into the expected partition
    after every restart

---
+++ repos/system_upgrade/el7toel8/libraries/efi_reboot_fix.py
@@ -0,0 +1,44 @@
+from re import compile as regexp
+
+from leapp.libraries.stdlib import run
+
+
+_current_boot_matcher = regexp(r'BootCurrent: (?P<boot_current>(\d*))')
+_next_boot_matcher = regexp(r'BootNext: (?P<boot_next>(\d*))')
+
+
+def get_current_boot_match(string):
+    match = _current_boot_matcher.match(string)
+    if not match:
+        return None
+    captured_groups = match.groupdict()
+    return captured_groups['boot_current']
+
+
+def get_next_boot_match(string):
+    match = _next_boot_matcher.match(string)
+    if not match:
+        return None
+    captured_groups = match.groupdict()
+    return captured_groups['boot_next']
+
+
+def maybe_emit_updated_boot_entry():
+    efi_info = run(['/sbin/efibootmgr'], checked=False, split=True)
+    if efi_info['exit_code'] != 0:
+        # Not an efi system
+        return
+
+    current_boot, next_boot = None, None
+    for line in efi_info['stdout']:
+        current_match = get_current_boot_match(line)
+        if current_match:
+            current_boot = current_match
+
+        next_match = get_next_boot_match(line)
+        if next_match:
+            next_boot = next_match
+
+    if current_boot and not next_boot:
+        # We set BootNext to CurrentBoot only if BootNext wasn't previously set
+        run(['/sbin/efibootmgr', '-n', current_boot])
