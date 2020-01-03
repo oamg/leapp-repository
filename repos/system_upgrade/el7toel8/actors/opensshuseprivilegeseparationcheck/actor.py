1cf2956fba3d6f8c000a47d865977ec163b2bc12
Dominik Rehák <drehak@redhat.com>
2020-01-03 12:22:17 +0100

    OpenSshUsePrivilegeSeparationCheck part 2: Split to library

--- repos/system_upgrade/el7toel8/actors/opensshuseprivilegeseparationcheck/actor.py
+++ repos/system_upgrade/el7toel8/actors/opensshuseprivilegeseparationcheck/actor.py
@@ -1,10 +1,7 @@
 from leapp.actors import Actor
-from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.actor import library
 from leapp.models import Report, OpenSshConfig
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
-from leapp.libraries.stdlib import api
-from leapp.reporting import create_report
-from leapp import reporting
 
 
 class OpenSshUsePrivilegeSeparationCheck(Actor):
@@ -20,29 +17,4 @@ class OpenSshUsePrivilegeSeparationCheck(Actor):
     tags = (ChecksPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        openssh_messages = self.consume(OpenSshConfig)
-        config = next(openssh_messages, None)
-        if list(openssh_messages):
-            api.current_logger().warning('Unexpectedly received more than one OpenSshConfig message.')
-        if not config:
-            raise StopActorExecutionError(
-                'Could not check openssh configuration', details={'details': 'No OpenSshConfig facts found.'}
-            )
-
-        if config.use_privilege_separation is not None and \
-           config.use_privilege_separation != "sandbox":
-            create_report([
-                reporting.Title('OpenSSH configured not to use privilege separation sandbox'),
-                reporting.Summary(
-                    'OpenSSH is configured to disable privilege '
-                    'separation sandbox, which is decreasing security '
-                    'and is no longer supported in RHEL 8'
-                ),
-                reporting.Severity(reporting.Severity.LOW),
-                reporting.Tags([
-                        reporting.Tags.AUTHENTICATION,
-                        reporting.Tags.SECURITY,
-                        reporting.Tags.NETWORK,
-                        reporting.Tags.SERVICES
-                ]),
-            ])
+        library.process(self.consume(OpenSshConfig))
