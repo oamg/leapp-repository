47a25bdff51768e128c37949e55a7627ce2f7be4
Dominik Rehák <33783628+drehak@users.noreply.github.com>
2020-01-07 12:24:05 +0000

    Add tests for OpenSshUsePrivilegeSeparationCheck (#424)
    opensshconfig: add tests, refactoring, update model
     
    - Add tests for OpenSshUsePrivilegeSeparationCheck and split functionality
      into the library. 
    - OpenSshConfig model:
      - document attributes
      - make the modified attribute non-nullable; set default: False

---
+++ repos/system_upgrade/el7toel8/actors/opensshuseprivilegeseparationcheck/libraries/library.py
@@ -0,0 +1,32 @@
+# from leapp.actors import Actor
+from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.stdlib import api
+from leapp import reporting
+
+
+def process(openssh_messages):
+    config = next(openssh_messages, None)
+    if list(openssh_messages):
+        api.current_logger().warning('Unexpectedly received more than one OpenSshConfig message.')
+    if not config:
+        raise StopActorExecutionError(
+            'Could not check openssh configuration', details={'details': 'No OpenSshConfig facts found.'}
+        )
+
+    if config.use_privilege_separation is not None and \
+       config.use_privilege_separation != "sandbox":
+        reporting.create_report([
+            reporting.Title('OpenSSH configured not to use privilege separation sandbox'),
+            reporting.Summary(
+                'OpenSSH is configured to disable privilege '
+                'separation sandbox, which is decreasing security '
+                'and is no longer supported in RHEL 8'
+            ),
+            reporting.Severity(reporting.Severity.LOW),
+            reporting.Tags([
+                    reporting.Tags.AUTHENTICATION,
+                    reporting.Tags.SECURITY,
+                    reporting.Tags.NETWORK,
+                    reporting.Tags.SERVICES
+            ]),
+        ])
