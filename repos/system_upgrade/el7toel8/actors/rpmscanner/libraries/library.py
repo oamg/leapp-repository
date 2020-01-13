015d09f918db4bcd2b7056c41db32e3e84924fbc
Dominik Rehák <33783628+drehak@users.noreply.github.com>
2020-01-13 13:55:13 +0000

    RpmScanner: Throw an error if a yum call fails due to invalid locale (#430)
    `yum_base.doPackageLists()` works with default locale, which cannot be
    overridden. So if it fails due to the default locale being invalid, we
    can't really do much about it.

--- repos/system_upgrade/el7toel8/actors/rpmscanner/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/rpmscanner/libraries/library.py
@@ -1,3 +1,5 @@
+from leapp.exceptions import StopActorExecutionError
+
 
 def get_package_repository_data():
     """ Return dictionary mapping package name with repository from which it was installed """
@@ -6,7 +8,18 @@ def get_package_repository_data():
     import yum
     yum_base = yum.YumBase()
     pkg_repos = {}
-    for pkg in yum_base.doPackageLists().installed:
-        pkg_repos[pkg.name] = pkg.ui_from_repo.lstrip('@')
+
+    try:
+        for pkg in yum_base.doPackageLists().installed:
+            pkg_repos[pkg.name] = pkg.ui_from_repo.lstrip('@')
+    except ValueError as e:
+        if 'locale' not in str(e):  # reraise if error is not related to locales
+            raise e
+        raise StopActorExecutionError(
+            message='Failed to get installed RPM packages because of an invalid locale',
+            details={
+                'hint': 'Please run leapp with a valid locale. ' +
+                        'You can get a list of installed locales by running `locale -a`.'
+            })
 
     return pkg_repos
