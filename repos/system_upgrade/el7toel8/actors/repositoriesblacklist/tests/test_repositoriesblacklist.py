11eb88461dcd0dfbe6a7b4ea28a1ac1172aafc4d
Petr Stodulka <xstodu05@gmail.com>
2020-01-17 02:23:15 +0100

    [RFE: beta|htb] repositoriesblacklist: take product type into account
    Mark optional repository only in relation to the product type. So
    when src product type is non 'ga', check only for specified non-ga
    optional repositories (e.g. .*optional-beta-rpms) and ignore the
    others.

--- repos/system_upgrade/el7toel8/actors/repositoriesblacklist/tests/test_repositoriesblacklist.py
+++ repos/system_upgrade/el7toel8/actors/repositoriesblacklist/tests/test_repositoriesblacklist.py
@@ -1,30 +1,46 @@
-# from leapp.models import RepositoriesBlacklisted, RepositoriesFacts, RepositoryFile, RepositoryData
+from collections import namedtuple
+
+import pytest
+
 from leapp.libraries.actor import library
 from leapp.libraries.common.testutils import produce_mocked
 from leapp.libraries.stdlib import api
 from leapp.models import (
+    EnvVar,
     RepositoriesBlacklisted,
     RepositoriesFacts,
     RepositoriesMap,
-    RepositoryMap,
-    RepositoryFile,
     RepositoryData,
+    RepositoryFile,
+    RepositoryMap,
 )
 from leapp.snactor.fixture import current_actor_context
 
 
-def test_with_optionals(monkeypatch):
+class CurrentActorMocked(object):
+    def __init__(self, envars=None):
+        if envars:
+            envarsList = [EnvVar(name=key, value=value) for key, value in envars.items()]
+        else:
+            envarsList = []
+        self.configuration = namedtuple('configuration', ['leapp_env_vars'])(envarsList)
+
+    def __call__(self):
+        return self
+
+
+@pytest.mark.parametrize('valid_opt_repoid,product_type', [
+    ('rhel-7-optional-rpms', 'ga'),
+    ('rhel-7-optional-beta-rpms', 'beta'),
+    ('rhel-7-optional-htb-rpms', 'htb'),
+])
+def test_with_optionals(monkeypatch, valid_opt_repoid, product_type):
+    all_opt_repoids = {'rhel-7-optional-rpms', 'rhel-7-optional-beta-rpms', 'rhel-7-optional-htb-rpms'}
+    # set of repos that should not be marked as optionals
+    non_opt_repoids = all_opt_repoids - {valid_opt_repoid} | {'rhel-7-blacklist-rpms'}
+
     def repositories_mock(*model):
         mapping = [
-            RepositoryMap(
-                to_pes_repo='rhel-7-foobar-rpms',
-                from_repoid='rhel-7-optional-rpms',
-                to_repoid='rhel-8-optional-rpms',
-                from_minor_version='all',
-                to_minor_version='all',
-                arch='x86_64',
-                repo_type='rpm',
-            ),
             RepositoryMap(
                 to_pes_repo='rhel-7-blacklist-rpms',
                 from_repoid='rhel-7-blacklist-rpms',
@@ -35,12 +51,23 @@ def test_with_optionals(monkeypatch):
                 repo_type='rpm',
             ),
         ]
+        for repoid in all_opt_repoids:
+            mapping.append(RepositoryMap(
+                to_pes_repo='rhel-7-foobar-rpms',
+                from_repoid=repoid,
+                to_repoid='rhel-8-optional-rpms',
+                from_minor_version='all',
+                to_minor_version='all',
+                arch='x86_64',
+                repo_type='rpm',
+            ))
         yield RepositoriesMap(repositories=mapping)
 
     monkeypatch.setattr(api, "consume", repositories_mock)
-    optionals = library._get_list_of_optional_repos()
-    assert 'rhel-7-optional-rpms' in optionals
-    assert 'rhel-7-blacklist-rpms' not in optionals
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked({'LEAPP_DEVEL_SOURCE_PRODUCT_TYPE': product_type}))
+    optionals = set(library._get_list_of_optional_repos().keys())
+    assert {valid_opt_repoid} == optionals
+    assert not non_opt_repoids & optionals
 
 
 def test_without_optionals(monkeypatch):
@@ -68,6 +95,7 @@ def test_without_optionals(monkeypatch):
         yield RepositoriesMap(repositories=mapping)
 
     monkeypatch.setattr(api, "consume", repositories_mock)
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
     assert not library._get_list_of_optional_repos()
 
 
@@ -118,6 +146,7 @@ def test_repositoriesblacklist_not_empty(monkeypatch):
 
 def test_repositoriesblacklist_empty(monkeypatch):
     monkeypatch.setattr(library, "_get_disabled_optional_repo", lambda: [])
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
     monkeypatch.setattr(api, "produce", produce_mocked())
 
     library.process()
