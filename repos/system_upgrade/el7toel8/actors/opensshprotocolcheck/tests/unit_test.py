4f48ace56ae8700d414c402761159f36a66244d3
Dominik Rehák <drehak@redhat.com>
2020-01-17 16:15:10 +0100

    Add tests for OpenSshProtocolCheck (#436)
    - split functionality into the library
    - add tests

---
+++ repos/system_upgrade/el7toel8/actors/opensshprotocolcheck/tests/unit_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.actor import library
+from leapp.models import OpenSshConfig, OpenSshPermitRootLogin, Report
+from leapp.snactor.fixture import current_actor_context
+
+
+def test_no_config(current_actor_context):
+    with pytest.raises(StopActorExecutionError):
+        library.process(iter([]))
+
+
+osprl = OpenSshPermitRootLogin(value='no')
+
+
+@pytest.mark.parametrize('protocol', [None, '1', '2', '1,2', '2,1', '7'])
+def test_protocol(current_actor_context, protocol):
+    current_actor_context.feed(OpenSshConfig(
+        permit_root_login=[osprl],
+        protocol=protocol
+    ))
+    current_actor_context.run()
+    if protocol:
+        assert current_actor_context.consume(Report)
+    else:
+        assert not current_actor_context.consume(Report)
