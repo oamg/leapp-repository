e6e7a424fdf5079f2844775f0b154a7a3976e675
Dominik Rehák <33783628+drehak@users.noreply.github.com>
2020-01-23 16:43:35 +0000

    Add tests for OpenSshProtocolCheck (#436) (#436)
    - split functionality into the library
    - add tests

--- repos/system_upgrade/el7toel8/actors/opensshprotocolcheck/actor.py
+++ repos/system_upgrade/el7toel8/actors/opensshprotocolcheck/actor.py
@@ -1,10 +1,7 @@
 from leapp.actors import Actor
-from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.actor import library
 from leapp.models import Report, OpenSshConfig
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
-from leapp.libraries.stdlib import api
-from leapp.reporting import create_report
-from leapp import reporting
 
 
 class OpenSshProtocolCheck(Actor):
@@ -22,29 +19,4 @@ class OpenSshProtocolCheck(Actor):
     tags = (ChecksPhaseTag, IPUWorkflowTag, )
 
     def process(self):
-        openssh_messages = self.consume(OpenSshConfig)
-        config = next(openssh_messages, None)
-        if list(openssh_messages):
-            api.current_logger().warning('Unexpectedly received more than one OpenSshConfig message.')
-        if not config:
-            raise StopActorExecutionError(
-                'Could not check openssh configuration', details={'details': 'No OpenSshConfig facts found.'}
-            )
-
-        if config.protocol:
-            create_report([
-                reporting.Title('OpenSSH configured with removed configuration Protocol'),
-                reporting.Summary(
-                    'OpenSSH is configured with removed configuration '
-                    'option Protocol. If this used to be for enabling '
-                    'SSHv1, this is no longer supported in RHEL 8. '
-                    'Otherwise this option can be simply removed.'
-                ),
-                reporting.Severity(reporting.Severity.LOW),
-                reporting.Tags([
-                        reporting.Tags.AUTHENTICATION,
-                        reporting.Tags.SECURITY,
-                        reporting.Tags.NETWORK,
-                        reporting.Tags.SERVICES
-                ]),
-            ])
+        library.process(self.consume(OpenSshConfig))
---
+++ repos/system_upgrade/el7toel8/actors/opensshprotocolcheck/libraries/library.py
@@ -0,0 +1,31 @@
+from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.stdlib import api
+from leapp import reporting
+
+
+def process(openssh_messages):
+    config = next(openssh_messages, None)
+    if list(openssh_messages):
+        api.current_logger().warning('Unexpectedly received more than one OpenSshConfig message.')
+    if not config:
+        raise StopActorExecutionError(
+            'Could not check openssh configuration', details={'details': 'No OpenSshConfig facts found.'}
+        )
+
+    if config.protocol:
+        reporting.create_report([
+            reporting.Title('OpenSSH configured with removed configuration Protocol'),
+            reporting.Summary(
+                'OpenSSH is configured with removed configuration '
+                'option Protocol. If this used to be for enabling '
+                'SSHv1, this is no longer supported in RHEL 8. '
+                'Otherwise this option can be simply removed.'
+            ),
+            reporting.Severity(reporting.Severity.LOW),
+            reporting.Tags([
+                    reporting.Tags.AUTHENTICATION,
+                    reporting.Tags.SECURITY,
+                    reporting.Tags.NETWORK,
+                    reporting.Tags.SERVICES
+            ]),
+        ])
