2c8548323c29dfd15e204f85629e08a86b4a3d40
Petr Stodulka <pstodulk@redhat.com>
2020-01-29 12:29:46 +0100

    [RFE: beta|htb] libraries.common.config: add the get_product_type function
    The function returns info about the product type that should be
    used by leapp, in the meaning: ga, beta, htb. Nothing else.
    Product types can be specified by envars:
            LEAPP_DEVEL_SOURCE_PRODUCT_TYPE
            LEAPP_DEVEL_TARGET_PRODUCT_TYPE
    for source and target systems.

--- repos/system_upgrade/el7toel8/libraries/config/__init__.py
+++ repos/system_upgrade/el7toel8/libraries/config/__init__.py
@@ -2,7 +2,7 @@ from leapp.libraries.stdlib import api
 
 
 def get_env(name, default=None):
-    """ Return Leapp environment variable value if matched by name """
+    """Return Leapp environment variable value if matched by name."""
     for var in api.current_actor().configuration.leapp_env_vars:
         if var.name == name:
             return var.value
@@ -10,5 +10,36 @@ def get_env(name, default=None):
 
 
 def get_all_envs():
-    """ Return all Leapp environment variables (both name and value) """
+    """Return all Leapp environment variables (both name and value)."""
     return api.current_actor().configuration.leapp_env_vars
+
+
+def get_product_type(sys_type):
+    """
+    Get expected product type (ga/beta/htb) of the chosen sys type.
+
+    By default, expected product type is 'ga'. It can be changed using these envars:
+        LEAPP_DEVEL_SOURCE_PRODUCT_TYPE
+        LEAPP_DEVEL_TARGET_PRODUCT_TYPE
+    which can set the product type of chosen system (source/target) to one of
+    valid product types: ga/beta/htb.
+
+    Raise ValueError when specified sys_type or set product_type is invalid.
+
+    :param sys_type: choose system for which to get the product type: 'source' or 'target'
+    :type sys_type: str
+    :return: 'ga' (default), 'htb', 'beta'
+    :rtype: str
+    """
+    if sys_type == 'source':
+        envar = 'LEAPP_DEVEL_SOURCE_PRODUCT_TYPE'
+    elif sys_type == 'target':
+        envar = 'LEAPP_DEVEL_TARGET_PRODUCT_TYPE'
+    else:
+        raise ValueError('Given invalid sys_type. Valid values: source/target')
+    val = get_env(envar, '').lower()
+    if not val:
+        return 'ga'
+    if val in ('beta', 'htb', 'ga'):
+        return val
+    raise ValueError('Invalid value in the {} envar. Possible values: ga/beta/htb.'.format(envar))
--- repos/system_upgrade/el7toel8/libraries/config/tests/test_getenvvars.py
+++ repos/system_upgrade/el7toel8/libraries/config/tests/test_getenvvars.py
@@ -1,25 +1,69 @@
 from collections import namedtuple
 
-from leapp.libraries.common.config import get_env, get_all_envs
+import pytest
+
+from leapp.libraries.common.config import get_env, get_all_envs, get_product_type
 from leapp.libraries.stdlib import api
 from leapp.models import EnvVar
 
 
 class CurrentActorMocked(object):
-    env_vars = [EnvVar(name='LEAPP_DEVEL_SKIP_WIP', value='0'),
-                EnvVar(name='LEAPP_DEVEL_SKIP_DIP', value='1'),
-                EnvVar(name='LEAPP_DEVEL_SKIP_RIP', value='2')]
-    configuration = namedtuple('configuration', ['leapp_env_vars'])(env_vars)
+    def __init__(self, envars=None):
+        if envars:
+            envarsList = [EnvVar(name=key, value=value) for key, value in envars.items()]
+        else:
+            envarsList = []
+        self.configuration = namedtuple('configuration', ['leapp_env_vars'])(envarsList)
+
+    def __call__(self):
+        return self
 
 
 def test_env_var_match(monkeypatch):
-
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked)
+    envars = {'LEAPP_DEVEL_SKIP_WIP': '0',
+              'LEAPP_DEVEL_SKIP_DIP': '1',
+              'LEAPP_DEVEL_SKIP_RIP': '2'}
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(envars))
     assert get_env('LEAPP_DEVEL_SKIP_WIP') == '0'
     assert not get_env('LEAPP_DEVEL_SKIP_PIP')
 
 
 def test_get_all_vars(monkeypatch):
+    envars = {'LEAPP_DEVEL_SKIP_WIP': '0',
+              'LEAPP_DEVEL_SKIP_DIP': '1',
+              'LEAPP_DEVEL_SKIP_RIP': '2'}
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(envars))
+    assert api.current_actor().configuration.leapp_env_vars == get_all_envs()
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
+    assert api.current_actor().configuration.leapp_env_vars == get_all_envs()
 
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked)
-    assert len(get_all_envs()) == 3
+
+def test_get_product_type_valid(monkeypatch):
+    prod_types = ('ga', 'beta', 'htb', 'GA', 'BETA', 'HTB', '')
+    for src, dst in [(i, j) for i in prod_types for j in prod_types]:
+        envars = {'LEAPP_DEVEL_SOURCE_PRODUCT_TYPE': src,
+                  'LEAPP_DEVEL_TARGET_PRODUCT_TYPE': dst}
+        exp_src = 'ga' if not src else src.lower()
+        exp_dst = 'ga' if not dst else dst.lower()
+        monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(envars))
+        assert exp_src == get_product_type('source')
+        assert exp_dst == get_product_type('target')
+    # return 'ga' if envars are not specified
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
+    assert get_product_type('source') == 'ga'
+    assert get_product_type('target') == 'ga'
+
+
+def test_get_product_type_invalid_product(monkeypatch):
+    for sys_type, envar in [('source', 'LEAPP_DEVEL_SOURCE_PRODUCT_TYPE'),
+                            ('target', 'LEAPP_DEVEL_TARGET_PRODUCT_TYPE')]:
+        monkeypatch.setattr(api, 'current_actor', CurrentActorMocked({envar: 'wrong'}))
+        with pytest.raises(ValueError) as err:
+            get_product_type(sys_type)
+        assert 'Invalid value in the {} envar'.format(envar) in str(err)
+
+
+def test_get_product_type_invalid_param():
+    with pytest.raises(ValueError) as err:
+        get_product_type('fail')
+    assert 'Given invalid sys_type.' in str(err)
