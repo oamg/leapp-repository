2ce678fe8613c0d96ff720b7374466695aa6dc42
Dominik Rehák <drehak@redhat.com>
2020-02-20 13:02:30 +0100

    Inhibit upgrade if multiple kernel-devel pkgs are installed
    When multiple kernel-devel packages are present, leapp can't process
    the transaction in some cases, so we inhibit the upgrade and present
    the user with hints on removing the surplus devel kernels.

---
+++ repos/system_upgrade/el7toel8/actors/kernel/checkinstalleddevelkernels/actor.py
@@ -0,0 +1,23 @@
+from leapp.actors import Actor
+from leapp.models import InstalledRedHatSignedRPM
+from leapp.tags import IPUWorkflowTag, ChecksPhaseTag
+from leapp.reporting import Report
+from leapp.libraries.actor import library
+
+
+class CheckInstalledDevelKernels(Actor):
+    """
+    Inhibit IPU (in-place upgrade) when multiple devel kernels are installed.
+
+    Because of an issue in DNF, the transaction can't be validated if there's
+    more than one package named kernel-devel. Therefore, in this case, we
+    inhibit the upgrade with a clearer remediation.
+    """
+
+    name = 'check_installed_devel_kernels'
+    consumes = (InstalledRedHatSignedRPM,)
+    produces = (Report,)
+    tags = (IPUWorkflowTag, ChecksPhaseTag)
+
+    def process(self):
+        library.process()
