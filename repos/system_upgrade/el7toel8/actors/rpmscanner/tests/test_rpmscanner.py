94a858a6df0b68ea92c503ba6f24c77a38b7a489
Vinzenz Feenstra <vfeenstr@redhat.com>
2020-03-10 13:04:13 +0100

    [DO NOT MERGE] Actor Context Fix Check
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- repos/system_upgrade/el7toel8/actors/rpmscanner/tests/test_rpmscanner.py
+++ repos/system_upgrade/el7toel8/actors/rpmscanner/tests/test_rpmscanner.py
@@ -4,7 +4,6 @@ from leapp.snactor.fixture import current_actor_context
 from leapp.models import InstalledRPM
 
 
-@pytest.mark.skip(reason="skipping on py3 raise error on snactor")
 def test_actor_execution(current_actor_context):
     current_actor_context.run()
     assert current_actor_context.consume(InstalledRPM)
