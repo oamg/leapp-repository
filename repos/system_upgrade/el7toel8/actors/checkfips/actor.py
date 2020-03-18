07577fa98aac4b97b803febc628b00543273b674
Dominik Rehák <drehak@redhat.com>
2020-03-18 22:01:48 +0100

    Inhibit upgrade if FIPS is enabled
    Due to a multitude of issues we've encountered, we can't ensure a
    stable and FIPS-compliant upgrade if FIPS is enabled. Because of that,
    if FIPS is detected as enabled in kernel command line, we inhibit the
    upgrade.

---
+++ repos/system_upgrade/el7toel8/actors/checkfips/actor.py
@@ -0,0 +1,35 @@
+from leapp.actors import Actor
+from leapp.exceptions import StopActorExecutionError
+from leapp.models import Report, KernelCmdline
+from leapp.tags import IPUWorkflowTag, ChecksPhaseTag
+from leapp import reporting
+
+
+class CheckFips(Actor):
+    """
+    Inhibit upgrade if FIPS is detected as enabled.
+    """
+
+    name = 'check_fips'
+    consumes = (KernelCmdline,)
+    produces = (Report,)
+    tags = (IPUWorkflowTag, ChecksPhaseTag)
+
+    def process(self):
+        cmdline = next(self.consume(KernelCmdline), None)
+        if not cmdline:
+            raise StopActorExecutionError('Cannot check FIPS state due to missing command line parameters',
+                                          details={'Problem': 'Did not receive a message with kernel command '
+                                                              'line parameters (KernelCmdline)'})
+        for parameter in cmdline.parameters:
+            if parameter.key == 'fips' and parameter.value == '1':
+                title = 'Cannot upgrade a system with FIPS mode enabled'
+                summary = 'Leapp has detected that FIPS is enabled on this system. ' \
+                          'In-place upgrade of systems in FIPS mode is currently unsupported.'
+                reporting.create_report([
+                    reporting.Title(title),
+                    reporting.Summary(summary),
+                    reporting.Severity(reporting.Severity.HIGH),
+                    reporting.Tags([reporting.Tags.SECURITY]),
+                    reporting.Flags([reporting.Flags.INHIBITOR])
+                ])
