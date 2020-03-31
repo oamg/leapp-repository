eea15b4eaa18dfbb260cd259fd58d7e1c32702b6
Petr Stodulka <xstodu05@gmail.com>
2020-03-31 18:02:18 +0200

    [RFE custom repos 1/9] add actor to scan the default custom repo file
    The actor scan the custom repo file that user could use to define
    own repositories that should be used during the IPU as well.
    
    The expected path of the file is:
        /etc/leapp/files/leapp_upgrade_repositories.repo
    
    These repositories will be used automatically for the IPU despite
    the enable/disable settings. It's required the file and defined
    repositories has to be valid.
    
    If the file doesn't exist, nothing happens.

---
+++ repos/system_upgrade/el7toel8/actors/scancustomrepofile/libraries/scancustomrepofile.py
@@ -0,0 +1,32 @@
+import os
+
+from leapp.libraries.common import repofileutils
+from leapp.libraries.stdlib import api
+from leapp.models import CustomTargetRepository
+
+
+CUSTOM_REPO_PATH = "/etc/leapp/files/leapp_upgrade_repositories.repo"
+
+
+def process():
+    """
+    Produce CustomTargetRepository msgs for the custom repo file if the file
+    exists.
+
+    The CustomTargetRepository msg is produced for every repository inside
+    the <CUSTOM_REPO_PATH> file.
+    """
+    if not os.path.isfile(CUSTOM_REPO_PATH):
+        api.current_logger().debug(
+                "The {} file doesn't exist. Nothing to do."
+                .format(CUSTOM_REPO_PATH))
+        return
+    api.current_logger().info("The {} file exists.".format(CUSTOM_REPO_PATH))
+    repofile = repofileutils.parse_repofile(CUSTOM_REPO_PATH)
+    for repo in repofile.data:
+        api.produce(CustomTargetRepository(
+            repoid=repo.repoid,
+            name=repo.name,
+            baseurl=repo.baseurl,
+            enabled=repo.enabled,
+        ))
---
+++ repos/system_upgrade/el7toel8/actors/scancustomrepofile/tests/test_scancustomrepofile.py
@@ -0,0 +1,75 @@
+import os
+
+from leapp.libraries.actor import scancustomrepofile
+from leapp.libraries.common import repofileutils
+from leapp.libraries.common.testutils import produce_mocked
+from leapp.libraries.stdlib import api
+from leapp.models import CustomTargetRepository, RepositoryData, RepositoryFile
+
+
+_REPODATA = [
+    RepositoryData(repoid="repo1", name="repo1name", baseurl="repo1url", enabled=True),
+    RepositoryData(repoid="repo2", name="repo2name", baseurl="repo2url", enabled=False),
+    RepositoryData(repoid="repo3", name="repo3name", enabled=True),
+    RepositoryData(repoid="repo4", name="repo4name", mirrorlist="mirror4list", enabled=True),
+]
+
+_CUSTOM_REPOS = [
+    CustomTargetRepository(repoid="repo1", name="repo1name", baseurl="repo1url", enabled=True),
+    CustomTargetRepository(repoid="repo2", name="repo2name", baseurl="repo2url", enabled=False),
+    CustomTargetRepository(repoid="repo3", name="repo3name", baseurl=None, enabled=True),
+    CustomTargetRepository(repoid="repo4", name="repo4name", baseurl=None, enabled=True),
+]
+
+
+class LoggerMocked(object):
+    def __init__(self):
+        self.infomsg = None
+        self.debugmsg = None
+
+    def info(self, msg):
+        self.infomsg = msg
+
+    def debug(self, msg):
+        self.debugmsg = msg
+
+    def __call__(self):
+        return self
+
+
+def test_no_repofile(monkeypatch):
+    monkeypatch.setattr(os.path, 'isfile', lambda dummy: False)
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+    monkeypatch.setattr(api, 'current_logger', LoggerMocked())
+    scancustomrepofile.process()
+    msg = "The {} file doesn't exist. Nothing to do.".format(scancustomrepofile.CUSTOM_REPO_PATH)
+    assert api.current_logger.debugmsg == msg
+    assert not api.produce.called
+
+
+def test_valid_repofile_exists(monkeypatch):
+    def _mocked_parse_repofile(fpath):
+        return RepositoryFile(file=fpath, data=_REPODATA)
+    monkeypatch.setattr(os.path, 'isfile', lambda dummy: True)
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+    monkeypatch.setattr(repofileutils, 'parse_repofile', _mocked_parse_repofile)
+    monkeypatch.setattr(api, 'current_logger', LoggerMocked())
+    scancustomrepofile.process()
+    msg = "The {} file exists.".format(scancustomrepofile.CUSTOM_REPO_PATH)
+    assert api.current_logger.infomsg == msg
+    assert api.produce.called == len(_CUSTOM_REPOS)
+    for crepo in _CUSTOM_REPOS:
+        assert crepo in api.produce.model_instances
+
+
+def test_empty_repofile_exists(monkeypatch):
+    def _mocked_parse_repofile(fpath):
+        return RepositoryFile(file=fpath, data=[])
+    monkeypatch.setattr(os.path, 'isfile', lambda dummy: True)
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+    monkeypatch.setattr(repofileutils, 'parse_repofile', _mocked_parse_repofile)
+    monkeypatch.setattr(api, 'current_logger', LoggerMocked())
+    scancustomrepofile.process()
+    msg = "The {} file exists.".format(scancustomrepofile.CUSTOM_REPO_PATH)
+    assert api.current_logger.infomsg == msg
+    assert not api.produce.called
