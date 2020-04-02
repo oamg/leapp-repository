297cac882a999926000cd0face55753b0896365b
Christian Heimes <cheimes@redhat.com>
2020-04-02 14:54:42 +0200

    Provide upgrade inhibitor for ipa-server
    IdM (ipa-server) does not support in-place upgrade from RHEL 7 to 8.
    Instead IdM servers must be upgraded by creating a new RHEL 8 machine
    and installing a replica.
    
    The new IpaScanner actor scans for the system for IdM packages and
    client/server instances. The new CheckIPAServer prohibits upgrades when
    an IdM server is installed and warns the user when ipa-server package is
    present.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/checkipaserver/actor.py
@@ -0,0 +1,30 @@
+from leapp.actors import Actor
+from leapp.libraries.actor.library import (
+    ipa_inhibit_upgrade,
+    ipa_warn_pkg_installed,
+)
+from leapp.models import IpaInfo
+from leapp.reporting import Report
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+class CheckIPAServer(Actor):
+    """
+    Check for ipa-server and inhibit upgrade
+    """
+
+    name = "check_ipa_server"
+    consumes = (IpaInfo,)
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        for ipainfo in self.consume(IpaInfo):
+            if ipainfo.is_server_configured:
+                self.log.error(
+                    "IdM server instance detected, inhibit upgrade"
+                )
+                ipa_inhibit_upgrade(ipainfo)
+            elif ipainfo.has_server_package:
+                self.log.info("Unused ipa-server package detected")
+                ipa_warn_pkg_installed(ipainfo)
---
+++ repos/system_upgrade/el7toel8/actors/checkipaserver/libraries/library.py
@@ -0,0 +1,66 @@
+from leapp import reporting
+
+MIGRATION_GUIDE = (
+    "https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux"
+    "/8/html/installing_identity_management/migrate-7-to-8_migrating"
+)
+
+
+def ipa_inhibit_upgrade(ipainfo):
+    """
+    Create upgrade inhibitor for configured ipa-server
+    """
+    entries = [
+        reporting.Title(
+            "ipa-server does not support in-place upgrade from RHEL 7 to 8"
+        ),
+        reporting.Summary(
+            "An IdM server installation was detected on the system. IdM "
+            "does not support in-place upgrade to RHEL 8. Please follow "
+            "the migration guide lines."
+        ),
+        reporting.Remediation(
+            hint="Please follow the IdM RHEL 7 to 8 migration guide lines."
+        ),
+        reporting.ExternalLink(
+            url=MIGRATION_GUIDE, title="Migrating IdM from RHEL 7 to 8",
+        ),
+        reporting.Severity(reporting.Severity.HIGH),
+        reporting.Flags([reporting.Flags.INHIBITOR]),
+        reporting.Tags([reporting.Tags.SERVICES]),
+        reporting.RelatedResource("package", "ipa-server"),
+    ]
+    return reporting.create_report(entries)
+
+
+def ipa_warn_pkg_installed(ipainfo):
+    """
+    Warn that unused ipa-server package is installed
+    """
+    if ipainfo.is_client_configured:
+        summary = (
+            "The ipa-server package is installed but only IdM client is "
+            "configured on this system."
+        )
+    else:
+        summary = (
+            "The ipa-server package is installed but neither IdM server "
+            "nor client is configured on this system."
+        )
+    entries = [
+        reporting.Title(
+            "ipa-server package is installed but no IdM is configured"
+        ),
+        reporting.Summary(summary),
+        reporting.Remediation(
+            hint="Remove unused ipa-server package",
+            commands=[["yum", "remove", "ipa-server"]],
+        ),
+        reporting.ExternalLink(
+            url=MIGRATION_GUIDE, title="Migrating IdM from RHEL 7 to 8",
+        ),
+        reporting.Severity(reporting.Severity.MEDIUM),
+        reporting.Tags([reporting.Tags.SERVICES]),
+        reporting.RelatedResource("package", "ipa-server"),
+    ]
+    return reporting.create_report(entries)
