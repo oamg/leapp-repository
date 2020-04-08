b50299f843a967aff3122a0cdd05a22b0467c189
Michal Ruprich <michalruprich@gmail.com>
2020-04-08 11:48:57 +0200

    Fixing code to cope with the guidelines

--- repos/system_upgrade/el7toel8/actors/quaggatofrr/libraries/library.py
+++ repos/system_upgrade/el7toel8/actors/quaggatofrr/libraries/library.py
@@ -3,21 +3,21 @@ import os
 import shutil
 from leapp.libraries.stdlib import CalledProcessError, api, run
 
-DAEMON_FILE='/etc/frr/daemons'
+DAEMON_FILE = '/etc/frr/daemons'
 #if this file sitll exists after the removal of quagga, it has been modified
-CONFIG_FILE='/etc/sysconfig/quagga.rpmsave'
-QUAGGA_CONF_FILES='/etc/quagga/'
-FRR_CONF_FILES='/etc/frr/'
+CONFIG_FILE = '/etc/sysconfig/quagga.rpmsave'
+QUAGGA_CONF_FILES = '/etc/quagga/'
+FRR_CONF_FILES = '/etc/frr/'
 
 def _get_config_data(path):
     ret = {}
     with open(path, 'r') as f:
         file_data = f.readlines()
-	
+
         #exclude WATCH and BABELD
         data_filter = '(?!WATCH|BABELD).*_OPTS="(.*)"'
         # data that we need is always DAEMON_OPTS="data"
-        search_filter = '(.*)_OPTS="(.*)"'	
+        search_filter = '(.*)_OPTS="(.*)"'
 
         if bool(file_data):
             data_regex = re.compile(r'{}'.format(data_filter))
@@ -40,7 +40,7 @@ def _edit_new_config(path, active_daemons, config_data):
     if bool(config_data):
         for daemon in config_data:
             data = re.sub(r'^{}_options=".*"'.format(daemon),
-                r'{}_options="{}"'.format(daemon,config_data[daemon]),
+                r'{}_options="{}"'.format(daemon, config_data[daemon]),
                 data, flags=re.MULTILINE)
 
     with open(path, 'w') as f:
@@ -75,13 +75,12 @@ def _enable_frr(quagga_facts):
 #due to an error in quagga, the conf files are not deleted after uninstall
 #we can copy them as they are
 def _copy_config_files(src_path, dest_path):
-    conf_files=os.listdir(src_path)
+    conf_files = os.listdir(src_path)
     for file_name in conf_files:
-        full_path = os.path.join(src_path,file_name)
+        full_path = os.path.join(src_path, file_name)
         if os.path.isfile(full_path):
             shutil.copy(full_path, dest_path)
             api.current_logger().debug('Copying {} to {}{}'.format(full_path, dest_path, file_name))
-     
 
 def process_facts(quagga_facts):
     _change_config(quagga_facts)
--- repos/system_upgrade/el7toel8/models/quaggatofrrfacts.py
+++ repos/system_upgrade/el7toel8/models/quaggatofrrfacts.py
@@ -4,7 +4,7 @@ from leapp.topics import SystemInfoTopic
 
 class QuaggaToFrrFacts(Model):
     '''
-    A list of configuration files used by quagga. This list is used to add yes/no to 
+    A list of configuration files used by quagga. This list is used to add yes/no to
     /etc/frr/daemons file. It indicates which daemons from frr should be run.
     '''
     topic = SystemInfoTopic
