8c739607a9473f23b54e41df82d91532020a226f
zhukovgreen <iam@zhukovgreen.pro>
2020-04-24 15:01:19 +0200

    Adjust enable_sctp for easy testing
    - provide _modprobe_d_path as arg
    - little improvement to check the command exit is 0 before saying Enabled SCTP

--- repos/system_upgrade/el7toel8/actors/sctpconfigupdate/libraries/sctpupdate.py
+++ repos/system_upgrade/el7toel8/actors/sctpconfigupdate/libraries/sctpupdate.py
@@ -1,16 +1,21 @@
+import os
+
 from leapp.libraries.stdlib import api, run
 from leapp.models import SCTPConfig
 
 
-def enable_sctp():
+def enable_sctp(_modprobe_d_path="/etc/modprobe.d"):
     """
     Enables the SCTP module by removing it from being black listed.
+    :type _modprobe_d_path: str
+    :param _modprobe_d_path: overwrite only in case of testing, by passing
+        some tmp_dir instead
     """
 
     api.current_logger().info('Enabling SCTP.')
     run(['/usr/bin/sed', '-i', r's/^\s*blacklist.*sctp/#&/',
-         '/etc/modprobe.d/sctp_diag-blacklist.conf',
-         '/etc/modprobe.d/sctp-blacklist.conf'])
+         os.path.join(_modprobe_d_path, 'sctp_diag-blacklist.conf'),
+         os.path.join(_modprobe_d_path, 'sctp-blacklist.conf')])
     api.current_logger().info('Enabled SCTP.')
 
 
