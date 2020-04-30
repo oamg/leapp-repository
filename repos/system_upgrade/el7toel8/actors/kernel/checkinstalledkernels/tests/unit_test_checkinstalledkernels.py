fb99a40942812ec1cf307459dfc6c61167144bf9
zhukovgreen <iam@zhukovgreen.pro>
2020-04-30 11:47:37 +0200

    make actor tests name unique
    This is needed because pytest is not able to run all tests in couple
    of files with the same name. To ensure we are able to run all tests
    with pytest, let's rename those files. See the good practice:
        https://docs.pytest.org/en/latest/goodpractices.html
    
    The new practice for the naming of tests is like this:
       - unit_test_<actordir>.py
       - component_test_<actordir>.py
    
    Or instead of actor dir, use the actor name. The goal is to ensure,
    you will not have to spend minutes by searching the name that
    is still free.

--- repos/system_upgrade/el7toel8/actors/kernel/checkinstalledkernels/tests/unit_test_checkinstalledkernels.py
+++ repos/system_upgrade/el7toel8/actors/kernel/checkinstalledkernels/tests/unit_test_checkinstalledkernels.py
@@ -3,9 +3,13 @@ from collections import namedtuple
 from leapp import reporting
 from leapp.libraries.actor import library
 from leapp.libraries.common.config import architecture
-from leapp.libraries.common.testutils import create_report_mocked, CurrentActorMocked, logger_mocked
+from leapp.libraries.common.testutils import (
+    CurrentActorMocked,
+    create_report_mocked,
+    logger_mocked,
+)
 from leapp.libraries.stdlib import api
-from leapp.models import InstalledRedHatSignedRPM, RPM
+from leapp.models import RPM, InstalledRedHatSignedRPM
 
 RH_PACKAGER = 'Red Hat, Inc. <http://bugzilla.redhat.com/bugzilla>'
 
@@ -14,18 +18,30 @@ def mocked_consume(pkgs):  # pkgs = [(name, version-number)]
     installed_rpms = []
     version = 1
     for pkg in pkgs:
-        installed_rpms.append(RPM(
-                name=pkg[0], arch='noarch',
-                version=str(version), release='{}.sm01'.format(pkg[1]), epoch='0',
-                packager=RH_PACKAGER, pgpsig='SOME_OTHER_SIG_X'))
+        installed_rpms.append(
+            RPM(
+                name=pkg[0],
+                arch='noarch',
+                version=str(version),
+                release='{}.sm01'.format(pkg[1]),
+                epoch='0',
+                packager=RH_PACKAGER,
+                pgpsig='SOME_OTHER_SIG_X',
+            )
+        )
         version += 1
 
     def f(*a):
         yield InstalledRedHatSignedRPM(items=installed_rpms)
+
     return f
 
 
-s390x_pkgs_single = [('kernel', 957), ('something', 957), ('kernel-something', 957)]
+s390x_pkgs_single = [
+    ('kernel', 957),
+    ('something', 957),
+    ('kernel-something', 957),
+]
 s390x_pkgs_multi = [('kernel', 957), ('something', 957), ('kernel', 956)]
 
 
@@ -37,7 +53,9 @@ def test_single_kernel_s390x(monkeypatch):
     library.process()
     assert not reporting.create_report.called
 
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(arch=architecture.ARCH_S390X))
+    monkeypatch.setattr(
+        api, 'current_actor', CurrentActorMocked(arch=architecture.ARCH_S390X)
+    )
     monkeypatch.setattr(api, 'consume', mocked_consume(s390x_pkgs_single))
     library.process()
     assert not reporting.create_report.called
@@ -51,36 +69,59 @@ def test_multi_kernel_s390x(monkeypatch):
     library.process()
     assert not reporting.create_report.called
 
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(arch=architecture.ARCH_S390X))
+    monkeypatch.setattr(
+        api, 'current_actor', CurrentActorMocked(arch=architecture.ARCH_S390X)
+    )
     monkeypatch.setattr(api, 'consume', mocked_consume(s390x_pkgs_multi))
     library.process()
     assert reporting.create_report.called
-    assert reporting.create_report.report_fields['title'] == 'Multiple kernels installed'
+    assert (
+        reporting.create_report.report_fields['title']
+        == 'Multiple kernels installed'
+    )
 
 
 versioned_kernel_pkgs = [('kernel', 456), ('kernel', 789), ('kernel', 1234)]
 
 
 def test_newest_kernel(monkeypatch):
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel='3.10.0-1234.21.1.el7.x86_64'))
+    monkeypatch.setattr(
+        api,
+        'current_actor',
+        CurrentActorMocked(kernel='3.10.0-1234.21.1.el7.x86_64'),
+    )
     monkeypatch.setattr(api, 'current_logger', logger_mocked())
     monkeypatch.setattr(api, 'consume', mocked_consume(versioned_kernel_pkgs))
     monkeypatch.setattr(reporting, 'create_report', create_report_mocked())
     library.process()
     assert not reporting.create_report.called
 
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel='3.10.0-456.43.1.el7.x86_64'))
+    monkeypatch.setattr(
+        api,
+        'current_actor',
+        CurrentActorMocked(kernel='3.10.0-456.43.1.el7.x86_64'),
+    )
     monkeypatch.setattr(api, 'current_logger', logger_mocked())
     monkeypatch.setattr(api, 'consume', mocked_consume(versioned_kernel_pkgs))
     monkeypatch.setattr(reporting, 'create_report', create_report_mocked())
     library.process()
     assert reporting.create_report.called
-    assert reporting.create_report.report_fields['title'] == 'Newest installed kernel not in use'
+    assert (
+        reporting.create_report.report_fields['title']
+        == 'Newest installed kernel not in use'
+    )
 
-    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel='3.10.0-789.35.2.el7.x86_64'))
+    monkeypatch.setattr(
+        api,
+        'current_actor',
+        CurrentActorMocked(kernel='3.10.0-789.35.2.el7.x86_64'),
+    )
     monkeypatch.setattr(api, 'current_logger', logger_mocked())
     monkeypatch.setattr(api, 'consume', mocked_consume(versioned_kernel_pkgs))
     monkeypatch.setattr(reporting, 'create_report', create_report_mocked())
     library.process()
     assert reporting.create_report.called
-    assert reporting.create_report.report_fields['title'] == 'Newest installed kernel not in use'
+    assert (
+        reporting.create_report.report_fields['title']
+        == 'Newest installed kernel not in use'
+    )
