065fb87d33b655a5c461e117a3aa946c9a52d4dc
zhukovgreen <iam@zhukovgreen.pro>
2020-06-19 12:43:28 +0200

    Skip grubdevname on s390x cpu arch

--- repos/system_upgrade/el7toel8/actors/grubdevname/actor.py
+++ repos/system_upgrade/el7toel8/actors/grubdevname/actor.py
@@ -1,5 +1,6 @@
 from leapp.actors import Actor
 from leapp.libraries.actor.grubdevname import get_grub_device
+from leapp.libraries.common.config import architecture
 from leapp.models import GrubDevice
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 
@@ -15,4 +16,6 @@ class Grubdevname(Actor):
     tags = (FactsPhaseTag, IPUWorkflowTag)
 
     def process(self):
+        if architecture.matches_architecture(architecture.ARCH_S390X):
+            return
         get_grub_device()
