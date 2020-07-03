3c51bfda210c5baa7d3629e2396e3a35b72382f4
Michal Reznik <mreznik@redhat.com>
2020-07-03 10:11:59 +0200

    Make sure "default.target.wants" dir exists
    In case nothing is enabled in the default target, the directory
    does not exist yet.

--- repos/system_upgrade/el7toel8/actors/createresumeservice/actor.py
+++ repos/system_upgrade/el7toel8/actors/createresumeservice/actor.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 
+from leapp.exceptions import StopActorExecutionError
 from leapp.actors import Actor
 from leapp.tags import FinalizationPhaseTag, IPUWorkflowTag
 from leapp.reporting import Report, create_report
@@ -21,19 +22,26 @@ class CreateSystemdResumeService(Actor):
 
     def process(self):
         service_name = 'leapp_resume.service'
+        systemd_dir = '/etc/systemd/system'
+
         service_templ_fpath = self.get_file_path(service_name)
-        shutil.copyfile(service_templ_fpath, os.path.join(
-            '/etc/systemd/system/', service_name))
+        shutil.copyfile(service_templ_fpath, os.path.join(systemd_dir, service_name))
 
         service_path = '/etc/systemd/system/{}'.format(service_name)
         symlink_path = '/etc/systemd/system/default.target.wants/{}'.format(service_name)
 
+        # in case nothing is enabled in the default target, the directory does not exist
+        try:
+            os.mkdir(os.path.join(systemd_dir, 'default.target.wants'))
+        except OSError:
+            pass
+
         try:
             os.symlink(service_path, symlink_path)
         except OSError as e:
-            self.report_error('Could not create a symlink to enable {}'.format(service_name),
-                              details=str(e))
-            return
+            raise StopActorExecutionError(
+                    'Could not create a symlink to enable {}'.format(service_name),
+                    details={"details": str(e)})
 
         create_report([
             reporting.Title('Leapp resume systemd service enabled'),
