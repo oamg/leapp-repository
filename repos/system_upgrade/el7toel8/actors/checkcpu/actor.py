6cfe956cd186d35d250fb0e91186ad7fb64a43b7
Dan Horák <dan@danny.cz>
2020-07-24 11:59:04 +0200

    checkcpu: add machine ids for IBM z15 as supported
    Signed-off-by: Dan Horák <dan@danny.cz>

--- repos/system_upgrade/el7toel8/actors/checkcpu/actor.py
+++ repos/system_upgrade/el7toel8/actors/checkcpu/actor.py
@@ -11,7 +11,7 @@ class CheckCPU(Actor):
     Currently we know just about cases with s390x where the set of CPUs supported
     by RHEL 8 is subset of CPUs supported on RHEL 7. We can detect such cases based
     on the machine field inside the /proc/cpuinfo file. expected values of the
-    field on supported machines are: 2964, 2965, 3906, 3907.
+    field on supported machines are: 2964, 2965, 3906, 3907, 8561, 8562.
     """
 
     name = "checkcpu"
