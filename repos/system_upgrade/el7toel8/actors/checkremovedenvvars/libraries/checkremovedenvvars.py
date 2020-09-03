ed820bd53036d45a0d4acea7831975548328bd75
Michal Reznik <mreznik@redhat.com>
2020-09-03 14:31:58 +0200

    Introduce actor to check for removed env vars
    Currently, there is no process to report users about
    removed environment variables.
    
    This was introduced with removal of "LEAPP_GRUB_DEVICE" env var.

---
+++ repos/system_upgrade/el7toel8/actors/checkremovedenvvars/libraries/checkremovedenvvars.py
@@ -0,0 +1,25 @@
+from leapp.libraries.common.config import get_all_envs
+from leapp.reporting import create_report
+from leapp import reporting
+
+DEPRECATED_VARS = ['LEAPP_GRUB_DEVICE']
+
+
+def process():
+
+    vars_to_report = []
+
+    for var in get_all_envs():
+        if var.name in DEPRECATED_VARS:
+            vars_to_report.append(var.name)
+
+    if vars_to_report:
+        vars_str = ' '.join(vars_to_report)
+        create_report([
+            reporting.Title('Leapp detected removed environment variable usage'),
+            reporting.Summary('The following Leapp related environment variable was removed: ' + vars_str),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Remediation(hint='Please do not use the reported variables'),
+            reporting.Flags(['inhibitor']),
+            reporting.Tags([reporting.Tags.UPGRADE_PROCESS]),
+        ])
---
+++ repos/system_upgrade/el7toel8/actors/checkremovedenvvars/tests/test_checkremovedenvvars.py
@@ -0,0 +1,25 @@
+import pytest
+
+from leapp.libraries.common.testutils import produce_mocked
+from leapp.libraries.stdlib import api
+from leapp.libraries.actor import checkremovedenvvars
+from leapp.libraries.common.testutils import CurrentActorMocked
+
+
+def test_removed_vars(monkeypatch):
+    envars = {'LEAPP_GRUB_DEVICE': '/dev/sda'}
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(envars=envars))
+    monkeypatch.setattr(api.current_actor, "produce", produce_mocked())
+    checkremovedenvvars.process()
+    assert api.current_actor.produce.called == 1
+    assert 'LEAPP_GRUB_DEVICE' in api.current_actor.produce.model_instances[0].report['summary']
+    assert 'inhibitor' in api.current_actor.produce.model_instances[0].report['flags']
+
+
+def test_no_removed_vars(monkeypatch):
+    envars = {'LEAPP_SKIP_RHSM': '1'}
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(envars=envars))
+    monkeypatch.setattr(api.current_actor, "produce", produce_mocked())
+    checkremovedenvvars.process()
+    assert not api.current_actor.produce.called
+    assert not api.current_actor.produce.model_instances
