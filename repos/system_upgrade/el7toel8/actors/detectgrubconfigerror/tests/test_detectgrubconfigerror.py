eb0011477e156a5a5274d716b79c8c1e4d223664
Dominik Rehák <drehak@redhat.com>
2020-09-14 15:27:40 +0200

    detectgrubconfigerror: Fix false positive on comments, add tests
    The false positive occured when GRUB_CMDLINE_LINUX was followed by a
    comment. In a later stage, it caused the AddUpgradeBootEntry actor to
    break the GRUB config and the booting process.
    
    Added tests include the /etc/default/grub config file on which this
    behavior occured, according to the bug report:
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1872356#c0

--- repos/system_upgrade/el7toel8/actors/detectgrubconfigerror/tests/test_detectgrubconfigerror.py
+++ repos/system_upgrade/el7toel8/actors/detectgrubconfigerror/tests/test_detectgrubconfigerror.py
@@ -8,6 +8,8 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_correct_config():
     assert not detect_config_error(os.path.join(CUR_DIR, 'files/grub.correct'))
     assert not detect_config_error(os.path.join(CUR_DIR, 'files/grub.correct_trailing_space'))
+    assert not detect_config_error(os.path.join(CUR_DIR, 'files/grub.correct_comment'))
+    assert not detect_config_error(os.path.join(CUR_DIR, 'files/grub.correct_puppet'))
 
 
 def test_wrong_config():
