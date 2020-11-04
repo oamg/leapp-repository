5fe6750b84779f083dab9f94c856312b619260a6
Petr Stodulka <pstodulk@redhat.com>
2020-11-04 17:21:17 +0100

    InstalledTargetKernelVersion: respect kernel-rt
    Previously just the kernel rpm has been detected. However, in case
    of real time systems, we need to detect the kernel-rt rpm. In case
    of real time kernels, the kernel rpm doesn't have to be installed
    at all.
    
    Currently it's not clear whether we should handle stuff realted to
    both kernels (kernel and kernel-rt) when both present. Right now,
    detect whether the original system was booted with preemptive kernel
    or not and based on that prefer the expected kernel.
    
    In case the target kernel-rt rpm is not present and it is expected,
    fallback to the kernel rpm to make possible finish of the upgrade,
    but log warning msg.
    
    TODO: raise report in such a case

--- repos/system_upgrade/el7toel8/actors/scaninstalledtargetkernelversion/tests/test_scaninstalledkernel_scaninstalledtargetkernelversion.py
+++ repos/system_upgrade/el7toel8/actors/scaninstalledtargetkernelversion/tests/test_scaninstalledkernel_scaninstalledtargetkernelversion.py
@@ -1,31 +1,76 @@
+import pytest
+
 from leapp.libraries import stdlib
 from leapp.libraries.actor import scankernel
+from leapp.libraries.common.testutils import CurrentActorMocked, logger_mocked
 from leapp.libraries.stdlib import api
 
 TARGET_KERNEL_VERSION = '1.2.3-4.el8.x86_64'
+TARGET_RT_KERNEL_VERSION = '1.2.3-4.rt56.7.el8.x86_64'
 TARGET_KERNEL = 'kernel-{}'.format(TARGET_KERNEL_VERSION)
+TARGET_RT_KERNEL = 'kernel-{}'.format(TARGET_RT_KERNEL_VERSION)
 OLD_KERNEL = 'kernel-0.1.2-3.el7.x86_64'
+OLD_RT_KERNEL = 'kernel-rt-0.1.2-3.rt4.5.el7.x86_64'
 
 
-def mocked_run_with_target_kernel(*args, **kwargs):
-    return {'stdout': [TARGET_KERNEL, OLD_KERNEL]}
+class MockedRun(object):
+
+    def __init__(self, stdouts):
+        # stdouts should be dict of list of strings: { str: [str1,str2,...]}
+        self._stdouts = stdouts
+
+    def __call__(self, *args, **kwargs):
+        for key in ('kernel', 'kernel-rt'):
+            if key in args[0]:
+                return {'stdout': self._stdouts.get(key, [])}
+        return {'stdout': []}
 
 
-def mocked_run_without_target_kernel(*args, **kwargs):
-    return {'stdout': [OLD_KERNEL]}
-
-
-def test_scaninstalledkernel(monkeypatch):
+@pytest.mark.parametrize('is_rt,exp_version,stdouts', [
+    (False, TARGET_KERNEL_VERSION, {'kernel': [OLD_KERNEL, TARGET_KERNEL]}),
+    (False, TARGET_KERNEL_VERSION, {'kernel': [TARGET_KERNEL, OLD_KERNEL]}),
+    (False, TARGET_KERNEL_VERSION, {
+        'kernel': [TARGET_KERNEL, OLD_KERNEL],
+        'kernel-rt': [TARGET_RT_KERNEL, OLD_RT_KERNEL],
+    }),
+    (True, TARGET_RT_KERNEL_VERSION, {'kernel-rt': [OLD_RT_KERNEL, TARGET_RT_KERNEL]}),
+    (True, TARGET_RT_KERNEL_VERSION, {'kernel-rt': [TARGET_RT_KERNEL, OLD_RT_KERNEL]}),
+    (True, TARGET_RT_KERNEL_VERSION, {
+        'kernel': [TARGET_KERNEL, OLD_KERNEL],
+        'kernel-rt': [TARGET_RT_KERNEL, OLD_RT_KERNEL],
+    }),
+])
+def test_scaninstalledkernel(monkeypatch, is_rt, exp_version, stdouts):
     result = []
-    monkeypatch.setattr(stdlib, 'run', mocked_run_with_target_kernel)
+    old_kver = '0.1.2-3.rt4.5.el7.x86_64' if is_rt else 'kernel-0.1.2-3.el7.x86_64'
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel=old_kver))
     monkeypatch.setattr(api, 'produce', result.append)
+    monkeypatch.setattr(scankernel, 'run', MockedRun(stdouts))
     scankernel.process()
-    assert result and result[0].version == TARGET_KERNEL_VERSION
+    assert len(result) == 1 and result[0].version == exp_version
+
+
+def test_scaninstalledkernel_missing_rt(monkeypatch):
+    result = []
+    old_kver = '0.1.2-3.rt4.5.el7.x86_64'
+    stdouts = {'kernel': [TARGET_KERNEL], 'kernel-rt': [OLD_RT_KERNEL]}
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel=old_kver))
+    monkeypatch.setattr(api, 'current_logger', logger_mocked())
+    monkeypatch.setattr(api, 'produce', result.append)
+    monkeypatch.setattr(scankernel, 'run', MockedRun(stdouts))
+    scankernel.process()
+    assert api.current_logger.warnmsg
+    assert len(result) == 1 and result[0].version == TARGET_KERNEL_VERSION
 
 
 def test_scaninstalledkernel_missing(monkeypatch):
     result = []
-    monkeypatch.setattr(stdlib, 'run', mocked_run_without_target_kernel)
+    old_kver = '0.1.2-3.rt4.5.el7.x86_64'
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(kernel=old_kver))
+    monkeypatch.setattr(api, 'current_logger', logger_mocked())
     monkeypatch.setattr(api, 'produce', result.append)
+    monkeypatch.setattr(scankernel, 'run', MockedRun({}))
     scankernel.process()
+    assert api.current_logger.warnmsg
+    assert api.current_logger.errmsg
     assert not result
