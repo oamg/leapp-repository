e41cb0403bf025530ec1c7ebc50ea0915bcd327f
Petr Stodulka <pstodulk@redhat.com>
2020-11-17 17:00:09 +0100

    commonleappdracutmodules: generate tasks for upgrade initramfs
    Use the new mechanism to influence the generation of the upgrade
    initramfs (TargetUserspaceUpgradeTasks, UpgradeInitramfsTasks).
    The deprecated messages are still provided as before to follow
    the deprecation process.

--- repos/system_upgrade/el7toel8/actors/commonleappdracutmodules/tests/test_modscan_commonleappdracutmodules.py
+++ repos/system_upgrade/el7toel8/actors/commonleappdracutmodules/tests/test_modscan_commonleappdracutmodules.py
@@ -6,7 +6,13 @@ from leapp.libraries.actor import modscan
 from leapp.libraries.common.config import architecture
 from leapp.libraries.common.testutils import CurrentActorMocked
 from leapp.libraries.stdlib import api
-from leapp.models import UpgradeDracutModule, RequiredUpgradeInitramPackages
+from leapp.models import (
+    RequiredUpgradeInitramPackages,  # deprecated
+    TargetUserSpaceUpgradeTasks,
+    UpgradeDracutModule,  # deprecated
+    UpgradeInitramfsTasks,
+)
+from leapp.utils.deprecation import suppress_deprecation
 
 
 def _files_get_folder_path(name):
@@ -15,17 +21,19 @@ def _files_get_folder_path(name):
     return path
 
 
+@suppress_deprecation(UpgradeDracutModule)
 def test_created_modules(monkeypatch):
     monkeypatch.setattr(api, 'get_actor_folder_path', _files_get_folder_path)
     path = os.path.abspath(api.get_actor_folder_path('dracut'))
-    required_modules = ['sys-upgrade', 'sys-upgrade-redhat']
-    for mod in modscan._create_dracut_modules():
-        index = required_modules.index(mod.name)
+    required_modules = {'sys-upgrade', 'sys-upgrade-redhat'}
+    # _old is for deprecated stuff, _new is ...
+    # we want to test that the new and original solution produce 'equivalent' output
+    produced_required_modules_old = set()
+    produced_required_modules_new = set()
 
+    def check_module(mod):
         # Ensures that this actor only includes known modules
-        assert index != -1
-
-        required_modules.pop(index)
+        assert mod.name in required_modules
 
         # Ensures that the path is valid
         assert mod.module_path
@@ -40,7 +48,18 @@ def test_created_modules(monkeypatch):
         # Ensure the directory name ends with the module name
         assert os.path.basename(mod.module_path).endswith(mod.name)
         assert not re.sub(r'^(85|90){}$'.format(mod.name), '', os.path.basename(mod.module_path))
-    assert not required_modules
+
+    for msg in modscan._create_dracut_modules():
+        if isinstance(msg, UpgradeDracutModule):
+            produced_required_modules_old.add(msg.name)
+            check_module(msg)
+        else:
+            for mod in msg.include_dracut_modules:
+                produced_required_modules_new.add(mod.name)
+                check_module(mod)
+
+    # ensure that old and new solution produce equivalent (expected) output
+    assert produced_required_modules_new == produced_required_modules_old == required_modules
 
 
 def test_required_packages(monkeypatch):
@@ -51,12 +70,20 @@ def test_required_packages(monkeypatch):
     # one
     monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
     monkeypatch.setattr(architecture, 'matches_architecture', lambda x: False)
-    assert set(modscan._REQUIRED_PACKAGES) == set(modscan._create_initram_packages().packages)
+    old_initram, new_initram = modscan._create_initram_packages()
+    assert (set(modscan._REQUIRED_PACKAGES)
+            == set(old_initram.packages)
+            == set(new_initram.install_rpms))
 
+    # for intel arch
     monkeypatch.setattr(architecture, 'matches_architecture', lambda x: True)
-    assert set(modscan._REQUIRED_PACKAGES + ['biosdevname']) == set(modscan._create_initram_packages().packages)
+    old_initram, new_initram = modscan._create_initram_packages()
+    assert (set(modscan._REQUIRED_PACKAGES + ['biosdevname'])
+            == set(old_initram.packages)
+            == set(new_initram.install_rpms))
 
 
+@suppress_deprecation(UpgradeDracutModule)
 def test_process_produces_modules(monkeypatch):
     monkeypatch.setattr(api, 'current_actor', CurrentActorMocked())
     messages = []
@@ -64,6 +91,8 @@ def test_process_produces_modules(monkeypatch):
     monkeypatch.setattr(api, 'get_actor_folder_path', _files_get_folder_path)
     modscan.process()
     assert messages
-    assert len(messages) == 3
+    assert len(messages) == 6
     assert len([msg for msg in messages if isinstance(msg, UpgradeDracutModule)]) == 2
+    assert len([msg for msg in messages if isinstance(msg, UpgradeInitramfsTasks)]) == 2
     assert len([msg for msg in messages if isinstance(msg, RequiredUpgradeInitramPackages)]) == 1
+    assert len([msg for msg in messages if isinstance(msg, TargetUserSpaceUpgradeTasks)]) == 1
