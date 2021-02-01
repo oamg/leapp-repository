aaa020ac4d317b3f7eabb4569b0ffe7fd600ae29
Vinzenz Feenstra <vfeenstr@redhat.com>
2021-02-01 14:54:45 +0100

    s390 inhibit if /boot is not on a separate partition
    Previously, when the upgrade is performed on a s390x machine with zipl
    and /boot is not on a separate partition, the content of /boot got
    deleted during the upgrade process. Since the potential solution for
    this problem turned out to fail the upgrade for all s390 upgrades, it
    has been decided to create an actor that inhibits the upgrade for the
    detected scenario that caused the deletion of the /boot content.
    
    This patch introduces a new check actor that inhibits the upgrade when:
    - The architecture is s390X
    - /boot is not a mounted partition
    
    In case all of the above is true, the upgrade is inhibited with a
    report.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ repos/system_upgrade/el7toel8/actors/checknonmountboots390/libraries/checknonmountboots390.py
@@ -0,0 +1,27 @@
+import os
+
+from leapp import reporting
+from leapp.libraries.common.config import architecture
+
+
+def perform_check():
+    if not architecture.matches_architecture(architecture.ARCH_S390X):
+        return
+
+    if os.path.ismount('/boot'):
+        return
+
+    data = [
+        reporting.Title('Leapp detected known issue related to /boot on s390x architecture'),
+        reporting.Summary((
+            'Due to a bug in the Leapp code, there is a situation when the upgrade process'
+            ' removes content of /boot when the directory is not on a separate partition and'
+            ' the system is running on S390x architecture. To avoid this from happening, we'
+            ' are inhibiting the upgrade process in this release until the issue has been fixed.'
+        )),
+        reporting.Flags([reporting.Flags.INHIBITOR]),
+        reporting.Tags([reporting.Tags.FILESYSTEM, reporting.Tags.UPGRADE_PROCESS, reporting.Tags.BOOT]),
+        reporting.Severity(reporting.Severity.HIGH),
+    ]
+
+    reporting.create_report(data)
