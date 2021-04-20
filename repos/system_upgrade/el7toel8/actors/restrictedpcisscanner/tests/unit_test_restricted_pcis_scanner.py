a4eb4dd1d9e9bd541cf40af4b60aadd219b8bc83
Dominik Rehák <drehak@redhat.com>
2021-04-20 16:19:43 +0200

    Rewrite RestrictedPCIsScanner using read_or_fetch

--- repos/system_upgrade/el7toel8/actors/restrictedpcisscanner/tests/unit_test_restricted_pcis_scanner.py
+++ repos/system_upgrade/el7toel8/actors/restrictedpcisscanner/tests/unit_test_restricted_pcis_scanner.py
@@ -6,6 +6,7 @@ import pytest
 
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.actor import restrictedpcisscanner
+from leapp.libraries.common import fetch
 from leapp.libraries.common.testutils import CurrentActorMocked, produce_mocked
 from leapp.libraries.stdlib import api
 from leapp.models import RestrictedPCIDevice, RestrictedPCIDevices
@@ -106,82 +107,25 @@ def json_loads_mock_gen(returns_first, returns_second):
 
 @pytest.mark.parametrize(
     (
-        "mcs_socket",
-        "mcs_available",
-        "global_files_exists",
         "bad_data",
         "json_returns_first",
         "json_returns_second",
     ),
     [
-        # Should use global files
+        # Correct data
         (
-            "not existing host of the mcs",
-            False,
-            True,
             False,
             unsupported_driver_names_example,
             unsupported_pci_ids_example,
         ),
-        # Should use global files (because it is preferred)
+        # Bad data. Should raise StopActorExecutionError
         (
-            "existing host",
-            True,
-            True,
-            False,
-            unsupported_driver_names_example,
-            unsupported_pci_ids_example,
-        ),
-        # Should use mcs (calls are mocked)
-        (
-            "existing host",
-            True,
-            False,
-            False,
-            unsupported_driver_names_example,
-            unsupported_pci_ids_example,
-        ),
-        # Should raise StopActorExecutionError
-        (
-            "not existing host",
-            False,
-            False,
-            True,
-            unsupported_driver_names_example,
-            unsupported_pci_ids_example,
-        ),
-        # Should use global files, which has bad data. Should raise StopActorExecutionError
-        (
-            "not existing host of the mcs",
-            False,
-            True,
             True,
             some_very_bad_data,
             some_very_bad_data,
         ),
-        # Should use global files, which has bad data. Should raise StopActorExecutionError
+        # Bad data. Should raise StopActorExecutionError
         (
-            "not existing host of the mcs",
-            False,
-            True,
-            True,
-            some_bad_data,
-            some_bad_data,
-        ),
-        # Should use mcs, which has bad data. Should raise StopActorExecutionError
-        (
-            "existing host",
-            True,
-            False,
-            True,
-            some_very_bad_data,
-            some_very_bad_data,
-        ),
-        # Should use mcs, which has bad data. Should raise StopActorExecutionError
-        (
-            "existing host",
-            True,
-            False,
             True,
             some_bad_data,
             some_bad_data,
@@ -190,88 +134,31 @@ def json_loads_mock_gen(returns_first, returns_second):
 )
 def test_basic_restricted_pci_scanner(
     monkeypatch,
-    mcs_socket,
-    mcs_available,
-    global_files_exists,
     bad_data,
     json_returns_first,
     json_returns_second,
 ):
-    """Test main actor function."""
-    monkeypatch.setattr(
-        api,
-        "current_actor",
-        CurrentActorMocked(
-            envars={
-                "LEAPP_DEVEL_PCIS_HOST": mcs_socket,
-            }
-        ),
-    )
     monkeypatch.setattr(api, "produce", produce_mocked())
-    if global_files_exists:
-        json_loads_mock = json_loads_mock_gen(
-            json_returns_first, json_returns_second
-        )
-        monkeypatch.setattr(
-            restrictedpcisscanner.json,
-            "load",
-            value=next(json_loads_mock),
-        )
-        monkeypatch.setattr(
-            restrictedpcisscanner,
-            "get_restricted_pcis_offline",
-            value=partial(
-                restrictedpcisscanner.get_restricted_pcis_offline,
-                # no need to provide any data, because we mocked the
-                #   json.loads
-                _open=mock_open(read_data=""),
-            ),
-        )
-        if bad_data:
-            with pytest.raises(StopActorExecutionError):
-                restrictedpcisscanner.produce_restricted_pcis()
-            return
-        restrictedpcisscanner.produce_restricted_pcis()
+    json_loads_mock = json_loads_mock_gen(
+        json_returns_first, json_returns_second
+    )
+    monkeypatch.setattr(
+        fetch,
+        "read_or_fetch",
+        blank_fn,
+    )
+    monkeypatch.setattr(
+        restrictedpcisscanner.json,
+        "loads",
+        value=next(json_loads_mock),
+    )
 
-    if not global_files_exists and mcs_available:
-        pool_manager_mock = mock.Mock()
-        get_restricted_pcis_offline = mock.Mock(
-            side_effect=FileNotFoundError()
-        )
-        json_loads_mock = json_loads_mock_gen(
-            json_returns_first, json_returns_second
-        )
-        # even though FileNotFoundError raised as side effect, the error
-        # number is None (should be 2), so we have to mock the ENOENT
-        monkeypatch.setattr(errno, "ENOENT", value=None)
-
-        monkeypatch.setattr(
-            restrictedpcisscanner.urllib3,
-            "PoolManager",
-            value=pool_manager_mock,
-        )
-        monkeypatch.setattr(
-            restrictedpcisscanner.json,
-            "loads",
-            value=next(json_loads_mock),
-        )
-        monkeypatch.setattr(
-            restrictedpcisscanner,
-            "get_restricted_pcis_offline",
-            value=get_restricted_pcis_offline,
-        )
-
-        if bad_data:
-            with pytest.raises(StopActorExecutionError):
-                restrictedpcisscanner.produce_restricted_pcis()
-            return
-        restrictedpcisscanner.produce_restricted_pcis()
-
-        pool_manager_mock.assert_called_once()
-    if not global_files_exists and not mcs_available:
+    if bad_data:
         with pytest.raises(StopActorExecutionError):
             restrictedpcisscanner.produce_restricted_pcis()
         return
+
+    restrictedpcisscanner.produce_restricted_pcis()
     assert len(api.produce.model_instances) == 1
     assert isinstance(api.produce.model_instances[0], RestrictedPCIDevices)
     assert isinstance(
