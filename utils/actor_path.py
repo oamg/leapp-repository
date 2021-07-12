03d6b0b85de5aec3ed3a1a17e55ea469007430e4
Petr Stodulka <pstodulk@redhat.com>
2021-07-12 19:34:15 +0200

    Tests: improve search of an ACTOR: support multiple leapp repos
    With the introduced common & el8toel9 repositories, the script searching
    the ACTOR returns only the first discovered ACTOR of the given name.
    However, it is expected that actors of the same name could exists
    in el7toel8 and el8toel9 repositories as these two are independent
    on each other. To fix this, we use for now a private method
    _lookup_actors() introduced by:
         https://github.com/oamg/leapp/pull/722
    Currently it's not possible to specify which repository should be
    tested. Right now all discovered actors of the given name are
    going to be tested when discovered.
    
    As well, the original error messages from the utils/actor_path.py
    script has been print on stdout which had several negative effects:
      - the error messages has not been printed anywhere
      - error messages has been executed in the Makefile as a shell cmd
      - printed errors contained just the first word of the error msgs
    
    This is kind of because the script in the Makefile ignores the exit
    code of the utils script. So I have moved the error messages into
    the stderr to print them always + added print of a special string
    to stdout to ensure the execution of tests will be stopped
    as expected (because the file names like the err msg will not exist
    the most probably).

--- utils/actor_path.py
+++ utils/actor_path.py
@@ -3,8 +3,14 @@ import sys
 
 from leapp.repository.scan import find_and_scan_repositories
 
+def err_exit():
+    # We want to be sure that `make test` (test_no_lint) will stop when expected
+    # actor is not found and want to be sure that users will not overlook error
+    # messages. This print will easily resolve the problem
+    sys.stdout.write('ERROR:__read_error_messages_above_this_one_on_stderr__')
+    sys.exit(1)
 
-if __name__ == '__main__':
+def main():
     logging.basicConfig(level=logging.INFO, filename='/dev/null')
     logger = logging.getLogger('run_pytest.py')
 
@@ -12,12 +18,24 @@ if __name__ == '__main__':
     repos = find_and_scan_repositories(BASE_REPO, include_locals=True)
     repos.load()
     if len(sys.argv) > 1:
-        actor = repos.lookup_actor(sys.argv[1])
-        if actor:
-            print(actor.full_path)
-        else:
-            sys.stdout.write('No actor found for search "{}"\n'.format(sys.argv[1]))
-            sys.exit(1)
+        actors = repos._lookup_actors(sys.argv[1])
+        if not actors:
+            sys.stderr.write('ERROR: No actor found for search "{}"\n'.format(sys.argv[1]))
+            err_exit()
+        print(' '.join([actor.full_path for actor in actors]))
     else:
-        sys.stdout.write('Missing commandline argument\n')
-        sys.exit(1)
+        sys.stderr.write('ERROR: Missing commandline argument\n')
+        err_exit()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        # Make a generic exception as in case of error, without the explicit
+        # sys.exit(1) call running of tests continues with all actors always
+        # and we expect that many people will be affected unless they installed
+        sys.stderr.write('ERROR: Unknown error: {}\n'.format(e))
+        sys.stderr.write('ERROR: Possibly you need newer version of the leapp framework\n')
+        sys.stderr.write('ERROR:   e.g.: rm -rf .tut && make install-deps-fedora\n')
+        err_exit()
