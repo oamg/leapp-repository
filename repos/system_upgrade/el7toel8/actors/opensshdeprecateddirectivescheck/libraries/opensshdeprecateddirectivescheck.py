7093823372e0bc889a63d97811fc6c773ac41da2
MichalHe <mhecko@redhat.com>
2021-08-02 11:01:48 +0200

    Change the report format according to the review suggestions.

--- repos/system_upgrade/el7toel8/actors/opensshdeprecateddirectivescheck/libraries/opensshdeprecateddirectivescheck.py
+++ repos/system_upgrade/el7toel8/actors/opensshdeprecateddirectivescheck/libraries/opensshdeprecateddirectivescheck.py
@@ -13,14 +13,19 @@ def inhibit_if_deprecated_directives_used(ssh_config_msg):
         for deprecated_directive in ssh_config_msg.deprecated_directives:
             deprecated_directives_report_text += '{0}{1}'.format(LIST_SEPARATOR_FMT, deprecated_directive)
 
+        sshd_config_path = '/etc/ssh/sshd_config'
         reporting.create_report([
             reporting.Title('A deprecated directive in the sshd configuration'),
             reporting.Summary(
-                'The following deprecated directives were found in the sshd configuration:{0}'
-                .format(deprecated_directives_report_text)
+                'The following deprecated directives were found in the sshd configuration file {0}:{1}'
+                .format(sshd_config_path, deprecated_directives_report_text)
             ),
             reporting.Severity(reporting.Severity.HIGH),
             reporting.Tags([reporting.Tags.SERVICES]),
             reporting.Flags([reporting.Flags.INHIBITOR]),
-            reporting.Remediation(hint='Remove the deprecated directives from the sshd configuration.')
+            reporting.RelatedResource('file', sshd_config_path),
+            reporting.Remediation(
+                hint='Remove the deprecated directives from the sshd configuration.',
+                commands=[["sed", "-i", "/^\\s*ShowPatchLevel.*$/Id", sshd_config_path]]
+            )
         ])
