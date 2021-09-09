0185c064f9bd05fe6ec6b3ae62a68a73707f8a47
Dan Horák <dan@danny.cz>
2021-09-09 11:40:51 +0200

    checkcpu: RHEL-9 minimal supported IBM Z system is z14
    Signed-off-by: Dan Horák <dan@danny.cz>

--- repos/system_upgrade/el8toel9/actors/checkcpu/actor.py
+++ repos/system_upgrade/el8toel9/actors/checkcpu/actor.py
@@ -9,9 +9,9 @@ class CheckCPU(Actor):
     Check whether the CPU is supported by the target system. Inhibit upgrade if not.
 
     Currently we know just about cases with s390x where the set of CPUs supported
-    by RHEL 8 is subset of CPUs supported on RHEL 7. We can detect such cases based
+    by RHEL 9 is subset of CPUs supported on RHEL 8. We can detect such cases based
     on the machine field inside the /proc/cpuinfo file. expected values of the
-    field on supported machines are: 2964, 2965, 3906, 3907, 8561, 8562.
+    field on supported machines are: 3906, 3907, 8561, 8562.
     """
 
     name = "checkcpu"
--- repos/system_upgrade/el8toel9/actors/checkcpu/libraries/cpu.py
+++ repos/system_upgrade/el8toel9/actors/checkcpu/libraries/cpu.py
@@ -5,7 +5,7 @@ from leapp.libraries.common.config import architecture
 from leapp.libraries.stdlib import api
 from leapp.models import CPUInfo
 
-SUPPORTED_MACHINE_TYPES = [2964, 2965, 3906, 3907, 8561, 8562]
+SUPPORTED_MACHINE_TYPES = [3906, 3907, 8561, 8562]
 
 
 def process():
@@ -22,8 +22,8 @@ def process():
     if cpuinfo.machine_type not in SUPPORTED_MACHINE_TYPES:
         summary = ("The system is not possible to upgrade because of unsupported"
                    " type of the processor. Based on the official documentation,"
-                   " z13, z14 and z15 processors are supported on the Red Hat Enterprise"
-                   " Linux 8 system for the IBM Z architecture. The supported processors"
+                   " z14 and z15 processors are supported on the Red Hat Enterprise"
+                   " Linux 9 system for the IBM Z architecture. The supported processors"
                    " have machine types {}. The detected machine type of the CPU is '{}'."
                    .format(", ".join([str(i) for i in SUPPORTED_MACHINE_TYPES]), cpuinfo.machine_type))
         report = [
@@ -37,4 +37,5 @@ def process():
                 url=("https://access.redhat.com/ecosystem/hardware/#/search?p=1&"
                      "c_version=Red%20Hat%20Enterprise%20Linux%208&ch_architecture=s390x"))
         ]
+        # FIXME(dhorak): update the URL to the document once it exists
         reporting.create_report(report)
