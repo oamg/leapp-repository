81fd63fafcbad23c9f29bba474713dabece0cd6c
Joe Shimkus <jshimkus@redhat.com>
2021-09-09 10:23:57 -0400

    Add framework for VDO migration

---
+++ repos/system_upgrade/el8toel9/actors/checkvdo/actor.py
@@ -0,0 +1,18 @@
+from leapp.actors import Actor
+from leapp.libraries.actor.checkvdo import check_vdo
+from leapp.models import Report
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+class CheckVdo(Actor):
+    """
+    Check if vdo instances need to be migrated to lvm management.
+    """
+
+    name = 'check_vdo'
+    consumes = ()
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        self.produce(check_vdo())
