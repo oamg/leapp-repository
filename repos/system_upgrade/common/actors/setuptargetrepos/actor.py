ec790a55a152c1ce1285079633a1e6625f31a86a
Petr Stodulka <pstodulk@redhat.com>
2021-09-11 16:10:48 +0200

    setuptargetrepos: adapt to the new repomap msgs and introduce new decision logic
    This is analogy to the previous commit. In this case we update the
    actor responsible for the 'final' mapping to the target repositories.
    Actor has been refactored completely to make it more readable
    with several bugfixes (e.g. removal of death code, ..).
    
    The original decision logic for repomapping was this:
       1. each src repoid map to ekvivalent target repoid + ga repoid
          (basically to each target repoid mentioned in the repomap data)
       2. blacklisted drop blacklisted repositories
       3. add all required custom target repositories
    
    IOW, it was typical that systems with EUS repositories expected EUS+GA
    repositories:
        src repos       || dst repos
        ----------------||---------------------
        EUSbase         || EUSbaseos, GAbaseos, EUSappstream, GAappstream
        EUSbase, GAbase || EUSbaseos, GAbaseos, EUSappstream, GAappstream
        GAbase          || GAbase, GAappstream
        ...
    
    The new logic works with families of repositories (based on pesids),
    so we decide to pick just the best fitting repository from each mapped
    family. So the table above is going to be changed like this:
        src repos       || dst repos
        ----------------||---------------------
        EUSbase         || EUSbaseos, EUSappstream
        EUSbase, GAbase || EUSbaseos, EUSappstream
        GAbase          || GAbase, GAappstream
        ...
    Each target repo family (pesid) has just one best candidate
    repository (or nothing in case of issues in mapping data).
    The priority of repositories is
        beta < ga < all_else
    where all else is e.g. eus, tus, aus, ... as we do not expect to have
    multiple premium channels available & enabled on one machine.
    In case of mapping between repositories have very special channels
    which does not follow typical channels (e.g. extras is just one, doesn't
    matter whether you have eus, aus, ga, ...it's same everywhere) or in
    case the target repo family doesn't contain a special channel (yet?)
    there is always fallback to default channels. In this case, we consider
    default channels to be get based on the top prio repository from base
    / baseos families and always the 'ga'. So e.g. in case an EUS repository
    is mapped to a family that has just a repo with the 'ga' channel,
    this one repository is going to be used for the fallback.
    
    Co-authored-by: MichalHe <mhecko@redhat.com>

--- repos/system_upgrade/common/actors/setuptargetrepos/actor.py
+++ repos/system_upgrade/common/actors/setuptargetrepos/actor.py
@@ -1,22 +1,16 @@
-import platform
-
 from leapp.actors import Actor
-from leapp.models import (CustomTargetRepository, RepositoriesBlacklisted, RepositoriesFacts, RepositoriesMap,
-                          RepositoriesSetupTasks, RHELTargetRepository, SkippedRepositories, TargetRepositories,
-                          UsedRepositories)
+from leapp.libraries.actor import setuptargetrepos
+from leapp.models import (
+    CustomTargetRepository,
+    RepositoriesBlacklisted,
+    RepositoriesFacts,
+    RepositoriesMapping,
+    RepositoriesSetupTasks,
+    SkippedRepositories,
+    TargetRepositories,
+    UsedRepositories,
+)
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
-
-
-EXTRAS_REPOS = set([
-    'rhel-7-for-arm-64-extras-rhui-rpms',
-    'rhel-7-for-arm-64-extras-rpms',
-    'rhel-7-for-power-9-extras-rpms',
-    'rhel-7-for-power-le-extras-rpms',
-    'rhel-7-for-system-z-a-extras-rpms',
-    'rhel-7-for-system-z-extras-rpms',
-    'rhel-7-server-extras-rpms',
-    'rhel-7-server-rhui-extras-rpms',
-])
 
 
 class SetupTargetRepos(Actor):
@@ -31,7 +25,7 @@ class SetupTargetRepos(Actor):
     name = 'setuptargetrepos'
     consumes = (CustomTargetRepository,
                 RepositoriesSetupTasks,
-                RepositoriesMap,
+                RepositoriesMapping,
                 RepositoriesFacts,
                 RepositoriesBlacklisted,
                 UsedRepositories)
@@ -39,67 +33,4 @@ class SetupTargetRepos(Actor):
     tags = (IPUWorkflowTag, FactsPhaseTag)
 
     def process(self):
-        custom_repos = []
-        for repo in self.consume(CustomTargetRepository):
-            custom_repos.append(repo)
-
-        enabled_repos = set()
-        for repos in self.consume(RepositoriesFacts):
-            for repo_file in repos.repositories:
-                for repo in repo_file.data:
-                    if repo.enabled:
-                        enabled_repos.add(repo.repoid)
-
-        rhel_repos = []
-        mapped_repos = set()
-        for repos_map in self.consume(RepositoriesMap):
-            for repo_map in repos_map.repositories:
-                # Check if repository map architecture matches system architecture
-                if platform.machine() != repo_map.arch:
-                    continue
-                if repo_map.from_repoid in EXTRAS_REPOS:
-                    # skip mapping for the Extras repositories to deal with
-                    # E4S -> std channel mapping
-                    # TODO(pstodulk): this is just workaround that will be
-                    # resolved in future.
-                    continue
-
-                mapped_repos.add(repo_map.from_repoid)
-                if repo_map.from_repoid in enabled_repos:
-                    rhel_repos.append(RHELTargetRepository(repoid=repo_map.to_repoid))
-
-        skipped_repos = enabled_repos.difference(mapped_repos)
-
-        used = {}
-        for used_repos in self.consume(UsedRepositories):
-            for used_repo in used_repos.repositories:
-                used[used_repo.repository] = used_repo.packages
-                for repo in repo_file.data:
-                    enabled_repos.add(repo.repoid)
-
-        # TODO(pstodulk): the part of the workaround mentioned above..
-        skipped_repos = skipped_repos.intersection(set(used.keys())) - EXTRAS_REPOS
-
-        if skipped_repos:
-            pkgs = set()
-            for repo in skipped_repos:
-                pkgs.update(used[repo])
-            self.produce(SkippedRepositories(repos=list(skipped_repos), packages=list(pkgs)))
-
-        for task in self.consume(RepositoriesSetupTasks):
-            for repo in task.to_enable:
-                rhel_repos.append(RHELTargetRepository(repoid=repo))
-
-        repos_blacklisted = set()
-        for blacklist in self.consume(RepositoriesBlacklisted):
-            repos_blacklisted.update(blacklist.repoids)
-        rhel_repos = [repo for repo in rhel_repos if repo.repoid not in repos_blacklisted]
-        custom_repos = [repo for repo in custom_repos if repo.repoid not in repos_blacklisted]
-
-        self.produce(TargetRepositories(
-            rhel_repos=rhel_repos,
-            custom_repos=custom_repos,
-        ))
-
-        # TODO: Some informational messages would be added for the report and
-        # + logs, so we and user will know exactly what is going on.
+        setuptargetrepos.process()
