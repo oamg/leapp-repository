c7d6f5a856cc8e3cabbe14a1118869b03389e23b
Petr Stodulka <pstodulk@redhat.com>
2021-09-11 15:53:34 +0200

    Introduce RepositoriesMapping model and drop RepositoriesMap model
    The new model is going to replace the original RepositoriesMap model
    that has been used for the old repomap format data. As we are going
    to switch into the new format, we are not able to use the old
    models as it's incompatible with other use-cases we want to cover
    with it.
    
    The new model reflects data structure used in the input data file,
    however it's already simplified as some pieces of data are needed
    just for the scanner actor (e.g. the upgrade path is known to all
    actors and doesn't have to be anyhow duplicated inside the repomap
    data models).
    
    Also we drop the original RepositoriesMap and RepositoryMap models
    because:
      * they are not used anymore by any actors (as these have been
        replaced by new models)
      * we cannot deprecate these models as from the nature of the change,
        we cannot keep the functionality around nor produce the original
        message as data in such a msg could not provide the same meaning.
        That means, we cannot say strictly that repoidX will be mapped
        to repoidY as the logic of mapping is going to be changed
        completely.
    
    Adding docstrings to inform people the new models are not supposed to
    be consumed by other (custom) actors as most likely we drop them
    in close future as well.

--- repos/system_upgrade/common/models/repositoriesmap.py
+++ repos/system_upgrade/common/models/repositoriesmap.py
@@ -2,34 +2,93 @@ from leapp.models import Model, fields
 from leapp.topics import TransactionTopic
 
 
-class RepositoryMap(Model):
-    """
-    Mapping between repositories to be used during upgrade.
+class RepoMapEntry(Model):
+    topic = TransactionTopic
 
-    Mapping of current system repository to target system version repositories to determine which
-    ones should be enabled for the correct upgrade process.
+    source = fields.String()
+    """The source PES id."""
+
+    target = fields.List(fields.String())
+    """List of target PES ids"""
+
+
+class PESIDRepositoryEntry(Model):
+    """
+    Represent metadata about particular repository.
+
+    The metadata are used to identify purpose and nature of the repository.
+
+    Warning: This model is not expected to be consumed/produced by any actor
+    directly. As well, it is not covered by deprecation process and can be
+    changed or removed any time.
     """
 
     topic = TransactionTopic
 
-    from_repoid = fields.String()
-    """Source RHEL repoid as present in the Red Hat CDN"""
-    to_repoid = fields.String()
-    """Target RHEL repoid as present in the Red Hat CDN"""
-    to_pes_repo = fields.String()
-    """Target RHEL repo name as used in the Package Evolution Service database"""
-    from_minor_version = fields.String()
-    """To which source RHEL minor versions the mapping relates to"""
-    to_minor_version = fields.String()
-    """To which target RHEL minor versions the mapping relates to"""
-    arch = fields.String()
-    """CPU architecture the mapping relates to"""
-    repo_type = fields.StringEnum(choices=['rpm', 'srpm', 'debuginfo'])
-    """Type of content the mapped repos hold"""
+    pesid = fields.String()
+    """
+    The PES id of the repository.
+
+    The PES id indicate family of YUM repositories. E.g. rhel8-BaseOS covers
+    variants of BaseOS YUM repositories for all channels, architectures,
+    RHUI, etc, which have basically same purpose.
+    """
+
+    major_version = fields.String()
+    """
+    The major version of OS.
+
+    E.g. for RHEL 7.9 the major version is 7. Since we work with versions
+    as with strings throughout the whole codebase, we keep this data type here too.
+    """
+
+    repoid = fields.String()
+    """
+    The repository ID which identifies the repository from YUM/DNF POV.
+    """
+
+    arch = fields.StringEnum(['x86_64', 's390x', 'ppc64le', 'aarch64'])
+    """
+    The architecture for which the repository is delivered.
+    """
+
+    repo_type = fields.StringEnum(['rpm', 'debug', 'srpm'])
+    """
+    The repository type.
+
+    In our case, we usually map just repositories with the 'rpm' type, so
+    usually you can see just this one, but the others are possible to add
+    too.
+    """
+
+    channel = fields.StringEnum(['ga', 'tuv', 'e4s', 'eus', 'aus', 'beta'])
+    """
+    The 'channel' of the repository.
+
+    The 'channel' could be a little bit inaccurate term, but let's use it in
+    this project. The standard repositories has 'ga' channel. 'beta'
+    repositories are unsupported for IPU, however they are useful for testing
+    purposes. The other channels indicate premium repositories.
+    """
+
+    rhui = fields.StringEnum(['', 'aws', 'azure'])
+    """
+    Indicate whether the repository is deliver for RHUI and which one.
+
+    For non-rhui systems: empty string
+    For AWS or Azure: 'aws' / 'azure'
+    """
 
 
-class RepositoriesMap(Model):
+class RepositoriesMapping(Model):
+    """
+    Private model containing information about mapping between repositories.
+
+    Warning: We expect to be only consumers of this model.
+    This means the model is not covered by deprecation process and can be
+    changed or removed any time.
+    """
     topic = TransactionTopic
 
-    repositories = fields.List(fields.Model(RepositoryMap), default=[])
-    """List of source RHEL repo <-> target RHEL repo mappings"""
+    mapping = fields.List(fields.Model(RepoMapEntry), default=[])
+    repositories = fields.List(fields.Model(PESIDRepositoryEntry), default=[])
