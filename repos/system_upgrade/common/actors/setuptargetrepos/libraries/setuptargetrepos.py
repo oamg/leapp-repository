854cf657baf96d4536df5ae9bf3de6ff06218705
Petr Stodulka <pstodulk@redhat.com>
2021-09-11 16:10:48 +0200

    setuptargetrepos: adapt to the new repomap msgs and introduce new decision logic
    This is analogy to the previous commit. In this case we update the
    actor responsible for the 'final' mapping to the target repositories.
    Actor has been refactored completely to make it more readable
    with several bugfixes (e.g. removal of death code, ..).
    
    The original decision logic for repomapping was this:
       1. each src repoid map to ekvivalent target repoid + ga repoid
          (basically to each target repoid mentioned in the repomap data)
       2. blacklisted drop blacklisted repositories
       3. add all required custom target repositories
    
    IOW, it was typical that systems with EUS repositories expected EUS+GA
    repositories:
        src repos       || dst repos
        ----------------||---------------------
        EUSbase         || EUSbaseos, GAbaseos, EUSappstream, GAappstream
        EUSbase, GAbase || EUSbaseos, GAbaseos, EUSappstream, GAappstream
        GAbase          || GAbase, GAappstream
        ...
    
    The new logic works with families of repositories (based on pesids),
    so we decide to pick just the best fitting repository from each mapped
    family. So the table above is going to be changed like this:
        src repos       || dst repos
        ----------------||---------------------
        EUSbase         || EUSbaseos, EUSappstream
        EUSbase, GAbase || EUSbaseos, EUSappstream
        GAbase          || GAbase, GAappstream
        ...
    Each target repo family (pesid) has just one best candidate
    repository (or nothing in case of issues in mapping data).
    The priority of repositories is
        beta < ga < all_else
    where all else is e.g. eus, tus, aus, ... as we do not expect to have
    multiple premium channels available & enabled on one machine.
    In case of mapping between repositories have very special channels
    which does not follow typical channels (e.g. extras is just one, doesn't
    matter whether you have eus, aus, ga, ...it's same everywhere) or in
    case the target repo family doesn't contain a special channel (yet?)
    there is always fallback to default channels. In this case, we consider
    default channels to be get based on the top prio repository from base
    / baseos families and always the 'ga'. So e.g. in case an EUS repository
    is mapped to a family that has just a repo with the 'ga' channel,
    this one repository is going to be used for the fallback.
    
    Co-authored-by: MichalHe <mhecko@redhat.com>

---
+++ repos/system_upgrade/common/actors/setuptargetrepos/libraries/setuptargetrepos.py
@@ -0,0 +1,136 @@
+
+from leapp.libraries.actor import setuptargetrepos_repomap
+from leapp.libraries.common.config.version import get_source_major_version
+from leapp.libraries.stdlib import api
+from leapp.models import (
+    CustomTargetRepository,
+    RHELTargetRepository,
+    RepositoriesBlacklisted,
+    RepositoriesFacts,
+    RepositoriesMapping,
+    RepositoriesSetupTasks,
+    SkippedRepositories,
+    TargetRepositories,
+    UsedRepositories,
+)
+
+
+def _get_enabled_repoids():
+    """
+    Collects repoids of all enabled repositories on the source system.
+
+    :param repositories_facts: Iterable of RepositoriesFacts containing info about repositories on the source system.
+    :returns: Set of all enabled repository IDs present on the source system.
+    :rtype: Set[str]
+    """
+    enabled_repoids = set()
+    for repos in api.consume(RepositoriesFacts):
+        for repo_file in repos.repositories:
+            for repo in repo_file.data:
+                if repo.enabled:
+                    enabled_repoids.add(repo.repoid)
+    return enabled_repoids
+
+
+def _get_blacklisted_repoids():
+    repos_blacklisted = set()
+    for blacklist in api.consume(RepositoriesBlacklisted):
+        repos_blacklisted.update(blacklist.repoids)
+    return repos_blacklisted
+
+
+def _get_custom_target_repos():
+    custom_repos = []
+    for repo in api.consume(CustomTargetRepository):
+        custom_repos.append(repo)
+    return custom_repos
+
+
+def _get_used_repo_dict():
+    """
+    Return dict: {used_repoid: [installed_packages]}
+    """
+    used = {}
+    for used_repos in api.consume(UsedRepositories):
+        for used_repo in used_repos.repositories:
+            used[used_repo.repository] = used_repo.packages
+    return used
+
+
+def _setup_repomap_handler(src_repoids):
+    repo_mappig_msg = next(api.consume(RepositoriesMapping), RepositoriesMapping())
+    repomap = setuptargetrepos_repomap.RepoMapDataHandler(repo_mappig_msg)
+    # TODO(pstodulk): what about skip this completely and keep the default 'ga'..?
+    default_channels = setuptargetrepos_repomap.get_default_repository_channels(repomap, src_repoids)
+    repomap.set_default_channels(default_channels)
+    return repomap
+
+
+def _get_mapped_repoids(repomap, src_repoids):
+    mapped_repoids = set()
+    src_maj_ver = get_source_major_version()
+    for repoid in src_repoids:
+        if repomap.get_pesid_repo_entry(repoid, src_maj_ver):
+            mapped_repoids.add(repoid)
+    return mapped_repoids
+
+
+def process():
+    # load all data / messages
+    used_repoids_dict = _get_used_repo_dict()
+    enabled_repoids = _get_enabled_repoids()
+    excluded_repoids = _get_blacklisted_repoids()
+    custom_repos = _get_custom_target_repos()
+
+    # TODO(pstodulk): isn't that a potential issue that we map just enabled repos
+    # instead of enabled + used repos??
+    # initialise basic data
+    repomap = _setup_repomap_handler(enabled_repoids)
+    mapped_repoids = _get_mapped_repoids(repomap, enabled_repoids)
+    skipped_repoids = enabled_repoids & set(used_repoids_dict.keys()) - mapped_repoids
+
+    # Now get the info what should be the target RHEL repositories
+    expected_repos = repomap.get_expected_target_pesid_repos(enabled_repoids)
+    target_rhel_repoids = set()
+    for target_pesid, target_pesidrepo in expected_repos.items():
+        if not target_pesidrepo:
+            # With the original repomap data, this should not happen (this should
+            # currently point to a problem in our data
+            # TODO(pstodulk): add report? inhibitor? what should be in the report?
+            api.current_logger().error(
+                'Missing target repository from the {} family (PES ID).'
+                .format(target_pesid)
+            )
+            continue
+        if target_pesidrepo.repoid in excluded_repoids:
+            api.current_logger().debug('Skipping the {} repo (excluded).'.format(target_pesidrepo.repoid))
+            continue
+        target_rhel_repoids.add(target_pesidrepo.repoid)
+
+    # FIXME: this could possibly result into a try to enable multiple repositories
+    # from the same family (pesid). But unless we have a bug in previous actors,
+    # it should not happen :) it's not blocker error anyway, so survive it.
+    # - We expect to deliver the fix as part of the refactoring when we merge
+    # setuptargetrepos & peseventsscanner actors together (+ blacklistrepos?)
+    for task in api.consume(RepositoriesSetupTasks):
+        for repo in task.to_enable:
+            if repo in excluded_repoids:
+                api.current_logger().debug('Skipping the {} repo from setup task (excluded).'.format(repo))
+                continue
+            target_rhel_repoids.add(repo)
+
+    # create the final lists and sort them (for easier testing)
+    rhel_repos = [RHELTargetRepository(repoid=repoid) for repoid in sorted(target_rhel_repoids)]
+    custom_repos = [repo for repo in custom_repos if repo.repoid not in excluded_repoids]
+    custom_repos = sorted(custom_repos, key=lambda x: x.repoid)
+
+    if skipped_repoids:
+        pkgs = set()
+        for repo in skipped_repoids:
+            pkgs.update(used_repoids_dict[repo])
+        api.produce(SkippedRepositories(repos=sorted(skipped_repoids), packages=sorted(pkgs)))
+
+    api.produce(TargetRepositories(
+        rhel_repos=rhel_repos,
+        custom_repos=custom_repos,
+    ))
--- repos/system_upgrade/common/actors/setuptargetrepos/tests/test_setuptargetrepos.py
+++ repos/system_upgrade/common/actors/setuptargetrepos/tests/test_setuptargetrepos.py
@@ -1,14 +1,42 @@
+import pytest
+
 from leapp.libraries import stdlib
-from leapp.models import CustomTargetRepository, TargetRepositories, RepositoryData, \
-    RepositoryFile, RepositoriesFacts, RepositoryMap, RepositoriesMap, RepositoriesSetupTasks, \
+from leapp.libraries.actor import setuptargetrepos
+from leapp.libraries.common.testutils import CurrentActorMocked, produce_mocked
+from leapp.libraries.stdlib import api
+from leapp.models import (
+    CustomTargetRepository,
+    TargetRepositories,
+    RepositoryData,
+    RepositoryFile,
+    RepositoriesFacts,
+    PESIDRepositoryEntry,
+    RepoMapEntry,
+    RepositoriesMapping,
+    RepositoriesSetupTasks,
     RepositoriesBlacklisted
+)
 
 
-def test_minimal_execution(current_actor_context):
-    current_actor_context.run()
+def test_minimal_execution(monkeypatch):
+    """
+    Tests whether the actor does not fail if no messages except the RepositoriesMapping are provided.
+    """
+    msgs = [
+        RepositoriesMapping(mapping=[], repositories=[])
+    ]
+
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(msgs=msgs))
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+
+    setuptargetrepos.process()
 
 
-def test_custom_repos(current_actor_context):
+def test_custom_repos(monkeypatch):
+    """
+    Tests whether the CustomRepos provided to the actor are propagated to the TargetRepositories after
+    blacklist filtering is applied on them.
+    """
     custom = CustomTargetRepository(repoid='rhel-8-server-rpms',
                                     name='RHEL 8 Server (RPMs)',
                                     baseurl='https://.../dist/rhel/server/8/os',
@@ -21,74 +49,116 @@ def test_custom_repos(current_actor_context):
 
     repos_blacklisted = RepositoriesBlacklisted(repoids=['rhel-8-blacklisted-rpms'])
 
-    current_actor_context.feed(custom)
-    current_actor_context.feed(blacklisted)
-    current_actor_context.feed(repos_blacklisted)
-    current_actor_context.run()
+    repositories_mapping = RepositoriesMapping(
+        mapping=[],
+        repositories=[]
+    )
 
-    assert current_actor_context.consume(TargetRepositories)
+    msgs = [custom, blacklisted, repos_blacklisted, repositories_mapping]
 
-    custom_repos = current_actor_context.consume(TargetRepositories)[0].custom_repos
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(msgs=msgs))
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+
+    setuptargetrepos.process()
+
+    assert api.produce.called
+
+    custom_repos = api.produce.model_instances[0].custom_repos
     assert len(custom_repos) == 1
     assert custom_repos[0].repoid == 'rhel-8-server-rpms'
 
 
-def test_repositories_setup_tasks(current_actor_context):
+def test_repositories_setup_tasks(monkeypatch):
+    """
+    Tests whether the actor propagates repositories received via a RepositoriesSetupTasks message
+    to the resulting TargetRepositories (and blacklist filtering is applied to them).
+    """
     repositories_setup_tasks = RepositoriesSetupTasks(to_enable=['rhel-8-server-rpms',
                                                                  'rhel-8-blacklisted-rpms'])
-
     repos_blacklisted = RepositoriesBlacklisted(repoids=['rhel-8-blacklisted-rpms'])
+    repositories_mapping = RepositoriesMapping(mapping=[], repositories=[])
+    msgs = [repositories_setup_tasks, repos_blacklisted, repositories_mapping]
 
-    current_actor_context.feed(repositories_setup_tasks)
-    current_actor_context.feed(repos_blacklisted)
-    current_actor_context.run()
-    assert current_actor_context.consume(TargetRepositories)
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(msgs=msgs))
+    monkeypatch.setattr(api, 'produce', produce_mocked())
 
-    rhel_repos = current_actor_context.consume(TargetRepositories)[0].rhel_repos
+    setuptargetrepos.process()
+
+    assert api.produce.called
+
+    rhel_repos = api.produce.model_instances[0].rhel_repos
     assert len(rhel_repos) == 1
     assert rhel_repos[0].repoid == 'rhel-8-server-rpms'
 
 
-def test_repos_mapping(current_actor_context):
+def test_repos_mapping(monkeypatch):
+    """
+    Tests whether actor correctly determines what repositories should be enabled on target based
+    on the information about what repositories are enabled on the source system using
+    the RepositoriesMapping information.
+    """
     repos_data = [
         RepositoryData(repoid='rhel-7-server-rpms', name='RHEL 7 Server'),
         RepositoryData(repoid='rhel-7-blacklisted-rpms', name='RHEL 7 Blacklisted')]
+
     repos_files = [RepositoryFile(file='/etc/yum.repos.d/redhat.repo', data=repos_data)]
     facts = RepositoriesFacts(repositories=repos_files)
-    arch = stdlib.run(['uname', '-m'])['stdout'].strip()
 
-    mapping = [RepositoryMap(from_repoid='rhel-7-server-rpms',
-                             to_repoid='rhel-8-for-{}-baseos-htb-rpms'.format(arch),
-                             to_pes_repo='rhel8-baseos',
-                             from_minor_version='all',
-                             to_minor_version='all',
-                             arch=arch,
-                             repo_type='rpm'),
-               RepositoryMap(from_repoid='rhel-7-server-rpms',
-                             to_repoid='rhel-8-for-{}-appstream-htb-rpms'.format(arch),
-                             to_pes_repo='rhel8-appstream',
-                             from_minor_version='all',
-                             to_minor_version='all',
-                             arch=arch,
-                             repo_type='rpm'),
-               RepositoryMap(from_repoid='rhel-7-blacklist-rpms',
-                             to_repoid='rhel-8-blacklist-rpms',
-                             to_pes_repo='rhel8-blacklist',
-                             from_minor_version='all',
-                             to_minor_version='all',
-                             arch=arch,
-                             repo_type='rpm')]
-    repos_map = RepositoriesMap(repositories=mapping)
+    repomap = RepositoriesMapping(
+        mapping=[RepoMapEntry(source='rhel7-base', target=['rhel8-baseos', 'rhel8-appstream', 'rhel8-blacklist'])],
+        repositories=[
+            PESIDRepositoryEntry(
+                pesid='rhel7-base',
+                repoid='rhel-7-server-rpms',
+                major_version='7',
+                arch='x86_64',
+                repo_type='rpm',
+                channel='ga',
+                rhui=''
+            ),
+            PESIDRepositoryEntry(
+                pesid='rhel8-baseos',
+                repoid='rhel-8-for-x86_64-baseos-htb-rpms',
+                major_version='8',
+                arch='x86_64',
+                repo_type='rpm',
+                channel='ga',
+                rhui=''
+            ),
+            PESIDRepositoryEntry(
+                pesid='rhel8-appstream',
+                repoid='rhel-8-for-x86_64-appstream-htb-rpms',
+                major_version='8',
+                arch='x86_64',
+                repo_type='rpm',
+                channel='ga',
+                rhui=''
+            ),
+            PESIDRepositoryEntry(
+                pesid='rhel8-blacklist',
+                repoid='rhel-8-blacklisted-rpms',
+                major_version='8',
+                arch='x86_64',
+                repo_type='rpm',
+                channel='ga',
+                rhui=''
+            ),
+        ]
+    )
 
     repos_blacklisted = RepositoriesBlacklisted(repoids=['rhel-8-blacklisted-rpms'])
 
-    current_actor_context.feed(facts)
-    current_actor_context.feed(repos_map)
-    current_actor_context.feed(repos_blacklisted)
-    current_actor_context.run()
-    assert current_actor_context.consume(TargetRepositories)
+    msgs = [facts, repomap, repos_blacklisted]
 
-    rhel_repos = current_actor_context.consume(TargetRepositories)[0].rhel_repos
+    monkeypatch.setattr(api, 'current_actor', CurrentActorMocked(msgs=msgs))
+    monkeypatch.setattr(api, 'produce', produce_mocked())
+
+    setuptargetrepos.process()
+    assert api.produce.called
+
+    rhel_repos = api.produce.model_instances[0].rhel_repos
     assert len(rhel_repos) == 2
-    assert {repo.repoid for repo in rhel_repos} == {'rhel-8-for-x86_64-baseos-htb-rpms',
-                                                    'rhel-8-for-x86_64-appstream-htb-rpms'}
+
+    produced_rhel_repoids = {repo.repoid for repo in rhel_repos}
+    expected_rhel_repoids = {'rhel-8-for-x86_64-baseos-htb-rpms', 'rhel-8-for-x86_64-appstream-htb-rpms'}
+    assert produced_rhel_repoids == expected_rhel_repoids
