61dba140b6d033a269f4a54bb4d932d338519a8d
Pavel Březina <pbrezina@redhat.com>
2021-10-06 12:50:51 +0200

    Add sssd el8toel9 actor
    Check SSSD configuration for changes in RHEL9 and report them in model.
    
    Implicit files domain is disabled by default. This may affect local
    smartcard authentication if there is not explicit files domain created.
    
    If there is no files domain and smartcard authentication is enabled,
    we will notify the administrator.

---
+++ repos/system_upgrade/el8toel9/actors/sssdfacts/tests/unit_test_sssdfacts_8to9.py
@@ -0,0 +1,168 @@
+import textwrap
+
+from six import StringIO
+
+from leapp.libraries.actor.sssdfacts import SSSDFactsLibrary_8to9
+from leapp.libraries.common import utils
+
+
+def get_config(content):
+    parser = utils.parse_config(StringIO(textwrap.dedent(content)))
+    return parser
+
+
+def test_empty_config():
+    config = utils.parse_config()
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_enable_files_domain_set__notset():
+    config = get_config("""
+    [sssd]
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_enable_files_domain_set__set_true():
+    config = get_config("""
+    [sssd]
+    enable_files_domain = true
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_enable_files_domain_set__set_false():
+    config = get_config("""
+    [sssd]
+    enable_files_domain = false
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_enable_files_domain_set__nodomain():
+    config = get_config("""
+    [sssd]
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_explicit_files_domain__notset():
+    config = get_config("""
+    [sssd]
+    domains = ldap
+
+    [domain/ldap]
+    id_provider = ldap
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_explicit_files_domain__set():
+    config = get_config("""
+    [sssd]
+    domains = files
+
+    [domain/files]
+    id_provider = files
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_pam_cert_auth__notset():
+    config = get_config("""
+    [pam]
+    pam_gssapi_services = sudo
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_pam_cert_auth__true():
+    config = get_config("""
+    [pam]
+    pam_gssapi_services = sudo
+    pam_cert_auth = true
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert facts.pam_cert_auth
+
+
+def test_pam_cert_auth__false():
+    config = get_config("""
+    [pam]
+    pam_gssapi_services = sudo
+    pam_cert_auth = false
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert not facts.explicit_files_domain
+    assert not facts.pam_cert_auth
+
+
+def test_complex_config():
+    config = get_config("""
+    [sssd]
+    user = root
+    services = nss, pam, sudo
+    domain = ldap, files
+
+    [pam]
+    pam_cert_auth = true
+
+    [domain/ldap]
+    id_provider = ldap
+    ldap_groups_use_matching_rule_in_chain = True
+
+    [domain/files]
+    id_provider = files
+    """)
+
+    facts = SSSDFactsLibrary_8to9(config).process()
+
+    assert not facts.enable_files_domain_set
+    assert facts.explicit_files_domain
+    assert facts.pam_cert_auth
