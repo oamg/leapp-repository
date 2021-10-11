c3fbd1fb6941b619d08a1d950103dddef1a72b47
Joe Shimkus <jshimkus@redhat.com>
2021-10-11 12:49:00 -0400

    Clarify terminology and naming.

--- repos/system_upgrade/el8toel9/actors/checkvdo/libraries/checkvdo.py
+++ repos/system_upgrade/el8toel9/actors/checkvdo/libraries/checkvdo.py
@@ -16,9 +16,13 @@ def _run_cmd(command, checked = True):
 def _canonicalize_device_path(path):
     return os.path.realpath(path)
 
-def _get_blkid_results():
-    """ Collect devices that have not been converted to lvm-based management. """
-    # VDO instances that have been converted to lvm-based management are
+#
+# Methods used in discovering VDO instances which require migration to
+# lvm-based management.
+#
+def _get_blkid_vdo_results():
+    """ Collect devices that have not been migrated to lvm-based management. """
+    # VDO instances that have been migrated to lvm-based management are
     # not identified by blkid as vdo type.
     command = ['blkid', '--output', 'device', '--match-token', 'TYPE=vdo']
     result = _run_cmd(command, checked = False)
@@ -27,21 +31,22 @@ def _get_blkid_results():
         raise exceptions.StopActorExecutionError('blkid exited with code: {0}'.format(exit_code))
     return "" if exit_code != 0 else result['stdout']
 
-def _get_unconverted_devices():
-    results = _get_blkid_results()
+
+def _get_unmigrated_vdo_devices():
+    results = _get_blkid_vdo_results()
     # Canonicalize the device paths.
     return [_canonicalize_device_path(x.strip()) for x in results.splitlines()]
 
 
-def _get_dmsetup_results():
+def _get_dmsetup_vdo_results():
     command = ['dmsetup', 'status', '--target', 'vdo']
     return _run_cmd(command)['stdout']
 
 
-def _get_unconverted_devices_info():
+def _get_unmigrated_vdo_devices_info():
     """ Return info for the specified devices. """
-    devices = _get_unconverted_devices()
-    results = _get_dmsetup_results()
+    devices = _get_unmigrated_vdo_devices()
+    results = _get_dmsetup_vdo_results()
     info = {}
     for line in results.splitlines():
         (name, _, _, _, device, *_) = line.split()
@@ -55,10 +60,10 @@ def _get_unconverted_devices_info():
 
 # Check for vdo instances that must be migrated to lvm management.
 def check_vdo():
-    # Find VDO devices that have not been converted to lvm-based management.
-    unconverted_vdo_devices = _get_unconverted_devices_info()
+    # Find VDO devices that have not been migrated to lvm-based management.
+    unmigrated_vdo_devices = _get_unmigrated_vdo_devices_info()
 
-    if len(unconverted_vdo_devices) == 0:
+    if len(unmigrated_vdo_devices) == 0:
         # Generate report indicating there are no vdo instances to migrate.
         reporting.create_report([
             reporting.Title('VDO instances migration to lvm-based management'),
@@ -68,7 +73,7 @@ def check_vdo():
         ])
     else:
         instances = ['{0}[{1}]'.format(name, device)
-                      for (device, name) in unconverted_vdo_devices.items()]
+                      for (device, name) in unmigrated_vdo_devices.items()]
         reporting.create_report([
             reporting.Title('VDO instances migration to lvm-based management'),
             reporting.Summary('VDO instances that require migration: {0}'.format(', '.join(instances))),
@@ -78,5 +83,5 @@ def check_vdo():
             reporting.Flags([reporting.Flags.INHIBITOR])
         ])
 
-    # TDOO: Find vdo instances that were in the process of being converted
+    # TDOO: Find vdo instances that were in the process of being migrated
     #       to lvm-based management but didn't complete the process.
