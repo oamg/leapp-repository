3329ab73b37c4682089e0f5be42fcde58e01f071
Vinzenz Feenstra <vfeenstr@redhat.com>
2022-01-18 11:48:59 +0100

    scandasd: el8 => el9 requires s390utils-core package
    Since el9, the s390utils-core package is no longer installed via
    dependency for s390 systems. This PR moves the actor to common, as it is
    required also for el8 to el9 upgrades and makes sure that the
    s390utils-core package will be installed.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- repos/system_upgrade/common/actors/scandasd/libraries/scandasd.py
+++ repos/system_upgrade/common/actors/scandasd/libraries/scandasd.py
@@ -10,14 +10,16 @@ DASD_CONF = '/etc/dasd.conf'
 def process():
     if not architecture.matches_architecture(architecture.ARCH_S390X):
         return
+    copy_files = []
     if os.path.isfile(DASD_CONF):
         # the file has to be copied into the targetuserspace container first,
         # then it can be included into the initramfs ==> both messages are
         # needed to be produced
-        api.produce(TargetUserSpaceUpgradeTasks(copy_files=[CopyFile(src=DASD_CONF)]))
+        copy_files = [CopyFile(src=DASD_CONF)]
         api.produce(UpgradeInitramfsTasks(include_files=[DASD_CONF]))
     else:
         api.current_logger().warning(
             "The {} file has not been discovered. DASD not used?"
             .format(DASD_CONF)
         )
+    api.produce(TargetUserSpaceUpgradeTasks(copy_files=copy_files, install_rpms=['s390utils-core']))
