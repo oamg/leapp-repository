3829512d74e18b065ddd0b74d3b88504d52ae3dc
Filip Janus <fjanus@redhat.com>
2022-02-21 13:35:35 +0100

    actor: Add mariadbcheck actor for el8toel9 upgrade
    actor: Add mariadbcheck actor for el8toel9 upgrade
    Since RHEL 8 supports multiple Mariadb streams(10.3, 10.5)
    and RHEL 9 contains only Mariadb 10.5 the actor checks presence of
    mariadb-server and provides a link to upgrade documentation.
    
    Related: https://bugzilla.redhat.com/show_bug.cgi?id=2037853

---
+++ repos/system_upgrade/el8toel9/actors/mariadbcheck/libraries/mariadbcheck.py
@@ -0,0 +1,53 @@
+from leapp import reporting
+from leapp.libraries.common.rpms import has_package
+from leapp.libraries.stdlib import api
+from leapp.models import InstalledRedHatSignedRPM
+
+# Summary for mariadb-server report
+report_server_inst_summary = (
+    'MariaDB server component will be upgraded. Since RHEL-9 includes'
+    ' MariaDB server 10.5 by default, which is incompatible with 10.3'
+    ' included in RHEL-8, it is necessary to proceed with additional steps'
+    ' for the complete upgrade of the MariaDB data.'
+)
+
+report_server_inst_hint = (
+    'Back up your data before proceeding with the upgrade'
+    ' and follow steps in the documentation section "Migrating to a RHEL 9 version of MariaDB"'
+    ' after the upgrade.'
+)
+
+# Link URL for mariadb-server report
+report_server_inst_link_url = 'https://access.redhat.com/articles/6743671'
+
+
+def _report_server_installed():
+    """
+    Create report on mariadb-server package installation detection.
+
+    Should remind user about present MariaDB server package
+    installation, warn them about necessary additional steps, and
+    redirect them to online documentation for the upgrade process.
+    """
+    reporting.create_report([
+        reporting.Title('MariaDB (mariadb-server) has been detected on your system'),
+        reporting.Summary(report_server_inst_summary),
+        reporting.Severity(reporting.Severity.MEDIUM),
+        reporting.Tags([reporting.Tags.SERVICES]),
+        reporting.ExternalLink(title='Migrating to a RHEL 9 version of MariaDB',
+                               url=report_server_inst_link_url),
+        reporting.RelatedResource('package', 'mariadb-server'),
+        reporting.Remediation(hint=report_server_inst_hint),
+        ])
+
+
+def report_installed_packages(_context=api):
+    """
+    Create reports according to detected MariaDB packages.
+
+    Create the report if the mariadb-server rpm (RH signed) is installed.
+    """
+    has_server = has_package(InstalledRedHatSignedRPM, 'mariadb-server', context=_context)
+
+    if has_server:
+        _report_server_installed()
