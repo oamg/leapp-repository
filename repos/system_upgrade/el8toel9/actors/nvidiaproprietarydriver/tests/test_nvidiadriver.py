efa3becc424438b3904013310d6a8b7ec675ae6a
Niels De Graef <ndegraef@redhat.com>
2022-04-12 13:08:31 +0200

    el8toel9: Warn about the NVIDIA driver before upgrade

---
+++ repos/system_upgrade/el8toel9/actors/nvidiaproprietarydriver/tests/test_nvidiadriver.py
@@ -0,0 +1,33 @@
+from leapp.models import ActiveKernelModule, ActiveKernelModulesFacts
+from leapp.reporting import Report
+
+
+def test_actor_with_nvidia_driver(current_actor_context):
+    with_nvidia = [
+        ActiveKernelModule(filename='nvidia', parameters=[]),
+        ActiveKernelModule(filename='kvm', parameters=[])]
+
+    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=with_nvidia))
+    current_actor_context.run()
+    report_fields = current_actor_context.consume(Report)[0].report
+    assert 'inhibitor' in report_fields['flags']
+
+
+def test_actor_without_nvidia_driver(current_actor_context):
+    without_nvidia = [
+        ActiveKernelModule(filename='i915', parameters=[]),
+        ActiveKernelModule(filename='kvm', parameters=[])]
+
+    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=without_nvidia))
+    current_actor_context.run()
+    assert not current_actor_context.consume(Report)
+
+
+def test_actor_with_nouveau_driver(current_actor_context):
+    without_nvidia = [
+        ActiveKernelModule(filename='nouveau', parameters=[]),
+        ActiveKernelModule(filename='kvm', parameters=[])]
+
+    current_actor_context.feed(ActiveKernelModulesFacts(kernel_modules=without_nvidia))
+    current_actor_context.run()
+    assert not current_actor_context.consume(Report)
