21ef4e57a99de1e417cf58fbb22644a40e2f63e0
Michal Reznik <mreznik@redhat.com>
2022-05-13 12:00:15 +0200

    Remove temporary leapp directory in /root
    Leaving the directory behind will cause issues during upgrades to
    another major verison. E.g in case of RHEL 7 > RHEL 8 > RHEL 9
    upgrade.

--- repos/system_upgrade/common/actors/preparepythonworkround/libraries/workaround.py
+++ repos/system_upgrade/common/actors/preparepythonworkround/libraries/workaround.py
@@ -1,7 +1,9 @@
 import os
+import shutil
 import sys
 
 from leapp.libraries.common.utils import makedirs
+from leapp.libraries.stdlib import api
 
 LEAPP_HOME = '/root/tmp_leapp_py3'
 
@@ -18,10 +20,15 @@ def _get_orig_leapp_path():
 
 def apply_python3_workaround():
     py3_leapp = os.path.join(LEAPP_HOME, 'leapp3')
+    if os.path.exists(LEAPP_HOME):
+        try:
+            shutil.rmtree(LEAPP_HOME)
+        except OSError as e:
+            api.current_logger().error('Could not remove {} directory: {}'.format(LEAPP_HOME, str(e)))
+
     makedirs(LEAPP_HOME)
     leapp_lib_symlink_path = os.path.join(LEAPP_HOME, 'leapp')
-    if not os.path.exists(leapp_lib_symlink_path):
-        os.symlink(_get_orig_leapp_path(), leapp_lib_symlink_path)
+    os.symlink(_get_orig_leapp_path(), leapp_lib_symlink_path)
     with open(py3_leapp, 'w') as f:
         f_content = [
             '#!/usr/bin/python3',
