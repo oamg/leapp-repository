6bd8f5f24708e3ad1397812e9d445d1f7f25e0fc
Inessa Vasilevskaya <ivasilev@redhat.com>
2022-06-13 18:53:44 +0200

    Use library function is_inhibitor to check for failures
    This patch has to be merged together with leapp's PR677.
    
    Depends-On: https://github.com/oamg/leapp/pull/677
    
    OAMG-1429

--- repos/system_upgrade/common/actors/verifycheckresults/libraries/verifycheckresults.py
+++ repos/system_upgrade/common/actors/verifycheckresults/libraries/verifycheckresults.py
@@ -1,8 +1,9 @@
 from leapp.exceptions import RequestStopAfterPhase
 from leapp.libraries.stdlib import api
 from leapp.reporting import Report
+from leapp.utils.report import is_inhibitor
 
 
 def check():
-    if [msg for msg in api.consume(Report) if 'inhibitor' in msg.report.get('flags', [])]:
+    if [msg for msg in api.consume(Report) if is_inhibitor(msg.report)]:
         raise RequestStopAfterPhase()
