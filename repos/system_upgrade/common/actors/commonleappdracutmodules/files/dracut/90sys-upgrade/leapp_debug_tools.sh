aadf694f946ca4821fe2d9aa47eea67dcb270af9
Matej Matuska <mmatuska@redhat.com>
2022-12-07 21:50:48 +0100

    Add leapp debug tools to initramfs
    Install a script with debug utilities to the Leapp upgrade dracut
    module.

---
+++ repos/system_upgrade/common/actors/commonleappdracutmodules/files/dracut/90sys-upgrade/leapp_debug_tools.sh
@@ -0,0 +1,38 @@
+# library containing some useful functions for debugging in initramfs
+
+# mounts the sysroot
+leapp_dbg_mount() {
+    systemctl start sysroot.mount
+    mount -o remount,rw /sysroot
+}
+
+# source programs from $NEWROOT, mount if not mounted
+leapp_dbg_source() {
+    systemctl is-active sysroot.mount --quiet || {
+        echo "sysroot not mounted, mounting...";
+        leapp_dbg_mount || return 1
+    }
+
+    for dir in /bin /sbin; do
+        export PATH="$PATH:${NEWROOT}$dir"
+    done
+
+    export LD_LIBRARY_PATH=/sysroot/lib64
+}
+
+# chroot into $NEWROOT
+leapp_dbg_chroot() {
+    systemctl is-active sysroot.mount --quiet || {
+        echo "sysroot not mounted, mounting...";
+        leapp_dbg_mount || return 1
+    }
+
+    for dir in /sys /run /proc /dev /dev/pts; do
+        mount --bind $dir "$NEWROOT$dir"
+    done || { echo "Failed to mount some directories" || return 1 }
+
+    chroot $NEWROOT sh -c "mount -a; /bin/bash"
+    for dir in /sys /run /proc /dev/pts /dev; do
+        umount $NEWROOT$dir
+    done
+}
