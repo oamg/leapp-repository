992efb45597e6ccaf5b217b21cdf6c925448ca83
PeterMocary <petermocary@gmail.com>
2023-01-26 15:13:00 +0100

    fixup! fixup! add check for mount points order in /etc/fstab to prevent overshadowing

--- repos/system_upgrade/common/actors/checkfstabmountorder/libraries/checkfstabmountorder.py
+++ repos/system_upgrade/common/actors/checkfstabmountorder/libraries/checkfstabmountorder.py
@@ -1,3 +1,5 @@
+import os
+
 from leapp import reporting
 from leapp.libraries.stdlib import api
 from leapp.models import StorageInfo
@@ -11,7 +13,7 @@ def _get_common_path(path1, path2):
     available in python2.7, thus can't be used here.
     """
 
-    if path1 == '' or path2 == '':
+    if not path1 or not path2:
         return ''
 
     path1 = path1.strip('/').split('/')
@@ -22,7 +24,7 @@ def _get_common_path(path1, path2):
         if path1_part != path2_part:
             break
         common_path.append(path1_part)
-    return '/' + '/'.join(common_path)
+    return os.path.join('/', *common_path)
 
 
 def _get_overshadowing_mount_points(mount_points):
@@ -30,73 +32,56 @@ def _get_overshadowing_mount_points(mount_points):
     Retrieve set of overshadowing and overshadowed mount points.
 
     :param list[str] mount_points: absolute paths to mount points
-    :returns: set of mount points
+    :returns: set of unique mount points without trailing /
     """
-
     overshadowing = set()
+    mount_points = [mp.rstrip('/') for mp in mount_points]
     for i, mount_point in enumerate(mount_points):
-        if mount_point[-1] == '/':
-            mount_point = mount_point[:-1]
-
         for overshadowing_mount_point in mount_points[i+1:]:
-            if overshadowing_mount_point[-1] == '/':
-                overshadowing_mount_point = overshadowing_mount_point[:-1]
-
             if _get_common_path(mount_point, overshadowing_mount_point) == overshadowing_mount_point:
                 overshadowing.add(overshadowing_mount_point)
                 overshadowing.add(mount_point)
-
     return overshadowing
 
 
 def check_fstab_mount_order():
     storage_info = next(api.consume(StorageInfo), None)
 
-    if storage_info:
+    if not storage_info:
+        return
 
-        mount_points = []
-        for fstab_entry in storage_info.fstab:
-            mount_point = fstab_entry.fs_file
-            if mount_point[-1] == '/':
-                mount_point = mount_point[:-1]
-            mount_points.append(mount_point)
+    mount_points = [fstab_entry.fs_file.rstrip('/') for fstab_entry in storage_info.fstab]
+    overshadowing = _get_overshadowing_mount_points(mount_points)
+    duplicates = {mp for mp in mount_points if mount_points.count(mp) > 1}
 
-        overshadowing = _get_overshadowing_mount_points(mount_points)
+    if not overshadowing:
+        return
 
-        if overshadowing:
+    overshadowing_in_order = [mp for mp in mount_points if mp in overshadowing]
+    overshadowing_fixed = sorted(set(mount_points), key=len)
+    summary = 'Leapp detected incorrect /etc/fstab format that causes overshadowing of mount points.'
+    hint = 'To prevent the overshadowing:'
 
-            overshadowing_in_order = []
-            for mount_point in mount_points:
-                if mount_point in overshadowing:
-                    overshadowing_in_order.append(mount_point)
+    if duplicates:
+        summary += '\nDetected mount points with duplicates:\n{}'.format(', '.join(duplicates))
+        hint += '\nRemove detected duplicates by using unique mount points.'
 
-            duplicates = set()
-            for mount_point in mount_points:
-                if mount_points.count(mount_point) > 1:
-                    duplicates.add(mount_point)
+    if overshadowing:
+        summary += '\nDetected order of overshadowing mount points:\n{}'.format(', '.join(overshadowing_in_order))
+        hint += (
+            '\nReorder the detected overshadowing entries.'
+            '\nPossible order of all mount points without overshadowing:\n{}'
+        ).format(', '.join(overshadowing_fixed))
 
-            summary = (
-                'Leapp detected incorrect order of entries in /etc/fstab that causes '
-                'overshadowing of mount points.\nDetected order of overshadowing mount points:\n{}'
-            ).format(', '.join(overshadowing_in_order))
-
-            if duplicates:
-                summary += ',\nDetected duplicate mount points:\n{}'.format(', '.join(duplicates))
-
-            hint = (
-                'To prevent the overshadowing reorder the entries in /etc/fstab and remove any duplicates. '
-                'Possible order of mount points without overshadowing:\n{}'
-            ).format(', '.join(sorted(overshadowing, key=len)))
-
-            reporting.create_report([
-                reporting.Title(
-                    'Detected incorrect order of entries or duplicate entries in /etc/fstab, preventing a successful '
-                    'in-place upgrade.'
-                ),
-                reporting.Summary(summary),
-                reporting.Remediation(hint=hint),
-                reporting.RelatedResource('file', '/etc/fstab'),
-                reporting.Severity(reporting.Severity.HIGH),
-                reporting.Groups([reporting.Groups.FILESYSTEM]),
-                reporting.Groups([reporting.Groups.INHIBITOR]),
-            ])
+    reporting.create_report([
+        reporting.Title(
+            'Detected incorrect order of entries or duplicate entries in /etc/fstab, preventing a successful '
+            'in-place upgrade.'
+        ),
+        reporting.Summary(summary),
+        reporting.Remediation(hint=hint),
+        reporting.RelatedResource('file', '/etc/fstab'),
+        reporting.Severity(reporting.Severity.HIGH),
+        reporting.Groups([reporting.Groups.FILESYSTEM]),
+        reporting.Groups([reporting.Groups.INHIBITOR]),
+    ])
