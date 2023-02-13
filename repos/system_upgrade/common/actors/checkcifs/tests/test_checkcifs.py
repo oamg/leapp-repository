060ee2f8337a6a6d9e5b371b55042de27dcd477b
Inessa Vasilevskaya <ivasilev@redhat.com>
2023-02-13 12:41:35 +0100

    Move CIFS check actor to common
    The check should be the safe for 7to8 and 8to9 upgrade.
    Also add logic to skip the check in case
    LEAPP_DEVEL_INITRAM_NETWORK env var is set.
    
    OAMG-8490

--- repos/system_upgrade/common/actors/checkcifs/tests/test_checkcifs.py
+++ repos/system_upgrade/common/actors/checkcifs/tests/test_checkcifs.py
@@ -1,10 +1,12 @@
+from leapp.libraries.common import config
 from leapp.models import FstabEntry, StorageInfo
 from leapp.reporting import Report
 from leapp.snactor.fixture import current_actor_context
 from leapp.utils.report import is_inhibitor
 
 
-def test_actor_with_fstab_entry(current_actor_context):
+def test_actor_with_fstab_entry(current_actor_context, monkeypatch):
+    monkeypatch.setattr(config, 'get_env', lambda x, y: y)
     with_fstab_entry = [FstabEntry(fs_spec="//10.20.30.42/share1", fs_file="/mnt/win_share1",
                                    fs_vfstype="cifs",
                                    fs_mntops="credentials=/etc/win-credentials,file_mode=0755,dir_mode=0755",
@@ -25,7 +27,8 @@ def test_actor_with_fstab_entry(current_actor_context):
     assert report_fields['title'] == "Use of CIFS detected. Upgrade can't proceed"
 
 
-def test_actor_no_cifs(current_actor_context):
+def test_actor_no_cifs(current_actor_context, monkeypatch):
+    monkeypatch.setattr(config, 'get_env', lambda x, y: y)
     with_fstab_entry = [FstabEntry(fs_spec="/dev/mapper/fedora-home", fs_file="/home",
                                    fs_vfstype="ext4",
                                    fs_mntops="defaults,x-systemd.device-timeout=0",
