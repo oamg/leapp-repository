4deae49aa381c2fd9b220ddc0d77193768baf41d
Toshio Kuratomi <tkuratom@redhat.com>
2024-03-26 01:58:12 -0700

    Fix kernel cmdline args we add not being propogated to newly installed kernels.
    On some upgrades, any kernel commandline args that we were adding were added to the default kernel
    but once the user installed a new kernel, those args were not propogated to the new kernel.  This
    was happening on S390x and on RHEL7=>8 upgrades.
    
    To fix this, we add the kernel commandline args to both the default kernel and to the defaults for
    all kernels.
    
    On S390x and upgrades to RHEL9 or greater, this is done by placing the kernel cmdline arguments into
    the /etc/kernel/cmdline file.
    
    On upgrades to RHEL <= 8 for all architectures other than S390x, this is done by having
    grub2-editenv modify the /boot/grub2/grubenv file.

--- repos/system_upgrade/common/actors/kernelcmdlineconfig/actor.py
+++ repos/system_upgrade/common/actors/kernelcmdlineconfig/actor.py
@@ -1,8 +1,10 @@
 import os
 
+from leapp import reporting
 from leapp.actors import Actor
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.actor import kernelcmdlineconfig
+from leapp.libraries.stdlib import api
 from leapp.models import FirmwareFacts, InstalledTargetKernelInfo, KernelCmdlineArg, TargetKernelCmdlineArgTasks
 from leapp.tags import FinalizationPhaseTag, IPUWorkflowTag
 
@@ -29,4 +31,27 @@ class KernelCmdlineConfig(Actor):
 
         if ff.firmware == 'bios' and os.path.ismount('/boot/efi'):
             configs = ['/boot/grub2/grub.cfg', '/boot/efi/EFI/redhat/grub.cfg']
-        kernelcmdlineconfig.modify_kernel_args_in_boot_cfg(configs)
+
+        try:
+            kernelcmdlineconfig.modify_kernel_args_in_boot_cfg(configs)
+        except kernelcmdlineconfig.ReadOfKernelArgsError as e:
+            api.current_logger().error(str(e))
+            reporting.create_report([
+                reporting.Title('Could not retrieve kernel command line arguments: {}'.format(e)),
+                reporting.Summary(
+                    'Unable to retrieve the existing kernel command line arguments in order'
+                    ' to set the default value for future installed kernels.  After the'
+                    ' system has been rebooted into the new version of RHEL, you should'
+                    ' check what kernel command line options are present in /proc/cmdline'
+                    ' and copy them into /etc/kernel/cmdline before installing any new kernels.'
+                ),
+                reporting.Severity(reporting.Severity.MEDIUM),
+                reporting.Groups([
+                    reporting.Groups.BOOT,
+                    reporting.Groups.KERNEL,
+                    reporting.Groups.POST,
+                ]),
+                reporting.RelatedResource('file', '/etc/kernel/cmdline'),
+                reporting.RelatedResource('file', '/proc/cmdline'),
+            ])
+            return
--- repos/system_upgrade/common/actors/kernelcmdlineconfig/libraries/kernelcmdlineconfig.py
+++ repos/system_upgrade/common/actors/kernelcmdlineconfig/libraries/kernelcmdlineconfig.py
@@ -1,8 +1,18 @@
+import re
+
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries import stdlib
-from leapp.libraries.common.config import architecture
+from leapp.libraries.common.config import architecture, version
 from leapp.libraries.stdlib import api
 from leapp.models import InstalledTargetKernelInfo, KernelCmdlineArg, TargetKernelCmdlineArgTasks
+
+KERNEL_CMDLINE_FILE = "/etc/kernel/cmdline"
+
+
+class ReadOfKernelArgsError(Exception):
+    """
+    Failed to retrieve the kernel command line arguments
+    """
 
 
 def run_grubby_cmd(cmd):
@@ -31,12 +41,24 @@ def format_kernelarg_msgs_for_grubby_cmd(kernelarg_msgs):
     return ' '.join(kernel_args)
 
 
+def set_default_kernel_args(kernel_args):
+    if (architecture.matches_architecture(architecture.ARCH_S390X) or
+            version.matches_target_version(">= 9.0")):
+        # Put kernel_args into /etc/kernel/cmdline
+        with open(KERNEL_CMDLINE_FILE, 'w') as f:
+            f.write(kernel_args)
+    else:
+        # Use grub2-editenv to put the kernel args into /boot/grub2/grubenv
+        stdlib.run(['grub2-editenv', '-', 'set', 'kernelopts={}'.format(kernel_args)])
+
+
 def modify_kernel_args_in_boot_cfg(configs_to_modify_explicitly=None):
     kernel_info = next(api.consume(InstalledTargetKernelInfo), None)
     if not kernel_info:
         return
 
     # Collect desired kernelopt modifications
+
     kernelargs_msgs_to_add = list(api.consume(KernelCmdlineArg))
     kernelargs_msgs_to_remove = []
     for target_kernel_arg_task in api.consume(TargetKernelCmdlineArgTasks):
@@ -45,6 +67,8 @@ def modify_kernel_args_in_boot_cfg(configs_to_modify_explicitly=None):
 
     if not kernelargs_msgs_to_add and not kernelargs_msgs_to_remove:
         return  # There is no work to do
+
+    # Modify the kernel cmdline for the default kernel
 
     grubby_modify_kernelargs_cmd = ['grubby', '--update-kernel={0}'.format(kernel_info.kernel_img_path)]
 
@@ -64,3 +88,23 @@ def modify_kernel_args_in_boot_cfg(configs_to_modify_explicitly=None):
             run_grubby_cmd(cmd)
     else:
         run_grubby_cmd(grubby_modify_kernelargs_cmd)
+
+    # Copy the args for the default kernel to be for all kernels.
+
+    kernel_args = None
+    cmd = ['grubby', '--info', kernel_info.kernel_img_path]
+    output = stdlib.run(cmd, split=False)
+    for record in output['stdout'].splitlines():
+        # This could be done with one regex but it's cleaner to parse it as
+        # structured data.
+        if record.startswith('args='):
+            data = record.split("=", 1)[1]
+            matches = re.match(r'^([\'"]?)(.*)\1$', data)
+            kernel_args = matches.group(2)
+            break
+    else:
+        raise ReadOfKernelArgsError(
+            "Failed to retrieve kernel command line to save for future installed kernels."
+        )
+
+    set_default_kernel_args(kernel_args)
