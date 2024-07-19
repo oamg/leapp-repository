423591160006b876ef1e9d415ff6f287fe36e3f0
tomasfratrik <tomasfratrik8@gmail.com>
2024-07-19 11:40:39 +0200

    Improve the description

--- repos/system_upgrade/common/actors/removeleftoverpackages/actor.py
+++ repos/system_upgrade/common/actors/removeleftoverpackages/actor.py
@@ -9,7 +9,8 @@ class RemoveLeftoverPackages(Actor):
     Remove packages left on the system after the upgrade to higher major version of RHEL.
 
     Removal of packages is necessary in order to keep the machine in supported state.
-    Actor generates report telling users what packages have been removed.
+    Produce a message that is consumed by the `reportleftoverpackages` actor,
+    which reports on the packages that have been removed.
     """
 
     name = 'remove_leftover_packages'
