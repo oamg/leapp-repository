oamg/leapp-repository

commit 7e0fb44bb673893d0409903f6a441d0eb2829d22

don't require all versions to be defined for obsoleted keys
in releases where we do not have any obsoleted keys, we still had to
define an entry (with an empty list), as otherwise the code would fail

instead, we can catch the KeyError and carry on as nothing happened

repos/system_upgrade/common/actors/removeobsoletegpgkeys/libraries/removeobsoleterpmgpgkeys.py
@@ -12,11 +12,14 @@ def _get_obsolete_keys():
     distribution = api.current_actor().configuration.os_release.release_id
     obsoleted_keys_map = get_distribution_data(distribution).get('obsoleted-keys', {})
     keys = []
-    for version in range(7, int(get_target_major_version()) + 1):
-        for key in obsoleted_keys_map[str(version)]:
-            name, version, release = key.rsplit("-", 2)
-            if has_package(InstalledRPM, name, version=version, release=release):
-                keys.append(key)
+    try:
+        for version in range(7, int(get_target_major_version()) + 1):
+            for key in obsoleted_keys_map[str(version)]:
+                name, version, release = key.rsplit("-", 2)
+                if has_package(InstalledRPM, name, version=version, release=release):
+                    keys.append(key)
+    except KeyError:
+        pass
 
     return keys
 

commit 45ef5496ab94564481b12dd28d6ceeba6efb5c4e

properly scope try/except when loading obsoleted keys
We want to load all possible keys, even *after* a KeyError happenend

Fixes: 7e0fb44bb673893d0409903f6a441d0eb2829d22

repos/system_upgrade/common/actors/removeobsoletegpgkeys/libraries/removeobsoleterpmgpgkeys.py
@@ -12,14 +12,14 @@ def _get_obsolete_keys():
     distribution = api.current_actor().configuration.os_release.release_id
     obsoleted_keys_map = get_distribution_data(distribution).get('obsoleted-keys', {})
     keys = []
-    try:
-        for version in range(7, int(get_target_major_version()) + 1):
+    for version in range(7, int(get_target_major_version()) + 1):
+        try:
             for key in obsoleted_keys_map[str(version)]:
                 name, version, release = key.rsplit("-", 2)
                 if has_package(InstalledRPM, name, version=version, release=release):
                     keys.append(key)
-    except KeyError:
-        pass
+        except KeyError:
+            pass
 
     return keys
 

repos/system_upgrade/common/actors/removeobsoletegpgkeys/tests/test_removeobsoleterpmgpgkeys.py
@@ -77,6 +77,56 @@ def test_get_obsolete_keys(monkeypatch, version, expected):
 
 
 @pytest.mark.parametrize(
+    "version, obsoleted_keys, expected",
+    [
+        (10, None, []),
+        (10, {}, []),
+        (10, {"8": ["gpg-pubkey-888-abc"], "10": ["gpg-pubkey-10-10"]}, ["gpg-pubkey-888-abc", "gpg-pubkey-10-10"]),
+        (9, {"8": ["gpg-pubkey-888-abc"], "9": ["gpg-pubkey-999-def"]}, ["gpg-pubkey-999-def", "gpg-pubkey-888-abc"]),
+        (8, {"8": ["gpg-pubkey-888-abc"], "9": ["gpg-pubkey-999-def"]}, ["gpg-pubkey-888-abc"])
+    ]
+)
+def test_get_obsolete_keys_incomplete_data(monkeypatch, version, obsoleted_keys, expected):
+    def get_target_major_version_mocked():
+        return version
+
+    def get_distribution_data_mocked(_distro):
+        if obsoleted_keys is None:
+            return {}
+        return {'obsoleted-keys': obsoleted_keys}
+
+    def has_package_mocked(*args, **kwargs):
+        return True
+
+    monkeypatch.setattr(
+        removeobsoleterpmgpgkeys,
+        "get_target_major_version",
+        get_target_major_version_mocked,
+    )
+
+    monkeypatch.setattr(
+        removeobsoleterpmgpgkeys,
+        "get_distribution_data",
+        get_distribution_data_mocked,
+    )
+
+    monkeypatch.setattr(
+        removeobsoleterpmgpgkeys,
+        "has_package",
+        has_package_mocked,
+    )
+
+    monkeypatch.setattr(
+        api,
+        "current_actor",
+        CurrentActorMocked(),
+    )
+
+    keys = removeobsoleterpmgpgkeys._get_obsolete_keys()
+    assert set(keys) == set(expected)
+
+
+@pytest.mark.parametrize(
     "keys, should_register",
     [
         (["gpg-pubkey-d4082792-5b32db75"], True),