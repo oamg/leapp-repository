81a3297516fbbd120b0fb870de36f1a1b290dd21
Jarek Prokop <jprokop@redhat.com>
2024-11-06 15:21:14 +0100

    Expand on the actor docstring for the el8->el9 rubygem-irb symlink fix.
    In RHEL 10, the directory is a regular directory again.
    
    The 2 actors are separate over creating a common solution for both.
    Expand in the docstring on the reason for the el8->el9 actor to
    differentiate them apart.

--- repos/system_upgrade/el8toel9/actors/registerrubyirbadjustment/actor.py
+++ repos/system_upgrade/el8toel9/actors/registerrubyirbadjustment/actor.py
@@ -5,7 +5,16 @@ from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 
 class RegisterRubyIRBAdjustment(Actor):
     """
-    Registers a workaround which will adjust the Ruby IRB directories during the upgrade.
+    Register a workaround to allow rubygem-irb's directory -> symlink conversion.
+
+    The /usr/share/ruby/irb has been moved from a directory to a symlink
+    in RHEL 9 and this conversion was not handled on RPM level.
+    This leads to DNF reporting package file conflicts when a major upgrade
+    is attempted and rubygem-irb (or ruby-irb) is installed.
+
+    Register "handlerubyirbsymlink" script that removes the directory prior
+    to DNF upgrade and allows it to create the expected symlink in place of
+    the removed directory.
     """
 
     name = 'register_ruby_irb_adjustment'
