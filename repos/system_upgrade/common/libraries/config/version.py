8cadc8864ed3bcfa3023644dc0457b339db096b8
Matej Matuska <mmatuska@redhat.com>
2025-02-12 15:33:21 +0100

    Drop 9.5

--- repos/system_upgrade/common/libraries/config/version.py
+++ repos/system_upgrade/common/libraries/config/version.py
@@ -19,7 +19,7 @@ _SUPPORTED_VERSIONS = {
     # Note: 'rhel-alt' is detected when on 'rhel' with kernel 4.x
     '7': {'rhel': ['7.9'], 'rhel-alt': [], 'rhel-saphana': ['7.9']},
     '8': {'rhel': ['8.10'], 'rhel-saphana': ['8.10']},
-    '9': {'rhel': ['9.4', '9.5', '9.6'], 'rhel-saphana': ['9.4', '9.6']},
+    '9': {'rhel': ['9.6'], 'rhel-saphana': ['9.6']},
 }
 
 
