2646484407bef15688fb4838c2f61d06f2098f81
Petr Stodulka <pstodulk@redhat.com>
2025-04-30 12:54:22 +0200

    Fix parsing of the kernel cmdline
    Original parsing has been splitting string tokens by "=" without a
    limit, means that e.g.
       root=UUID=some-id
    has been split to
       (root, UUID, some-id)
    and and stored just "root" and "UUID" strings in the parsed msg,
    instead of "root" and "UUID=some-id".
    
    So split the key=value just based on the first occurence of "="
    instead.
    
    JIRA: RHELMISC-12490
    
    Co-Authored-By: Karolina Kula <kkula@redhat.com>

--- repos/system_upgrade/common/actors/scankernelcmdline/actor.py
+++ repos/system_upgrade/common/actors/scankernelcmdline/actor.py
@@ -1,12 +1,12 @@
 from leapp.actors import Actor
-from leapp.libraries.stdlib import run
-from leapp.models import KernelCmdline, KernelCmdlineArg
+from leapp.libraries.actor import scankernelcmdline
+from leapp.models import KernelCmdline
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 
 
 class ScanKernelCmdline(Actor):
     """
-    No documentation has been provided for the scan_kernel_cmdline actor.
+    Scan the kernel command line of the booted system.
     """
 
     name = 'scan_kernel_cmdline'
@@ -15,12 +15,4 @@ class ScanKernelCmdline(Actor):
     tags = (FactsPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        cmdline = run(['cat', '/proc/cmdline'])['stdout'].strip()
-        parameters = []
-        for parameter in cmdline.split(' '):
-            if '=' in parameter:
-                kv = parameter.split('=')
-                parameters.append(KernelCmdlineArg(key=kv[0], value=kv[1]))
-            else:
-                parameters.append(KernelCmdlineArg(key=parameter))
-        self.produce(KernelCmdline(parameters=parameters))
+        scankernelcmdline.parse_cmdline_input()
