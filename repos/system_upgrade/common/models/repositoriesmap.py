08593787ed90f60ee1d01a45f3ba90fa974b04f9
Petr Stodulka <pstodulk@redhat.com>
2025-05-27 13:12:29 +0200

    squash! repomap: Add distro to field support
    Update the PESIDRepositoryEntry model:
     * add the distro field represented by String
     * change rhui type from StringEnum to String
    
    Reflects changes in the repomap json schema for version 1.3.0,
    which drops enumeration for the `rhui` field and replace it by simple
    String type to make the solution more scalable. This also means that
    vendor of the repomap data file must validate this field "manually".

--- repos/system_upgrade/common/models/repositoriesmap.py
+++ repos/system_upgrade/common/models/repositoriesmap.py
@@ -71,14 +71,14 @@ class PESIDRepositoryEntry(Model):
     purposes. The other channels indicate premium repositories.
     """
 
-    rhui = fields.StringEnum(['', 'aws', 'azure', 'google', 'alibaba'])
+    rhui = fields.String(default='')
     """
     Specifies what cloud provider (RHUI) is the repository specific to.
 
     Empty string denotes that the repository is not specific to any cloud provider.
     """
 
-    distro = fields.StringEnum(['rhel', 'centos'])
+    distro = fields.String()
     """
     Specifies what distribution is the repository specific to.
     """
