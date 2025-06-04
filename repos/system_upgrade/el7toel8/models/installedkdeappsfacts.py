c3a21747edd7ead60a18f3b1ec840bb512f5832d
Tomas Fratrik <tfratrik@redhat.com>
2025-06-04 15:50:25 +0200

    Drop el7toel8 leapp repository
    Removing el7toel8 repository since the upgrade path is no longer
    supported.
    Changes:
    * el7toel8 repo removed
    * Reflect this change in the documentation
    * Makefile adjusted
    * Adjusted tests for scancustommodifications actor, which tested actors
      from el7toel8 repo.
    
    Jira: RHELMISC-11005

--- repos/system_upgrade/el7toel8/models/installedkdeappsfacts.py
+++
@@ -1,7 +0,0 @@
-from leapp.models import fields, Model
-from leapp.topics import SystemFactsTopic
-
-
-class InstalledKdeAppsFacts(Model):
-    topic = SystemFactsTopic
-    installed_apps = fields.List(fields.String(), default=[])
