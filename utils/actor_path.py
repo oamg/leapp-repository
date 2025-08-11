abf782ceb16b408796e644e9912720b54719987b
Matej Matuska <mmatuska@redhat.com>
2025-08-11 14:32:40 +0200

    Makefile: Partially fix ACTOR tests and enable them in containers
    The ACTOR variable for running tests of a single actor has long been
    unusable, git bisect led me to dc3abf6 as the commit where
    it was (first?) broken due to the same model module being defined in
    both el7toel8 and el8toel9 repos.
    
    This patch unfortunately doesn't fix that, but works around it by
    enabling the utils/actor_path.py script to search in specific
    repositories. These are passed from the Makefile via REPOSITORIES
    variable.
    
    As the Makefile rules for containerized tests already use repositories,
    the ACTOR variable is just passed along.
    
    NOTE: the code in actor_path.py is ugly and uses private APIs, however
    that's nothing new :).

--- utils/actor_path.py
+++ utils/actor_path.py
@@ -1,7 +1,10 @@
 import logging
+import os
 import sys
 
-from leapp.repository.scan import find_and_scan_repositories
+from leapp.repository.manager import RepositoryManager
+from leapp.repository.scan import _resolve_repository_links, find_and_scan_repositories, scan_repo
+
 
 def err_exit():
     # We want to be sure that `make test` (test_no_lint) will stop when expected
@@ -10,22 +13,36 @@ def err_exit():
     sys.stdout.write('ERROR:__read_error_messages_above_this_one_on_stderr__')
     sys.exit(1)
 
+
 def main():
     logging.basicConfig(level=logging.INFO, filename='/dev/null')
     logger = logging.getLogger('run_pytest.py')
 
     BASE_REPO = 'repos'
-    repos = find_and_scan_repositories(BASE_REPO, include_locals=True)
-    repos.load()
-    if len(sys.argv) > 1:
-        actors = repos._lookup_actors(sys.argv[1])
-        if not actors:
-            sys.stderr.write('ERROR: No actor found for search "{}"\n'.format(sys.argv[1]))
-            err_exit()
-        print(' '.join([actor.full_path for actor in actors]))
+    SYSUPG_REPO = os.path.join(BASE_REPO, 'system_upgrade')
+
+    if len(sys.argv) == 2:
+        manager = find_and_scan_repositories(BASE_REPO, include_locals=True)
+        manager.load()
+    elif len(sys.argv) == 3:
+        repos = sys.argv[2].split(',')
+        # TODO: it would be nicer to have some function in the framework for
+        # the scanning and resolving done below
+        manager = RepositoryManager()
+        for repo in repos:
+            manager.add_repo(scan_repo(os.path.join(SYSUPG_REPO, repo)))
+        _resolve_repository_links(manager=manager, include_locals=True)
+        manager.load()
     else:
         sys.stderr.write('ERROR: Missing commandline argument\n')
+        sys.stderr.write('Usage: actor_path.py <actor_name> [repositories]\n')
         err_exit()
+
+    actors = manager._lookup_actors(sys.argv[1])
+    if not actors:
+        sys.stderr.write('ERROR: No actor found for search "{}"\n'.format(sys.argv[1]))
+        err_exit()
+    print(' '.join([actor.full_path for actor in actors]))
 
 
 if __name__ == '__main__':
