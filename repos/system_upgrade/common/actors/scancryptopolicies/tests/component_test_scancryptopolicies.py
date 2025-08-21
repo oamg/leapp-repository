2804698e108cae2b5310b7b94ed313517c94eb8d
Matej Matuska <mmatuska@redhat.com>
2025-08-21 19:49:42 +0200

    scancryptopolicies: Remove RHEL 7 early return

--- repos/system_upgrade/common/actors/scancryptopolicies/tests/component_test_scancryptopolicies.py
+++ repos/system_upgrade/common/actors/scancryptopolicies/tests/component_test_scancryptopolicies.py
@@ -1,18 +1,10 @@
 import os
 
-import pytest
-
 from leapp.libraries.actor import scancryptopolicies
-from leapp.libraries.common.config import version
 from leapp.models import CryptoPolicyInfo
 
 
-@pytest.mark.parametrize(('source_version', 'should_run'), [
-    ('7', False),
-    ('8', True),
-    ('9', True),
-])
-def test_actor_execution(monkeypatch, current_actor_context, source_version, should_run):
+def test_actor_execution(monkeypatch, current_actor_context):
     def read_current_policy_mock(filename):
         return "DEFAULT_XXX"
 
@@ -28,19 +20,14 @@ def test_actor_execution(monkeypatch, current_actor_context, source_version, sho
         return _original_listdir(path)
 
     def isfile_mock(filename):
-        if filename.endswith('/modules'):
-            return False
-        return True
+        return not filename.endswith('/modules')
 
-    monkeypatch.setattr(version, 'get_source_major_version', lambda: source_version)
     monkeypatch.setattr(scancryptopolicies, 'read_current_policy', read_current_policy_mock)
     _original_listdir = os.listdir
     monkeypatch.setattr(os, 'listdir', listdir_mock)
     monkeypatch.setattr(os.path, 'isfile', isfile_mock)
     current_actor_context.run()
-    if should_run:
-        cpi = current_actor_context.consume(CryptoPolicyInfo)
-        assert cpi
-        assert cpi[0].current_policy == 'DEFAULT_XXX'
-    else:
-        assert not current_actor_context.consume(CryptoPolicyInfo)
+
+    cpi = current_actor_context.consume(CryptoPolicyInfo)
+    assert cpi
+    assert cpi[0].current_policy == 'DEFAULT_XXX'
