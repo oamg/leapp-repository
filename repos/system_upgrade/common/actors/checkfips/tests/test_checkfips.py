d327f568a9ecb5de67e219c9174f547dadfbc8bd
Matej Matuska <mmatuska@redhat.com>
2025-08-21 18:00:53 +0200

    checkfips: Drop RHEL 7 inhibitor and update tests
    The tests never covered the part where UpgradeInitramfsTasks have to get
    produced by the actor on 8->9.

--- repos/system_upgrade/common/actors/checkfips/tests/test_checkfips.py
+++ repos/system_upgrade/common/actors/checkfips/tests/test_checkfips.py
@@ -1,23 +1,40 @@
 import pytest
 
 from leapp.libraries.common.config import version
-from leapp.models import FIPSInfo, Report
+from leapp.models import DracutModule, FIPSInfo, Report, UpgradeInitramfsTasks
 from leapp.utils.report import is_inhibitor
 
 
-@pytest.mark.parametrize(('fips_info', 'target_major_version', 'should_inhibit'), [
-    (FIPSInfo(is_enabled=True), '8', True),
-    (FIPSInfo(is_enabled=True), '9', False),
-    (FIPSInfo(is_enabled=False), '8', False),
+@pytest.mark.parametrize(('fips_info', 'target_major_version', 'should_produce'), [
     (FIPSInfo(is_enabled=False), '9', False),
+    (FIPSInfo(is_enabled=True), '9', True),
+    (FIPSInfo(is_enabled=False), '10', False),
+    (FIPSInfo(is_enabled=True), '10', False),
 ])
-def test_check_fips(monkeypatch, current_actor_context, fips_info, target_major_version, should_inhibit):
+def test_check_fips(monkeypatch, current_actor_context, fips_info, target_major_version, should_produce):
     monkeypatch.setattr(version, 'get_target_major_version', lambda: target_major_version)
+
     current_actor_context.feed(fips_info)
     current_actor_context.run()
-    if should_inhibit:
-        output = current_actor_context.consume(Report)
+
+    # no inhibitor in any case
+    assert not any(is_inhibitor(msg.report) for msg in current_actor_context.consume(Report))
+
+    output = current_actor_context.consume(UpgradeInitramfsTasks)
+    if should_produce:
         assert len(output) == 1
-        assert is_inhibitor(output[0].report)
+
+        expected_initramfs_files = [
+            '/etc/crypto-policies/back-ends/opensslcnf.config',
+            '/etc/pki/tls/openssl.cnf',
+            '/usr/lib64/ossl-modules/fips.so',
+        ]
+
+        assert output[0].include_files == expected_initramfs_files
+
+        assert len(output[0].include_dracut_modules) == 1
+        mod = output[0].include_dracut_modules[0]
+        assert isinstance(mod, DracutModule)
+        assert mod.name == "fips"
     else:
-        assert not any(is_inhibitor(msg.report) for msg in current_actor_context.consume(Report))
+        assert not output
