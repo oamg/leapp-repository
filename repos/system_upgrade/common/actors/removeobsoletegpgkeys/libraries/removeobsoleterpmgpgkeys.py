18f73d4a0f1d120cdac2363d909ce14d45d47f35
Matej Matuska <mmatuska@redhat.com>
2025-09-19 17:21:29 +0200

    SQUASH removeobsoletegpgkeys

--- repos/system_upgrade/common/actors/removeobsoletegpgkeys/libraries/removeobsoleterpmgpgkeys.py
+++ repos/system_upgrade/common/actors/removeobsoletegpgkeys/libraries/removeobsoleterpmgpgkeys.py
@@ -1,3 +1,4 @@
+from leapp.libraries.common.config import get_target_distro_id
 from leapp.libraries.common.config.version import get_target_major_version
 from leapp.libraries.common.distro import get_distribution_data
 from leapp.libraries.common.rpms import has_package
@@ -9,7 +10,7 @@ def _get_obsolete_keys():
     """
     Return keys obsoleted in target and previous versions
     """
-    distribution = api.current_actor().configuration.os_release.release_id
+    distribution = get_target_distro_id()
     obsoleted_keys_map = get_distribution_data(distribution).get('obsoleted-keys', {})
     keys = []
     for version in range(7, int(get_target_major_version()) + 1):
