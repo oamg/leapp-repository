0dfa695ff37bd02f2f3e2c3df66b97c86eb56ff0
Petr Stodulka <pstodulk@redhat.com>
2025-12-03 18:43:22 +0100

    Check NVMe devices: handle NVMe-FC and NVMe-pci
    TBD
    
    Jira-ref: RHEL-46807

---
+++ repos/system_upgrade/common/actors/checknvme/actor.py
@@ -0,0 +1,30 @@
+from leapp.actors import Actor
+from leapp.libraries.actor import checknvme
+from leapp.models import NVMEInfo
+from leapp.reporting import Report
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+
+
+class CheckLuks(Actor):
+    """
+    Check if NVMe devices are used and possibly register additional actions.
+
+    To be able to boot correctly the storage with NVMe devices, the initramfs
+    needs to contain nvmf dracut module with additional possibly required data.
+    Register all required actions that should happen to handle correctly systems
+    with NVMe devices.
+
+    Currently covered:
+        * NVMe (PCIe)
+        * NVMe-FC
+    """
+    # FIXME(pstodulk): update the description once the actor is fully
+    # implemented
+
+    name = 'check_nvme'
+    consumes = (NVMEInfo,)
+    produces = (Report, TargetUserSpaceUpgradeTasks, UpgradeInitramfsTasks)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        checknvme.process()
