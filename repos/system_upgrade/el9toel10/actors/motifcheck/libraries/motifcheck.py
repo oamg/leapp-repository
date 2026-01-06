639c0ca0e232e3e78770d5945bf96c575a783194
Tomas Pelka <tpelka@redhat.com>
2026-01-06 16:35:45 +0100

    Add motifcheck actor for RHEL 9 to RHEL 10 upgrade
    Add a new actor to detect the Motif package on RHEL 9 systems and warn
    users that Motif is no longer available in RHEL 10. The actor creates
    a high-severity report recommending migration to alternative GUI toolkits
    such as GTK or Qt.
    
    Related: DESKTOP-1399

---
+++ repos/system_upgrade/el9toel10/actors/motifcheck/libraries/motifcheck.py
@@ -0,0 +1,54 @@
+from leapp import reporting
+from leapp.libraries.common.rpms import has_package
+from leapp.libraries.stdlib import api
+from leapp.models import DistributionSignedRPM
+
+# Summary for motif report
+report_motif_inst_summary = (
+    'The Motif package has been detected on your system. Motif is no longer available in RHEL 10.'
+    ' Applications that depend on Motif will not work after the upgrade.'
+    ' You will need to either migrate to an alternative GUI toolkit (such as GTK or Qt)'
+    ' or maintain the Motif package through alternative means.'
+)
+
+report_motif_inst_hint = (
+    'Consider migrating applications to a modern GUI toolkit before proceeding with the upgrade.'
+    ' If you must retain Motif-based applications, you may need to rebuild the Motif package'
+    ' from source or find alternative distribution channels. Please review your application'
+    ' requirements and plan accordingly.'
+)
+
+# Link URL for motif report
+report_motif_inst_link_url = 'https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/10/html/10.0_release_notes/removed-features#removed-features-graphics-infrastructures'
+
+
+def _report_motif_installed():
+    """
+    Create report on motif package installation detection.
+
+    Should remind user about present motif package
+    installation, warn them about the lack of motif support in RHEL 10, and
+    redirect them to online documentation for the migration process.
+    """
+    reporting.create_report([
+        reporting.Title('Motif has been detected on your system'),
+        reporting.Summary(report_motif_inst_summary),
+        reporting.Severity(reporting.Severity.HIGH),
+        reporting.Groups([reporting.Groups.SERVICES]),
+        reporting.ExternalLink(title='RHEL 10 Removed Features - Graphics Infrastructures',
+                               url=report_motif_inst_link_url),
+        reporting.RelatedResource('package', 'motif'),
+        reporting.Remediation(hint=report_motif_inst_hint),
+        ])
+
+
+def report_installed_packages(_context=api):
+    """
+    Create reports according to detected motif packages.
+
+    Create the report if the motif rpm (RH signed) is installed.
+    """
+    has_motif = has_package(DistributionSignedRPM, 'motif', context=_context)
+
+    if has_motif:
+        _report_motif_installed()
