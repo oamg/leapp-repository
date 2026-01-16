e14b695bd279cb369440694f0982018253975a23
Tomas Pelka <tpelka@redhat.com>
2026-01-16 15:50:49 +0100

    Add xorgcheck actor for RHEL 9 to RHEL 10 upgrade
    Implements detection of Xorg server packages that are removed in RHEL 10.
    The actor checks for the following packages:
    - xorg-x11-server-Xdmx
    - xorg-x11-server-Xephyr
    - xorg-x11-server-Xnest
    - xorg-x11-server-Xorg
    - xorg-x11-server-Xvfb
    
    Creates a HIGH severity report when any Xorg server packages are detected,
    warning users about their removal and suggesting migration to Wayland.
    
    Jira: DESKTOP-1326

---
+++ repos/system_upgrade/el9toel10/actors/xorgcheck/libraries/xorgcheck.py
@@ -0,0 +1,65 @@
+from leapp import reporting
+from leapp.libraries.common.rpms import has_package
+from leapp.models import DistributionSignedRPM
+
+# List of Xorg server packages to check
+XORG_PACKAGES = [
+    'xorg-x11-server-Xdmx',
+    'xorg-x11-server-Xephyr',
+    'xorg-x11-server-Xnest',
+    'xorg-x11-server-Xorg',
+    'xorg-x11-server-Xvfb',
+]
+
+# Summary for Xorg report
+report_xorg_inst_summary = (
+    'Xorg server packages have been detected on your system. The Xorg server is no longer available '
+    'in RHEL 10. Applications and services that depend on these Xorg server packages will not work '
+    'after the upgrade. You will need to migrate to Wayland or maintain the Xorg packages through '
+    'alternative means.'
+)
+
+report_xorg_inst_hint = (
+    'Consider migrating to Wayland before proceeding with the upgrade.'
+)
+
+# Link URL for Xorg report
+report_xorg_inst_link_url = 'https://red.ht/rhel-10-removed-features-graphics-infrastructures'
+
+
+def _report_xorg_installed(packages):
+    """
+    Create report on Xorg server package installation detection.
+
+    Should remind user about present Xorg server package installations,
+    warn them about the lack of Xorg support in RHEL 10, and redirect
+    them to online documentation for the migration process.
+
+    Parameters:
+        packages (list): List of installed Xorg package names
+    """
+    reporting.create_report([
+        reporting.Title('Xorg server packages have been detected on your system'),
+        reporting.Summary(report_xorg_inst_summary),
+        reporting.Severity(reporting.Severity.HIGH),
+        reporting.Groups([reporting.Groups.SERVICES]),
+        reporting.ExternalLink(title='RHEL 10 Removed Features - Graphics Infrastructures',
+                               url=report_xorg_inst_link_url),
+        reporting.Remediation(hint=report_xorg_inst_hint),
+        ] + [reporting.RelatedResource('package', pkg) for pkg in packages])
+
+
+def report_installed_packages():
+    """
+    Create reports according to detected Xorg server packages.
+
+    Create the report if any Xorg server rpm (RH signed) is installed.
+    """
+    installed_packages = []
+
+    for package in XORG_PACKAGES:
+        if has_package(DistributionSignedRPM, package):
+            installed_packages.append(package)
+
+    if installed_packages:
+        _report_xorg_installed(installed_packages)
