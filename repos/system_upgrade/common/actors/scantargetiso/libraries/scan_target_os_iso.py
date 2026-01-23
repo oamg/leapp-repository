729b1cf99226b1b0f37dce7ce6b9a8e687df40a6
Matej Matuska <mmatuska@redhat.com>
2026-01-23 12:09:10 +0100

    scan_target_os_iso: Make the actor distro agnostic
    The determination of target version from the ISO does several checks
    with hardcoded values such as the redhat-release packages name, the
    filename and contents of /etc/redhat-release, as well as parsing the
    actual version from there.
    
    Most of these checks use hardcoded values which are only valid on RHEL.
    This patch makes the checks work on CentOS Stream and Alma Linux by
    obtaining the values from a map by distro.

--- repos/system_upgrade/common/actors/scantargetiso/libraries/scan_target_os_iso.py
+++ repos/system_upgrade/common/actors/scantargetiso/libraries/scan_target_os_iso.py
@@ -5,44 +5,83 @@ from leapp.libraries.common.mounting import LoopMount, MountError
 from leapp.libraries.stdlib import api, CalledProcessError, run
 from leapp.models import CustomTargetRepository, TargetOSInstallationImage
 
+DISTRO_RELEASE_PKGS = {
+    'rhel': 'redhat-release',
+    'centos': 'centos-stream-release',
+    'almalinux': 'almalinux-release',
+}
+
+DISTRO_RELEASE_FILES = {
+    'rhel': '/etc/redhat-release',
+    'centos': '/etc/centos-release',
+    'almalinux': '/etc/almalinux-release',
+}
+
+# TODO move these out
+DISTRO_NAMES = {
+    'rhel': 'Red Hat Enterprise Linux',
+    'centos': 'CentOS Stream',
+    'almalinux': 'AlmaLinux',
+}
+
 
 def determine_rhel_version_from_iso_mountpoint(iso_mountpoint):
     baseos_packages = os.path.join(iso_mountpoint, 'BaseOS/Packages')
     if os.path.isdir(baseos_packages):
-        def is_rh_release_pkg(pkg_name):
-            return pkg_name.startswith('redhat-release') and 'eula' not in pkg_name
+        target_distro = ipu_config.get_target_distro_id()
 
-        redhat_release_pkgs = [pkg for pkg in os.listdir(baseos_packages) if is_rh_release_pkg(pkg)]
+        def is_release_pkg(pkg_name):
+            return pkg_name.startswith(DISTRO_RELEASE_PKGS[target_distro]) and 'eula' not in pkg_name
 
-        if not redhat_release_pkgs:
+        distro_release_pkgs = [pkg for pkg in os.listdir(baseos_packages) if is_release_pkg(pkg)]
+
+        if not distro_release_pkgs:
             return ''  # We did not determine anything
 
-        if len(redhat_release_pkgs) > 1:
-            api.current_logger().warning('Multiple packages with name redhat-release* found when '
-                                         'determining RHEL version of the supplied installation ISO.')
+        if len(distro_release_pkgs) > 1:
+            api.current_logger().warning(
+                "Multiple packages with name {}* found when determining target version of the supplied"
+                " installation ISO.".format(DISTRO_RELEASE_PKGS[target_distro])
+            )
 
-        redhat_release_pkg = redhat_release_pkgs[0]
+        distro_release_pkg = distro_release_pkgs[0]
 
-        determined_rhel_ver = ''
         try:
-            rh_release_pkg_path = os.path.join(baseos_packages, redhat_release_pkg)
+            release_pkg_path = os.path.join(baseos_packages, distro_release_pkg)
             # rpm2cpio is provided by rpm; cpio is a dependency of yum (rhel7) and a dependency of dracut which is
             # a dependency for leapp (rhel8+)
-            cpio_archive = run(['rpm2cpio', rh_release_pkg_path])
-            etc_rh_release_contents = run(['cpio', '--extract', '--to-stdout', './etc/redhat-release'],
-                                          stdin=cpio_archive['stdout'])
+            cpio_archive = run(['rpm2cpio', release_pkg_path])
+            etc_release_contents = run(
+                [
+                    "cpio",
+                    "--extract",
+                    "--to-stdout",
+                    f".{DISTRO_RELEASE_FILES[target_distro]}",
+                ],
+                stdin=cpio_archive["stdout"],
+            )
 
-            # 'Red Hat Enterprise Linux Server release 7.9 (Maipo)' -> ['Red Hat...', '7.9 (Maipo']
-            product_release_fragments = etc_rh_release_contents['stdout'].split('release')
+            # 'Red Hat Enterprise Linux Server release 7.9 (Maipo)' -> ['Red Hat...', '7.9 (Maipo)']
+            # Red Hat Enterprise Linux release 8.10 (Ootpa)
+            # CentOS Stream release 8
+            product_release_fragments = etc_release_contents['stdout'].split('release')
             if len(product_release_fragments) != 2:
                 return ''  # Unlikely. Either way we failed to parse the release
 
-            if not product_release_fragments[0].startswith('Red Hat'):
+            if not product_release_fragments[0].startswith(DISTRO_NAMES[target_distro]):
                 return ''
 
-            determined_rhel_ver = product_release_fragments[1].strip().split(' ', 1)[0]  # Remove release name (Maipo)
-            return determined_rhel_ver
+            determined_ver = product_release_fragments[1].strip().split(' ', 1)[0]  # Remove release name (Maipo)
+            return determined_ver
         except CalledProcessError:
+            # FIXME?: This might fail e.g. if the ISO isn't complete
+            # (download/scp/...) interrupted. Maybe we should at include
+            # info that in the report?
+            # Leaving an exact example from the logs (yes the empty line is there):
+            # error: /var/lib/leapp/iso_scan_mountpoint/BaseOS/Packages/centos-stream-release-9.0-26.el9.noarch.rpm: read failed: Input/output error (5)
+
+            # error reading header from package
+
             return ''
     return ''
 
@@ -62,7 +101,7 @@ def inform_ipu_about_request_to_use_target_iso():
                                               was_mounted_successfully=False))
         return
 
-    # Mount the given ISO, extract the available repositories and determine provided RHEL version
+    # Mount the given ISO, extract the available repositories and determine provided target version
     iso_scan_mountpoint = '/var/lib/leapp/iso_scan_mountpoint'
     try:
         with LoopMount(source=target_iso_path, target=iso_scan_mountpoint):
