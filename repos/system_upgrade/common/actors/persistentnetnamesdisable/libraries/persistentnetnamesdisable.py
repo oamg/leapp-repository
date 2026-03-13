8d8774c34518a6269d56f100c2fc312ec0bbbde1
Petr Stodulka <pstodulk@redhat.com>
2026-03-13 16:15:20 +0100

    persistentnetnamesdisable: refactoring + fix ethX detection
    The original check of ethX interfaces was buggy as it detected all
    interfaces with the eth[0-9] substring:
      * eth0
      * preeth0
      * eth0post
      ...
    The check has been corrected to search just for ethX kernel names,
    skipping NIC names with additional prefixes or suffixes. Added test
    for the ethX_count function. Tests have been slightly refactored to
    minimize planned changes in the Interface model.
    
    Also the actor has been originally executed in incorrect phase.
    Moving the actor to CheckPhase where it should be. Also I refactorred
    the actor a little bit, moving the code to the private library to
    follow current best practices.
    
    Jira: RHEL-3370

---
+++ repos/system_upgrade/common/actors/persistentnetnamesdisable/libraries/persistentnetnamesdisable.py
@@ -0,0 +1,75 @@
+import re
+
+from leapp import reporting
+from leapp.libraries.common.config.version import get_target_major_version
+from leapp.libraries.stdlib import api
+from leapp.models import KernelCmdlineArg, PersistentNetNamesFacts
+from leapp.reporting import create_report
+
+
+def ethX_count(interfaces):
+    """
+    Count how many network interfaces with ethX naming is present.
+    """
+    ethX = re.compile('^eth[0-9]+$')
+    count = 0
+
+    for i in interfaces:
+        if ethX.match(i.name):
+            count = count + 1
+    return count
+
+
+def single_eth0(interfaces):
+    return len(interfaces) == 1 and interfaces[0].name == 'eth0'
+
+
+def disable_persistent_naming():
+    api.current_logger().info(
+        "Single eth0 network interface detected."
+        " Appending 'net.ifnames=0' for the target system kernel commandline"
+    )
+    api.produce(KernelCmdlineArg(**{'key': 'net.ifnames', 'value': '0'}))
+
+
+def report_ethX_ifaces():
+    report_entries = [
+        reporting.Title('Unsupported network configuration'),
+        reporting.Summary(
+            'Detected multiple physical network interfaces where one or more'
+            ' use kernel naming (e.g. eth0). Upgrade process cannot continue'
+            ' because stability of names can not be guaranteed.'
+        ),
+        reporting.ExternalLink(
+            title='How to Perform an In-Place Upgrade when Using Kernel-Assigned NIC Names',
+            url='https://access.redhat.com/solutions/4067471'
+        ),
+        reporting.Remediation(
+            hint='Rename all ethX network interfaces following the attached KB solution article.'
+        ),
+        reporting.Severity(reporting.Severity.HIGH),
+        reporting.Groups([reporting.Groups.NETWORK]),
+        reporting.Groups([reporting.Groups.INHIBITOR])
+    ]
+
+    if get_target_major_version() == '9':
+        report_entries.append(
+            reporting.ExternalLink(
+                title='RHEL 8 to RHEL 9: inplace upgrade fails at '
+                      '"Network configuration for unsupported device types detected"',
+                url='https://access.redhat.com/solutions/7009239'
+            )
+        )
+
+    create_report(report_entries)
+
+
+def process():
+    interfaces = next(api.consume(PersistentNetNamesFacts)).interfaces
+
+    if single_eth0(interfaces):
+        disable_persistent_naming()
+        return
+
+    if len(interfaces) > 1 and ethX_count(interfaces):
+        report_ethX_ifaces()
--- repos/system_upgrade/common/actors/persistentnetnamesdisable/tests/test_persistentnetnamesdisable.py
+++ repos/system_upgrade/common/actors/persistentnetnamesdisable/tests/test_persistentnetnamesdisable.py
@@ -1,17 +1,53 @@
 import pytest
 
-from leapp.libraries.common.config import version
+from leapp.libraries.actor import persistentnetnamesdisable
 from leapp.models import Interface, KernelCmdlineArg, PCIAddress, PersistentNetNamesFacts
 from leapp.reporting import Report
 from leapp.snactor.fixture import current_actor_context
 from leapp.utils.report import is_inhibitor
 
 
+def _gen_ifaces_by_names(names):
+    pci = PCIAddress(domain="0000", bus="3e", function="00", device="PCI bridge")
+    interfaces = []
+    for nic_name in names:
+        interfaces.append(Interface(
+            name=nic_name,
+            devpath="/devices/platform/usb/cdc-wdm0",
+            driver="pcieport",
+            mac="52:54:00:0b:4a:6d",
+            pci_info=pci,
+            vendor="redhat",
+        ))
+    return interfaces
+
+
+@pytest.mark.parametrize(('interfaces', 'exp_result'), (
+    (_gen_ifaces_by_names(['eno1', 'eno2', 'myfoo00', 'nicname']), 0),
+    (_gen_ifaces_by_names(['preeth0', 'eth2post', 'preeth0post']), 0),
+    (_gen_ifaces_by_names(['eth0']), 1),
+    (_gen_ifaces_by_names(['eth0', 'eth1', 'eth01', 'eth4980']), 4),
+    (_gen_ifaces_by_names(['myeth0', 'eth0', 'something']), 1),
+))
+def test_ethX_count(interfaces, exp_result):
+    """
+    Test the correct detection of ethX interfaces.
+
+    It tests the bug causing https://issues.redhat.com/browse/RHEL-3370
+    """
+    assert persistentnetnamesdisable.ethX_count(interfaces) == exp_result
+
+
 def test_actor_single_eth0(current_actor_context):
     pci = PCIAddress(domain="0000", bus="3e", function="00", device="PCI bridge")
-    interface = [Interface(name="eth0", mac="52:54:00:0b:4a:6d", vendor="redhat",
-                           driver="pcieport", pci_info=pci,
-                           devpath="/devices/platform/usb/cdc-wdm0")]
+    interface = [Interface(
+        name="eth0",
+        mac="52:54:00:0b:4a:6d",
+        vendor="redhat",
+        driver="pcieport",
+        pci_info=pci,
+        devpath="/devices/platform/usb/cdc-wdm0"
+    )]
     current_actor_context.feed(PersistentNetNamesFacts(interfaces=interface))
     current_actor_context.run()
     assert not current_actor_context.consume(Report)
@@ -21,15 +57,25 @@ def test_actor_single_eth0(current_actor_context):
     'target_version', ['9', '10']
 )
 def test_actor_more_ethX(monkeypatch, current_actor_context, target_version):
-    monkeypatch.setattr(version, 'get_target_major_version', lambda: target_version)
+    monkeypatch.setattr(persistentnetnamesdisable, 'get_target_major_version', lambda: target_version)
     pci1 = PCIAddress(domain="0000", bus="3e", function="00", device="PCI bridge")
     pci2 = PCIAddress(domain="0000", bus="3d", function="00", device="Serial controller")
-    interface = [Interface(name="eth0", mac="52:54:00:0b:4a:6d", vendor="redhat",
-                           driver="pcieport", pci_info=pci1,
-                           devpath="/devices/platform/usb/cdc-wdm0"),
-                 Interface(name="eth1", mac="52:54:00:0b:4a:6a", vendor="redhat",
-                           driver="serial", pci_info=pci2,
-                           devpath="/devices/hidraw/hidraw0")]
+    interface = [
+        Interface(
+            name="eth0",
+            mac="52:54:00:0b:4a:6d",
+            vendor="redhat",
+            driver="pcieport",
+            pci_info=pci1,
+            devpath="/devices/platform/usb/cdc-wdm0"),
+        Interface(
+            name="eth1",
+            mac="52:54:00:0b:4a:6a",
+            vendor="redhat",
+            driver="serial",
+            pci_info=pci2,
+            devpath="/devices/hidraw/hidraw0")
+    ]
     current_actor_context.feed(PersistentNetNamesFacts(interfaces=interface))
     current_actor_context.run()
 
@@ -50,9 +96,15 @@ def test_actor_more_ethX(monkeypatch, current_actor_context, target_version):
 
 def test_actor_single_int_not_ethX(current_actor_context):
     pci = PCIAddress(domain="0000", bus="3e", function="00", device="PCI bridge")
-    interface = [Interface(name="tap0", mac="52:54:00:0b:4a:60", vendor="redhat",
-                           driver="pcieport", pci_info=pci,
-                           devpath="/devices/platform/usb/cdc-wdm0")]
+    interface = [
+        Interface(
+            name="tap0",
+            mac="52:54:00:0b:4a:60",
+            vendor="redhat",
+            driver="pcieport",
+            pci_info=pci,
+            devpath="/devices/platform/usb/cdc-wdm0")
+    ]
     current_actor_context.feed(PersistentNetNamesFacts(interfaces=interface))
     current_actor_context.run()
     assert not current_actor_context.consume(Report)
@@ -62,15 +114,25 @@ def test_actor_single_int_not_ethX(current_actor_context):
     'target_version', ['9', '10']
 )
 def test_actor_ethX_and_not_ethX(monkeypatch, current_actor_context, target_version):
-    monkeypatch.setattr(version, 'get_target_major_version', lambda: target_version)
+    monkeypatch.setattr(persistentnetnamesdisable, 'get_target_major_version', lambda: target_version)
     pci1 = PCIAddress(domain="0000", bus="3e", function="00", device="PCI bridge")
     pci2 = PCIAddress(domain="0000", bus="3d", function="00", device="Serial controller")
-    interface = [Interface(name="virbr0", mac="52:54:00:0b:4a:6d", vendor="redhat",
-                           driver="pcieport", pci_info=pci1,
-                           devpath="/devices/platform/usb/cdc-wdm0"),
-                 Interface(name="eth0", mac="52:54:00:0b:4a:6a", vendor="redhat",
-                           driver="serial", pci_info=pci2,
-                           devpath="/devices/hidraw/hidraw0")]
+    interface = [
+        Interface(
+            name="virbr0",
+            mac="52:54:00:0b:4a:6d",
+            vendor="redhat",
+            driver="pcieport",
+            pci_info=pci1,
+            devpath="/devices/platform/usb/cdc-wdm0"),
+        Interface(
+            name="eth0",
+            mac="52:54:00:0b:4a:6a",
+            vendor="redhat",
+            driver="serial",
+            pci_info=pci2,
+            devpath="/devices/hidraw/hidraw0")
+    ]
     current_actor_context.feed(PersistentNetNamesFacts(interfaces=interface))
     current_actor_context.run()
     assert current_actor_context.consume(Report)
