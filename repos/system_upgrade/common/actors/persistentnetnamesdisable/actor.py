edd087f30667b771c8a5bb1d4b308c7a042c675e
Petr Stodulka <pstodulk@redhat.com>
2026-03-13 16:15:20 +0100

    persistentnetnamesdisable: refactoring + fix ethX detection
    The original check of ethX interfaces was buggy as it detected all
    interfaces with the eth[0-9] substring:
      * eth0
      * preeth0
      * eth0post
      ...
    The check has been corrected to search just for ethX kernel names,
    skipping NIC names with additional prefixes or suffixes. Added test
    for the ethX_count function. Tests have been slightly refactored to
    minimize planned changes in the Interface model.
    
    Also the actor has been originally executed in incorrect phase.
    Moving the actor to CheckPhase where it should be. Also I refactorred
    the actor a little bit, moving the code to the private library to
    follow current best practices.
    
    Jira: RHEL-3370

--- repos/system_upgrade/common/actors/persistentnetnamesdisable/actor.py
+++ repos/system_upgrade/common/actors/persistentnetnamesdisable/actor.py
@@ -1,11 +1,8 @@
-import re
-
-from leapp import reporting
 from leapp.actors import Actor
-from leapp.libraries.common.config.version import get_target_major_version
+from leapp.libraries.actor import persistentnetnamesdisable
 from leapp.models import KernelCmdlineArg, PersistentNetNamesFacts
-from leapp.reporting import create_report, Report
-from leapp.tags import FactsPhaseTag, IPUWorkflowTag
+from leapp.reporting import Report
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
 
 
 class PersistentNetNamesDisable(Actor):
@@ -16,57 +13,7 @@ class PersistentNetNamesDisable(Actor):
     name = 'persistentnetnamesdisable'
     consumes = (PersistentNetNamesFacts,)
     produces = (KernelCmdlineArg, Report)
-    tags = (FactsPhaseTag, IPUWorkflowTag)
-
-    @staticmethod
-    def ethX_count(interfaces):
-        ethX = re.compile('eth[0-9]+')
-        count = 0
-
-        for i in interfaces:
-            if ethX.match(i.name):
-                count = count + 1
-        return count
-
-    @staticmethod
-    def single_eth0(interfaces):
-        return len(interfaces) == 1 and interfaces[0].name == 'eth0'
-
-    def disable_persistent_naming(self):
-        self.log.info("Single eth0 network interface detected. Appending 'net.ifnames=0' to RHEL-8 kernel commandline")
-        self.produce(KernelCmdlineArg(**{'key': 'net.ifnames', 'value': '0'}))
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        interfaces = next(self.consume(PersistentNetNamesFacts)).interfaces
-
-        if self.single_eth0(interfaces):
-            self.disable_persistent_naming()
-        elif len(interfaces) > 1 and self.ethX_count(interfaces) > 0:
-            report_entries = [
-                reporting.Title('Unsupported network configuration'),
-                reporting.Summary(
-                    'Detected multiple physical network interfaces where one or more use kernel naming (e.g. eth0). '
-                    'Upgrade process can not continue because stability of names can not be guaranteed. '
-                ),
-                reporting.ExternalLink(
-                    title='How to Perform an In-Place Upgrade when Using Kernel-Assigned NIC Names',
-                    url='https://access.redhat.com/solutions/4067471'
-                ),
-                reporting.Remediation(
-                    hint='Rename all ethX network interfaces following the attached KB solution article.'
-                ),
-                reporting.Severity(reporting.Severity.HIGH),
-                reporting.Groups([reporting.Groups.NETWORK]),
-                reporting.Groups([reporting.Groups.INHIBITOR])
-            ]
-
-            if get_target_major_version() == '9':
-                report_entries.append(
-                    reporting.ExternalLink(
-                        title='RHEL 8 to RHEL 9: inplace upgrade fails at '
-                              '"Network configuration for unsupported device types detected"',
-                        url='https://access.redhat.com/solutions/7009239'
-                    )
-                )
-
-            create_report(report_entries)
+        persistentnetnamesdisable.process()
