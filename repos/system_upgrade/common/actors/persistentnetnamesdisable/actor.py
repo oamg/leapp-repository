806e65d67cac9e16c0341f366133ae3c14844fcd
Petr Stodulka <pstodulk@redhat.com>
2026-03-25 15:57:37 +0100

    persistentnetnamesdisable: Mention net.naming-scheme in report
    Since RHEL 8 it's possible to specifcy net.naming-scheme to have
    a consistent NIC names across RHEL major releases. When a specific
    net.naming-scheme is not specified in kernel cmdline, suggest people
    this option as well.

--- repos/system_upgrade/common/actors/persistentnetnamesdisable/actor.py
+++ repos/system_upgrade/common/actors/persistentnetnamesdisable/actor.py
@@ -1,6 +1,7 @@
 from leapp.actors import Actor
 from leapp.libraries.actor import persistentnetnamesdisable
 from leapp.models import (
+    KernelCmdline,
     PersistentNetNamesFacts,
     TargetKernelCmdlineArgTasks,
     UpgradeKernelCmdlineArgTasks
@@ -11,11 +12,21 @@ from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
 
 class PersistentNetNamesDisable(Actor):
     """
-    Disable systemd-udevd persistent network naming on machine with single eth0 NIC
+    Check whether the system has any (physical) NICs with kernel naming (ethX)
+
+    The kernel naming is in general unstable - there is no guarantee of persistent
+    NIC names between reboots, so eth0 can becaome eth3 and vice versa. If the
+    system has more than one physical network interface, the kernel naming must
+    not be used otherwise the upgrade is inhibited. The report contains remediation
+    hints for user to resolve the problem before the upgrade.
+
+    On systems with only one physical network interface with eth0 NIC, register
+    task to disable systemd-udevd persistent network naming (set `net.ifnames=0`
+    on kernel cmdline for the upgrade environment and the upgraded system.
     """
 
     name = 'persistentnetnamesdisable'
-    consumes = (PersistentNetNamesFacts,)
+    consumes = (PersistentNetNamesFacts, KernelCmdline)
     produces = (Report, TargetKernelCmdlineArgTasks, UpgradeKernelCmdlineArgTasks)
     tags = (ChecksPhaseTag, IPUWorkflowTag)
 
