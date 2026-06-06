937df7ca5d6c772012fd60b901dc8e5d81ea32fc
Petr Stodulka <pstodulk@redhat.com>
2026-06-06 12:30:55 +0200

    repositoriesmapping: scan_repositories -> load_repositories_mapping
    The original `scan_repositories()` name was confusing and it did not
    reflect the purpose of the function. The new name
    `load_repositories_mapping()` seems to be more intuitive.
    
    Olso dropped obsolete check for the "repomap.csv" file. The file existed
    only on RHEL 7, more than 3 years ago. So it's a good time to remove
    this relict from the past.

--- repos/system_upgrade/common/actors/targetcontentresolver/libraries/targetcontentresolver.py
+++ repos/system_upgrade/common/actors/targetcontentresolver/libraries/targetcontentresolver.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.actor import pes_events_scanner, repositoriesblocklist, setuptargetrepos
-from leapp.libraries.actor.repomap_loader import scan_repositories
+from leapp.libraries.actor.repomap_loader import load_repositories_mapping
 from leapp.libraries.stdlib import api
 from leapp.models import CustomTargetRepository, RepositoriesBlacklisted, RepositoriesFacts, RepositoriesSetupTasks
 from leapp.utils.deprecation import suppress_deprecation
@@ -97,7 +97,7 @@ def process():
         raise StopActorExecutionError(str(e))
 
     external_tasks = _get_external_reposetup_tasks()
-    repositories_map_msg = scan_repositories()
+    repositories_map_msg = load_repositories_mapping()
     blocklisted_repoids = repositoriesblocklist.compute_blocklist(
         repositories_map_msg,
         external_tasks,
