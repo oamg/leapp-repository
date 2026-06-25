b0c371911fa0afa2814472c552f0986a9662a51b
mcharaya <mcharaya@redhat.com>
2026-06-25 12:16:21 -0400

    eap_repo_blocklist: enable matching EAP repos and blocklist 8.0

--- repos/system_upgrade/el8toel9/actors/eap_repo_blocklist/actor.py
+++ repos/system_upgrade/el8toel9/actors/eap_repo_blocklist/actor.py
@@ -1,20 +1,24 @@
 from leapp.actors import Actor
 from leapp.libraries.actor import eap_repo_blocklist
-from leapp.models import DistributionSignedRPM, RepositoriesBlacklisted
+from leapp.models import DistributionSignedRPM, RepositoriesBlacklisted, RepositoriesSetupTasks
+from leapp.reporting import Report
 from leapp.tags import IPUWorkflowTag, FactsPhaseTag
 
 
 class EapRepoBlocklist(Actor):
     """
-    Blocklist conflicting EAP repos during RHEL in-place upgrade.
-    Detects the installed EAP version (7.4, 8.0, 8.1) from DistributionSignedRPM
+    Check and handle EAP repos during RHEL in-place upgrade.
+
+    Detects the installed EAP version (7.4, 8.1) from DistributionSignedRPM
     and produces RepositoriesBlacklisted to disable non-matching EAP repos for
     the target RHEL 9, preventing wrong package resolution during upgrade.
+    EAP 7.4 enables both jb-eap-7.4-for-rhel-9 and jb-eap-7.4-els-for-rhel-9 repos.
+    EAP 8.0 is not supported for leapp upgrade and will produce an inhibitor.
     """
 
     name = 'eap_repo_blocklist'
     consumes = (DistributionSignedRPM,)
-    produces = (RepositoriesBlacklisted,)
+    produces = (RepositoriesBlacklisted, RepositoriesSetupTasks, Report)
     tags = (IPUWorkflowTag, FactsPhaseTag)
 
     def process(self):
--- repos/system_upgrade/el8toel9/actors/eap_repo_blocklist/libraries/eap_repo_blocklist.py
+++ repos/system_upgrade/el8toel9/actors/eap_repo_blocklist/libraries/eap_repo_blocklist.py
@@ -1,10 +1,18 @@
 from leapp.libraries.stdlib import api
-from leapp.models import DistributionSignedRPM, RepositoriesBlacklisted
+from leapp.models import DistributionSignedRPM, RepositoriesBlacklisted, RepositoriesSetupTasks
+from leapp.reporting import create_report, Title, Summary, Severity, Groups, Remediation
 
 EAP_RHEL9_REPOS = {
-    '7.4': 'jb-eap-7.4-for-rhel-9-x86_64-rpms',
-    '8.0': 'jb-eap-8.0-for-rhel-9-x86_64-rpms',
-    '8.1': 'jb-eap-8.1-for-rhel-9-x86_64-rpms',
+    '7.4': [
+        'jb-eap-7.4-for-rhel-9-x86_64-rpms',
+        'jb-eap-7.4-els-for-rhel-9-x86_64-rpms',
+    ],
+    '8.0': [
+        'jb-eap-8.0-for-rhel-9-x86_64-rpms',
+    ],
+    '8.1': [
+        'jb-eap-8.1-for-rhel-9-x86_64-rpms',
+    ],
 }
 
 
@@ -29,10 +37,28 @@ def process():
 
     api.current_logger().info('Detected installed EAP version: {}'.format(installed_eap))
 
+    if installed_eap == '8.0':
+        create_report([
+            Title('JBoss EAP 8.0 upgrade via leapp is not supported'),
+            Summary(
+                'JBoss EAP 8.0 is not in the leapp upgrade scope. '
+                'Upgrade JBoss EAP to 8.1 before performing the RHEL upgrade.'
+            ),
+            Severity(Severity.HIGH),
+            Groups([Groups.INHIBITOR]),
+            Remediation(hint='Upgrade JBoss EAP to version 8.1 prior to running leapp upgrade.'),
+        ])
+        return
+
+    to_enable = EAP_RHEL9_REPOS[installed_eap]
     to_blocklist = [
-        repo for version, repo in EAP_RHEL9_REPOS.items()
+        repo
+        for version, repos in EAP_RHEL9_REPOS.items()
         if version != installed_eap
+        for repo in repos
     ]
 
+    api.current_logger().info('Enabling repos: {}'.format(to_enable))
     api.current_logger().info('Blocklisting repos: {}'.format(to_blocklist))
+    api.produce(RepositoriesSetupTasks(to_enable=to_enable))
     api.produce(RepositoriesBlacklisted(repoids=to_blocklist))
